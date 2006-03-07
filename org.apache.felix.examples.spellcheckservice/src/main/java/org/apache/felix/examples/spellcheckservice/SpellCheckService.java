e4def9f905e55e4fa0c83829dffd1fddebbbc659
Alex Karasulu <akarasulu@apache.org>
2006-03-07 16:50:26 +0000

    got the fifth example working
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@383928 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.examples.spellcheckservice/src/main/java/org/apache/felix/examples/spellcheckservice/SpellCheckService.java
+++ org.apache.felix.examples.spellcheckservice/src/main/java/org/apache/felix/examples/spellcheckservice/SpellCheckService.java
@@ -18,9 +18,23 @@ package org.apache.felix.examples.spellcheckservice;
 
 
 /**
+ * A simple service interface that defines a spell check service. A spell check
+ * service checks the spelling of all words in a given passage. A passage is any
+ * number of words separated by a space character and the following punctuation
+ * marks: comma, period, exclamation mark, question mark, semi-colon, and colon.
  * 
  * @author <a href="mailto:felix-dev@incubator.apache.org">Felix Project Team</a>
  */
 public interface SpellCheckService
 {
+    /**
+     * Checks a given passage for spelling errors. A passage is any number of
+     * words separated by a space and any of the following punctuation marks:
+     * comma (,), period (.), exclamation mark (!), question mark (?),
+     * semi-colon (;), and colon(:).
+     * 
+     * @param passage the passage to spell check.
+     * @return An array of misspelled words or null if no words are misspelled.
+     */
+    public String[] check( String passage );
 }
