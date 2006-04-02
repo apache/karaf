3b4e4efc51e420068d9a55cee7a70e0e3f6ca7ab
Michael E. Rodriguez <erodriguez@apache.org>
2006-04-02 20:07:31 +0000

    Updates to Felix examples related to recent artifactId, package, and other renaming: o  "Gravity Service Binder" to "Apache Felix Service Binder" o  org.ungoverned.gravity to org.apache.felix.servicebinder o  org.osgi to org.osgi.core
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@390895 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.examples.spellcheckbinder/src/main/java/org/apache/felix/examples/spellcheckbinder/Activator.java
+++ org.apache.felix.examples.spellcheckbinder/src/main/java/org/apache/felix/examples/spellcheckbinder/Activator.java
@@ -17,14 +17,14 @@
 package org.apache.felix.examples.spellcheckbinder;
 
 
-import org.ungoverned.gravity.servicebinder.GenericActivator;
+import org.apache.felix.servicebinder.GenericActivator;
 
 
 /**
  * This example re-implements the spell check service of Example 5 using the
- * Gravity Service Binder. The Service Binder greatly simplifies creating OSGi
+ * Apache Felix Service Binder. The Service Binder greatly simplifies creating OSGi
  * applications by essentially eliminating the need to write OSGi-related code;
- * instead of writing OSGi code for you bundle, you create a simple XML file to
+ * instead of writing OSGi code for your bundle, you create a simple XML file to
  * describe your bundle's service dependencies. This class extends the generic
  * bundle activator; it does not provide any additional functionality. All
  * functionality for service-related tasks, such as look-up and binding, is
