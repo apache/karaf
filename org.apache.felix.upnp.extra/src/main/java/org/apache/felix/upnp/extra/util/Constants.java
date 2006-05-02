5761bf2daeea87e682d173d724c01481729c0ea9
Francesco Furfari <furfari@apache.org>
2006-05-02 19:16:40 +0000

    fixed bad license header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@399017 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.upnp.extra/src/main/java/org/apache/felix/upnp/extra/util/Constants.java
+++ org.apache.felix.upnp.extra/src/main/java/org/apache/felix/upnp/extra/util/Constants.java
@@ -1,23 +1,18 @@
 /*
- DomoWare UPnP Base Driver is an implementation of the OSGi UnP Device Spcifaction
- Copyright (C) 2004  Matteo Demuru, Francesco Furfari, Stefano "Kismet" Lenzi
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
-
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
- You can contact us at:
- {matte-d, sygent, kismet-sl} [at] users.sourceforge.net
+ *   Copyright 2006 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
  */
 package org.apache.felix.upnp.extra.util;
 /**
