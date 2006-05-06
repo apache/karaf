12fab7e4652426c8cd9c2b34d17f08ea9caee233
Karl Pauls <pauls@apache.org>
2006-05-06 11:59:40 +0000

    Initial implementation of a bundle mapping  UPnP Events to the  Event Admin (FELIX-68)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@400297 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.felix.eventadmin.bridge.upnp/src/main/java/org/apache/felix/eventadmin/bridge/upnp/Activator.java
@@ -0,0 +1,53 @@
+/*
+ *   Copyright 2005 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.felix.eventadmin.bridge.upnp;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The BundleActivator that will register an UPnPEventListener service with the 
+ * framework on start. Subsequently, UPnPEvents will be bridged to available 
+ * EventAdmin services (as per spec).
+ * 
+ * @author <a href="mailto:felix-dev@incubator.apache.org">Felix Project Team</a>
+ */
+public class Activator implements BundleActivator
+{
+    /**
+     * This registers an UPnPEventListener service with the framework that bridges 
+     * UPnPEvents to the EventAdmin.
+     * 
+     * @param context The context to use
+     * 
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    public void start(final BundleContext context) throws Exception
+    {
+        new UPnPEventToEventAdminBridge(context);
+    }
+
+    /**
+     * Stop the bridging of UPnPEvents to the EventAdmin.
+     * 
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(final BundleContext context) throws Exception
+    {
+        // Services are unregistered by the framework
+    }
+}
---
+++ org.apache.felix.eventadmin.bridge.upnp/src/main/java/org/apache/felix/eventadmin/bridge/upnp/UPnPEventToEventAdminBridge.java
@@ -0,0 +1,142 @@
+/*
+ *   Copyright 2005 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.felix.eventadmin.bridge.upnp;
+
+import java.util.Dictionary;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
+import org.osgi.service.upnp.UPnPEventListener;
+
+/**
+ * This class registers itself as an UPnPEventListener service with the 
+ * framework and subsequently, bridges UPnPEvents received to available EventAdmin 
+ * services. In order to track EventAdmin services this class registers itself as
+ * a ServiceListener for EventAdmin services too.
+ * 
+ * @author <a href="mailto:felix-dev@incubator.apache.org">Felix Project Team</a>
+ */
+public class UPnPEventToEventAdminBridge implements ServiceListener,
+    UPnPEventListener
+{
+    // The references to the EventAdmins additionally, used as a lock
+    private final Set m_refs = new HashSet();
+    
+    private final BundleContext m_context;
+    
+    /**
+     * Registers itself as an UPnPEventListener service with the given context and
+     * in order to track EventAdmin services as a ServicListener too.
+     * 
+     * @param context The context to register with.
+     */
+    public UPnPEventToEventAdminBridge(final BundleContext context)
+    {
+        synchronized(m_refs)
+        {
+            m_context = context;
+            
+            try {
+                m_context.addServiceListener(this, "(" + Constants.OBJECTCLASS + 
+                    "=" + EventAdmin.class.getName() + ")");
+                
+                final ServiceReference[] refs = m_context.getServiceReferences(
+                    EventAdmin.class.getName(), null);
+                
+                if(null != refs)
+                {
+                    for(int i = 0;i < refs.length;i++)
+                    {
+                        m_refs.add(refs[i]);
+                    }
+                }
+            } catch(InvalidSyntaxException e) {
+                 // This will never happen
+            }
+            
+            m_context.registerService(UPnPEventListener.class.getName(), this, null);
+        }
+    }
+    
+    /**
+     * Add newly registered service references.
+     * 
+     * @param event If event.getType equals REGISTERED the reference is added.
+     * 
+     * @see org.osgi.framework.ServiceListener#serviceChanged(org.osgi.framework.ServiceEvent)
+     */
+    public void serviceChanged(final ServiceEvent event)
+    {
+        synchronized (m_refs)
+        {
+            if(ServiceEvent.REGISTERED == event.getType())
+            {
+                m_refs.add(event.getServiceReference());
+            }
+        }
+    }
+
+    /**
+     * Bridge any event to all available EventAdmin services.
+     * 
+     * @param deviceId Bridged to <tt>upnp.deviceId</tt>
+     * @param serviceId Bridged to <tt>upnp.serviceId</tt>
+     * @param events Bridged to <tt>upnp.events</tt>
+     * 
+     * @see org.osgi.service.upnp.UPnPEventListener#notifyUPnPEvent(java.lang.String, java.lang.String, java.util.Dictionary)
+     */
+    public void notifyUPnPEvent(final String deviceId, final String serviceId, 
+        final Dictionary events)
+    {
+        synchronized (m_refs)
+        {
+            for (Iterator iter = m_refs.iterator(); iter.hasNext();)
+            {
+                final ServiceReference ref = (ServiceReference) iter.next();
+                
+                final EventAdmin eventAdmin = (EventAdmin) m_context.getService(ref);
+                
+                if(null != eventAdmin)
+                {
+                    eventAdmin.postEvent(new Event("org/osgi/service/upnp/UPnPEvent",
+                        new Hashtable(){{
+                            put("upnp.deviceId", deviceId);
+                            put("upnp.serviceId", serviceId);
+                            put("upnp.events", events);
+                        }}));
+// TODO: See 113.3.2 
+                    
+                    m_context.ungetService(ref);
+                }
+                else
+                {
+                    iter.remove();
+                }
+            }
+        }
+    }
+}
