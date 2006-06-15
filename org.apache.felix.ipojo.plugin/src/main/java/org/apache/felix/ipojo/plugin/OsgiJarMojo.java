126a639f9aee541cee3d57a5f023055743aabf4e
Richard S. Hall <rickhall@apache.org>
2006-06-15 10:11:26 +0000

    Added missing copyright/license headers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@414533 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.ipojo.plugin/src/main/java/org/apache/felix/ipojo/plugin/OsgiJarMojo.java
+++ org.apache.felix.ipojo.plugin/src/main/java/org/apache/felix/ipojo/plugin/OsgiJarMojo.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2005 The Apache Software Foundation
+ *   Copyright 2006 The Apache Software Foundation
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
  *   limitations under the License.
  *
  */
-
 package org.apache.felix.ipojo.plugin;
 
 //import java.io.BufferedReader;
