8fd85d666299876f20936922851d3ed3b0c3a9a4
Stephane Frenot <sfrenot@apache.org>
2006-08-29 14:49:31 +0000

    Starting mosgi documentation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@438084 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mosgi.doc/core.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+java -Dfelix.config.properties=file:../mosgi.doc/config.properties.core -jar bin/felix.jar
+
---
+++ mosgi.doc/jmxconsole.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -Dfelix.config.properties=file:../mosgi.doc/config.properties.jmxconsole -jar bin/felix.jar
