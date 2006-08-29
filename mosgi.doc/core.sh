5c363889289afaf249e9ae00039b1b8bf56579af
Stephane Frenot <sfrenot@apache.org>
2006-08-29 15:05:23 +0000

    A little error in the scripts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@438088 13f79535-47bb-0310-9956-ffa450edef68

--- mosgi.doc/core.sh
+++ mosgi.doc/core.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "cd into felix directory"
-cd ../org.apache.felix.main
+cd ../main
 
 echo "lauching felix with core gateway properties"
 java -Dfelix.config.properties=file:../mosgi.doc/config.properties.core -jar bin/felix.jar
