8996c5cb2f2440d0a0afdf6ffaf3c2252949fce2
Stephane Frenot <sfrenot@apache.org>
2006-09-19 12:12:50 +0000

    FELIX-144 -------- Removing notices for jmx agent
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@447826 13f79535-47bb-0310-9956-ffa450edef68

--- mosgi.jmx.agent/src/main/java/org/apache/felix/mosgi/jmx/agent/AgentActivator.java
+++ mosgi.jmx.agent/src/main/java/org/apache/felix/mosgi/jmx/agent/AgentActivator.java
@@ -1,39 +1,21 @@
 /*
- * Copyright (C) 2003 Stéphane Frénot
- * Copyright (C) 2003 INSA Lyon Laboratoire CITI, France
- * Copyright (C) 2003 INRIA projet ARES, France
- *
- * This program is licensed under the Apache Software License
- * version 1.1; refer to the ASL-LICENSE.txt file included with
- * this program for details.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Contact: Stéphane Frénot stephane.frenot@insa-lyon.fr
- * Contributor(s): Didier Donsez, didier.donsez@imag.fr
- * 
- * $Id: AgentActivator.java,v 1.6 2005/09/12 09:52:13 sfrenot Exp $
- *
- * TODO : profile should only call bc.getProperty
-**/
-/*
- *   Copyright 2005 The Apache Software Foundation
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+* 
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.    
+*/
 package org.apache.felix.mosgi.jmx.agent;
 
 import java.util.StringTokenizer;
