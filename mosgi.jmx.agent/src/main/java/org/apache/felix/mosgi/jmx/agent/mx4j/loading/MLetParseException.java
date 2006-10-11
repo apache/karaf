cd27cc6c159c41a09a43c095a3ac2a6229308691
Stephane Frenot <sfrenot@apache.org>
2006-10-11 08:25:06 +0000

    FELIX-147 ---------
    Adding a notice for MX4J included code, modified mosgi.jmx.* projects
    to include MX4J licence file. Corrected sources to include apache
    headers.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@462730 13f79535-47bb-0310-9956-ffa450edef68

--- mosgi.jmx.agent/src/main/java/org/apache/felix/mosgi/jmx/agent/mx4j/loading/MLetParseException.java
+++ mosgi.jmx.agent/src/main/java/org/apache/felix/mosgi/jmx/agent/mx4j/loading/MLetParseException.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (C) MX4J.
- * All rights reserved.
- *
- * This software is distributed under the terms of the MX4J License version 1.0.
- * See the terms of the MX4J License in the documentation provided with this software.
- */
-
 /* 
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
