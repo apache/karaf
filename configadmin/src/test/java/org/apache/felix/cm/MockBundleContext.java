642b48ef737916f10626d7d214403c537e25990f
Felix Meschberger <fmeschbe@apache.org>
2007-04-11 18:14:28 +0000

    Fix wrong license headers
    git-svn-id: https://svn.apache.org/repos/asf/incubator/felix/trunk@527593 13f79535-47bb-0310-9956-ffa450edef68

--- configadmin/src/test/java/org/apache/felix/cm/MockBundleContext.java
+++ configadmin/src/test/java/org/apache/felix/cm/MockBundleContext.java
@@ -1,16 +1,20 @@
-/*
- * $Url: $
- * $Id$
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Copyright 1997-2005 Day Management AG
- * Barfuesserplatz 6, 4001 Basel, Switzerland
- * All Rights Reserved.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * This software is the confidential and proprietary information of
- * Day Management AG, ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Day.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.felix.cm;
 
@@ -33,7 +37,6 @@ import org.osgi.framework.ServiceRegistration;
  * The <code>MockBundleContext</code> TODO
  *
  * @author fmeschbe
- * @version $Rev:$, $Date:$
  */
 public class MockBundleContext implements BundleContext
 {
