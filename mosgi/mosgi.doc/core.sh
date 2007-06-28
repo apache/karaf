5095408bf80547b4d691edfcd6fd6527f2039863
Stephane Frenot <sfrenot@apache.org>
2007-06-28 14:37:13 +0000

    FELIX-307 --------- Moving all poms to maven-bundle-plugin
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@551579 13f79535-47bb-0310-9956-ffa450edef68

--- mosgi/mosgi.doc/core.sh
+++ mosgi/mosgi.doc/core.sh
@@ -2,8 +2,8 @@
 
 
 echo "cd into felix directory"
-cd ../main
+cd ../../main
 
 echo "lauching felix with core gateway properties"
-java -Dfelix.config.properties=file:../mosgi.doc/config.properties.core -jar bin/felix.jar
+java -Dfelix.config.properties=file:../mosgi/mosgi.doc/config.properties.core -jar bin/felix.jar
 
--- mosgi/mosgi.doc/jmxconsole.sh
+++ mosgi/mosgi.doc/jmxconsole.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "cd into felix directory"
-cd ../main
+cd ../../main
 
 echo "lauching felix with jmxconsole properties"
-java -Dfelix.config.properties=file:../mosgi.doc/config.properties.jmxconsole -jar bin/felix.jar
+java -Dfelix.config.properties=file:../mosgi/mosgi.doc/config.properties.jmxconsole -jar bin/felix.jar
