7830f6dd92cb2cd64f146796a80f4b2eb216273c
Stefano Lenzi <lenzi@apache.org>
2007-08-25 23:50:23 +0000

    Removed useless prefix from all the project related to UPnP
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@569746 13f79535-47bb-0310-9956-ffa450edef68

--- upnp/doc/core.sh.bat
+++ upnp/doc/core.sh.bat
@@ -5,5 +5,5 @@ echo "cd into felix directory"
 cd ../../main
 
 echo "lauching felix with core gateway properties"
-java -Dfelix.config.properties=file:../upnp/upnp.doc/config.properties.core -jar bin/felix.jar
+java -Dfelix.config.properties=file:../upnp/doc/config.properties.core -jar bin/felix.jar
 
--- upnp/doc/jmxconsole.sh.bat
+++ upnp/doc/jmxconsole.sh.bat
@@ -3,4 +3,4 @@ echo "cd into felix directory"
 cd ../../main
 
 echo "lauching felix with jmxconsole properties"
-java -Dfelix.config.properties=file:../upnp.doc/config.properties.jmxconsole -jar bin/felix.jar
+java -Dfelix.config.properties=file:../upnp/doc/config.properties.jmxconsole -jar bin/felix.jar
