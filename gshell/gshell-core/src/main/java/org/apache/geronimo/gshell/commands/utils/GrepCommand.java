eb6db9c53e5b31650cd543ddcaa363734cd68404
Guillaume Nodet <gnodet@apache.org>
2008-03-19 16:29:58 +0000

    SMX4KNL-24: Add a few utils commands: cat, java, exec, sleep
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/kernel/trunk@638925 13f79535-47bb-0310-9956-ffa450edef68

--- gshell/gshell-core/src/main/java/org/apache/geronimo/gshell/commands/utils/GrepCommand.java
+++ gshell/gshell-core/src/main/java/org/apache/geronimo/gshell/commands/utils/GrepCommand.java
@@ -8,23 +8,54 @@ import org.apache.geronimo.gshell.clp.Argument;
 import org.apache.geronimo.gshell.clp.Option;
 import org.apache.geronimo.gshell.command.annotation.CommandComponent;
 import org.apache.geronimo.gshell.support.OsgiCommandSupport;
+import org.codehaus.plexus.util.StringUtils;
 
-@CommandComponent(id="utils:grep", description="Grep")
+@CommandComponent(id="utils:grep", description="Print lines matching a pattern")
 public class GrepCommand extends OsgiCommandSupport {
 
     @Argument(required=true, description="Regular expression")
     private String regex;
 
-    @Option(name = "-v", description = "Inverse matching")
-    private boolean inverse;
+    @Option(name="-n", aliases = { "--line-number" }, description="Prefix each line of output with the line number within its input file.")
+    private boolean lineNumber;
+
+    @Option(name = "-v", aliases = { "--invert-match" }, description = "Invert the sense of matching, to select non-matching lines.")
+    private boolean invertMatch;
+
+    @Option(name = "-w", aliases = { "--word-regexp" }, description = "Select only those lines containing matches that form whole " +
+                                                                      "words.  The test is that the matching substring must either be " +
+                                                                      "at  the beginning of the line, or preceded by a non-word constituent " +
+                                                                      "character.  Similarly, it must be either at the end of " +
+                                                                      "the line or followed by a non-word constituent character.  " +
+                                                                      "Word-constituent characters are letters, digits, and the underscore.")
+    private boolean wordRegexp;
+
+    @Option(name = "-x", aliases = { "--line-regexp" }, description = "Select only those matches that exactly match the whole line.")
+    private boolean lineRegexp;
 
     protected Object doExecute() throws Exception {
-        Pattern p = Pattern.compile(regex);
+        String regexp = regex;
+        if (wordRegexp) {
+            regexp = "\\b" + regexp + "\\b";
+        }
+        if (lineRegexp) {
+            regexp = "^" + regexp + "$";
+        } else {
+            regexp = ".*" + regexp + ".*";
+        }
+        Pattern p = Pattern.compile(regexp);
         try {
-            while (true) {
-                String line = readLine(io.in);
-                if (p.matcher(line).matches() ^ inverse) {
+            int lineno = 1;
+            String line;
+            while ((line = readLine(io.in)) != null) {
+                if (p.matcher(line).matches() ^ invertMatch) {
+                    if (lineNumber) {
+                        String gutter = StringUtils.leftPad(String.valueOf(lineno), 6);
+                        io.out.print(gutter);
+                        io.out.print("  ");
+                    }
                     io.out.println(line);
+                    lineno++;
                 }
             }
         } catch (IOException e) {
