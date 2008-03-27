e31f1614cff6d138b5a2fe92a2c114671446eed7
Guillaume Nodet <gnodet@apache.org>
2008-03-27 17:12:05 +0000

    Allow the features:refreshUrl to reload everything
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/kernel/trunk@641904 13f79535-47bb-0310-9956-ffa450edef68

--- gshell/gshell-features/src/main/java/org/apache/servicemix/gshell/features/internal/commands/RefreshUrlCommand.java
+++ gshell/gshell-features/src/main/java/org/apache/servicemix/gshell/features/internal/commands/RefreshUrlCommand.java
@@ -16,20 +16,28 @@
  */
 package org.apache.servicemix.gshell.features.internal.commands;
 
-import java.util.List;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.geronimo.gshell.clp.Argument;
 import org.apache.geronimo.gshell.command.annotation.CommandComponent;
 import org.apache.servicemix.gshell.features.FeaturesService;
+import org.apache.servicemix.gshell.features.Repository;
 
 @CommandComponent(id="features:refreshUrl", description="Reload the repositories to obtain a fresh list of features")
 public class RefreshUrlCommand extends FeaturesCommandSupport {
 
-    @Argument(required = true, multiValued = true, description = "Repository URLs")
+    @Argument(required = false, multiValued = true, description = "Repository URLs (leave empty for all)")
     List<String> urls;
 
     protected void doExecute(FeaturesService admin) throws Exception {
+        if (urls == null || urls.isEmpty()) {
+            urls = new ArrayList<String>();
+            for (Repository repo : admin.listRepositories()) {
+                urls.add(repo.getURL().toString());
+            }
+        }
         for (String strUrl : urls) {
             URL url = new URL(strUrl);
             admin.removeRepository(url);
