2f38d7f266f7c7cc1a6cc0bbfc22520f22396721
Felix Meschberger <fmeschbe@apache.org>
2008-06-16 11:16:38 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@668111 13f79535-47bb-0310-9956-ffa450edef68

--- webconsole/src/main/java/org/apache/felix/webconsole/internal/servlet/RenderBridge.java
+++ webconsole/src/main/java/org/apache/felix/webconsole/internal/servlet/RenderBridge.java
@@ -20,8 +20,6 @@ package org.apache.felix.webconsole.internal.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
