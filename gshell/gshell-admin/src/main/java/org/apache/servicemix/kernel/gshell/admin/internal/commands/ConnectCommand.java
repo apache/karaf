3e0c625264df6b528d18507d39621c4ec621237e
Guillaume Nodet <gnodet@apache.org>
2009-03-05 15:34:48 +0000

    Fix the admin/connect command
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/kernel/trunk@750476 13f79535-47bb-0310-9956-ffa450edef68

--- gshell/gshell-admin/src/main/java/org/apache/servicemix/kernel/gshell/admin/internal/commands/ConnectCommand.java
+++ gshell/gshell-admin/src/main/java/org/apache/servicemix/kernel/gshell/admin/internal/commands/ConnectCommand.java
@@ -37,7 +37,7 @@ public class ConnectCommand extends AdminCommandSupport {
 
     protected Object doExecute() throws Exception {
         int port = getExistingInstance(instance).getPort();
-        ShellContextHolder.get().getShell().execute("remote/ssh -l " + username + " -P " + password + " -p " + port + " localhost");
+        ShellContextHolder.get().getShell().execute("ssh -l " + username + " -P " + password + " -p " + port + " localhost");
         return Result.SUCCESS;
     }
 }
