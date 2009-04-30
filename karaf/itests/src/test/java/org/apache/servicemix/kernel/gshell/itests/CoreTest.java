cbb5cb58ce09e155b805c5dc56339a1e73e4c773
Guillaume Nodet <gnodet@apache.org>
2009-04-30 07:04:50 +0000

    FELIX-1099: finish the switch to pax-exam
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@770093 13f79535-47bb-0310-9956-ffa450edef68

--- karaf/itests/src/test/java/org/apache/servicemix/kernel/gshell/itests/CoreTest.java
+++ karaf/itests/src/test/java/org/apache/servicemix/kernel/gshell/itests/CoreTest.java
@@ -24,17 +24,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.ops4j.pax.exam.CoreOptions.felix;
-import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.systemPackages;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
-import static org.ops4j.pax.exam.CoreOptions.bootClasspathLibrary;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.OptionUtils;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.configProfile;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.logProfile;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.profile;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.Bundle;
@@ -65,7 +55,7 @@ public class CoreTest extends AbstractIntegrationTest{
 
         shell.execute("log/display");
 
-        b.uninstall();
+        b.stop();
 
         try {
             shell.execute("log/display");
@@ -84,86 +74,19 @@ public class CoreTest extends AbstractIntegrationTest{
         shell.execute("..");
     }
     
-//    @Test
-//    public void testInstallFeature() throws Exception {
-//        Shell shell = getOsgiService(Shell.class);
-//
-//        try {
-//            shell.execute("obr");
-//            fail("command should not exist");
-//        } catch (CommandLineExecutionFailed e) {
-//            assertNotNull(e.getCause());
-//            assertTrue(e.getCause() instanceof NoSuchCommandException);
-//        }
-//        try {
-//            shell.execute("wrapper");
-//            fail("command should not exist");
-//        } catch (CommandLineExecutionFailed e) {
-//            assertNotNull(e.getCause());
-//            assertTrue(e.getCause() instanceof NoSuchCommandException);
-//        }
-//        String url = getClass().getClassLoader().getResource("features.xml").toString();
-//        addFeatureRepo(url);
-//        installFeature("obr");
-//        installFeature("wrapper");
-//        shell.execute("obr");
-//        shell.execute("wrapper");
-//    }
-
-    /**
-     * TODO: This test seems to fail, there must be a timing issue somewhere
-     *
+    @Test
     public void testCommandGroupAfterInstall() throws Exception {
-        Bundle b = installBundle("org.apache.servicemix.kernel.gshell", "org.apache.servicemix.kernel.gshell.log", null, "jar");
+        Bundle b = getInstalledBundle("org.apache.servicemix.kernel.gshell.log");
+        b.start();
         Shell shell = getOsgiService(Shell.class);
         shell.execute("log");
         shell.execute("help");
         shell.execute("..");
     }
-     */
 
     @Configuration
     public static Option[] configuration() {
-        Option[] options = options(
-            // install log service using pax runners profile abstraction (there are more profiles, like DS)
-            logProfile(),
-            configProfile(),
-            profile("spring.dm").version("1.2.0-rc1"),
-
-            // this is how you set the default log level when using pax logging (logProfile)
-            systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
-            systemProperty("servicemix.name").value("root"),
-            systemProperty("servicemix.base").value("target/smx.base"),
-            systemProperty("servicemix.startLocalConsole").value("false"),
-            systemProperty("servicemix.startRemoteShell").value("false"),
-
-            // hack system packages
-            systemPackages("org.apache.servicemix.kernel.main.spi;version=1.0.0", "org.apache.servicemix.kernel.jaas.boot"),
-            bootClasspathLibrary(mavenBundle("org.apache.servicemix.kernel.jaas", "org.apache.servicemix.kernel.jaas.boot")).afterFramework(),
-            bootClasspathLibrary(mavenBundle("org.apache.servicemix.kernel", "org.apache.servicemix.kernel.main")).afterFramework(),
-
-            // Bundles
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.jline"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-httpclient"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-jexl"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-vfs"),
-            mavenBundle("org.apache.mina", "mina-core"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.oro"),
-            mavenBundle("org.apache.servicemix.kernel.jaas", "org.apache.servicemix.kernel.jaas.config"),
-            mavenBundle("org.apache.sshd", "sshd-core"),
-            mavenBundle("org.apache.servicemix.kernel.gshell", "org.apache.servicemix.kernel.gshell.core"),
-            mavenBundle("org.apache.servicemix.kernel.gshell", "org.apache.servicemix.kernel.gshell.osgi"),
-            mavenBundle("org.apache.servicemix.kernel.gshell", "org.apache.servicemix.kernel.gshell.log").noStart(),
-
-            felix()
-        );
-
-        // use config generated by the Maven plugin (until PAXEXAM-62/64 get resolved)
-        if (CoreTest.class.getClassLoader().getResource("META-INF/maven/paxexam-config.args") != null) {
-            options = OptionUtils.combine(options, mavenConfiguration());
-        }
-
-        return options;
+        return AbstractIntegrationTest.configuration();
     }
 
 }
