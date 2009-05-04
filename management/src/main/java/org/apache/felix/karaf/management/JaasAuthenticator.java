8b48d985f38da9510aed850e943837689ddd7ab5
Guillaume Nodet <gnodet@apache.org>
2009-05-04 06:59:07 +0000

    FELIX-1098: Remove some spring dependencies
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/karaf@771206 13f79535-47bb-0310-9956-ffa450edef68

--- management/src/main/java/org/apache/felix/karaf/management/JaasAuthenticator.java
+++ management/src/main/java/org/apache/felix/karaf/management/JaasAuthenticator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.servicemix.management;
+package org.apache.felix.karaf.management;
 
 import java.io.IOException;
 
