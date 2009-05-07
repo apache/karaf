8182780c8b2ba024e07adfab06e02260627ab181
Guillaume Nodet <gnodet@apache.org>
2009-05-07 13:19:47 +0000

    Fix problem with exported commands not being available from the console, fix integration tests
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/karaf@772643 13f79535-47bb-0310-9956-ffa450edef68

--- itests/src/test/java/org/apache/felix/karaf/gshell/itests/AbstractIntegrationTest.java
+++ itests/src/test/java/org/apache/felix/karaf/gshell/itests/AbstractIntegrationTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.felix.karaf.gshell.itests;
 
+import java.util.Dictionary;
+
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Inject;
 import org.ops4j.pax.exam.Option;
@@ -39,6 +41,7 @@ import org.osgi.framework.Constants;
 import org.osgi.framework.Filter;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
 
 public abstract class AbstractIntegrationTest {
@@ -71,11 +74,23 @@ public abstract class AbstractIntegrationTest {
             }
             Filter osgiFilter = FrameworkUtil.createFilter(flt);
             tracker = new ServiceTracker(bundleContext, osgiFilter, null);
-            tracker.open();
+            tracker.open(true);
             // Note that the tracker is not closed to keep the reference
-            // This is buggy, has the service reference may change i think
+            // This is buggy, as the service reference may change i think
             Object svc = type.cast(tracker.waitForService(timeout));
             if (svc == null) {
+
+                Dictionary dic = bundleContext.getBundle().getHeaders();
+                System.err.println("Test bundle headers: " + dic);
+                ServiceReference[] refs = bundleContext.getAllServiceReferences(null, null);
+                for (ServiceReference ref : refs) {
+                    System.err.println("ServiceReference: " + ref);
+                }
+                refs = bundleContext.getAllServiceReferences(null, flt);
+                for (ServiceReference ref : refs) {
+                    System.err.println("Filtered ServiceReference: " + ref);
+                }
+
                 throw new RuntimeException("Gave up waiting for service " + flt);
             }
             return type.cast(svc);
@@ -102,52 +117,6 @@ public abstract class AbstractIntegrationTest {
 
     public static MavenArtifactProvisionOption mavenBundle(String groupId, String artifactId) {
         return CoreOptions.mavenBundle().groupId(groupId).artifactId(artifactId).versionAsInProject();
-    }
-
-    @Configuration
-    public static Option[] configuration() {
-        Option[] options = options(
-            // install log service using pax runners profile abstraction (there are more profiles, like DS)
-            profile("log").version("1.3.0"),
-            profile("felix.config").version("1.0.10"),
-            profile("spring.dm").version("1.2.0-rc1"),
-
-            // this is how you set the default log level when using pax logging (logProfile)
-            systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
-            systemProperty("karaf.name").value("root"),
-            systemProperty("karaf.base").value("target/smx.base"),
-            systemProperty("karaf.startLocalConsole").value("false"),
-            systemProperty("karaf.startRemoteShell").value("false"),
-
-            // hack system packages
-            systemPackages("org.apache.felix.karaf.main.spi;version=1.0.0", "org.apache.felix.karaf.jaas.boot"),
-            bootClasspathLibrary(mavenBundle("org.apache.felix.karaf.jaas", "org.apache.felix.karaf.jaas.boot")).afterFramework(),
-            bootClasspathLibrary(mavenBundle("org.apache.felix.karaf", "org.apache.felix.karaf.main")).afterFramework(),
-
-            // Bundles
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.jline"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-httpclient"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-jexl"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-vfs"),
-            mavenBundle("org.apache.mina", "mina-core"),
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.oro"),
-            mavenBundle("org.apache.felix.karaf.jaas", "org.apache.felix.karaf.jaas.config"),
-            mavenBundle("org.apache.sshd", "sshd-core"),
-            mavenBundle("org.apache.felix.karaf.gshell", "org.apache.felix.karaf.gshell.core"),
-            mavenBundle("org.apache.felix.karaf.gshell", "org.apache.felix.karaf.gshell.osgi"),
-            mavenBundle("org.apache.felix.karaf.gshell", "org.apache.felix.karaf.gshell.log").noStart(),
-
-
-//            mavenBundle("org.apache.felix", "org.apache.felix.bundlerepository"),
-
-            scanFeatures(
-                    maven().groupId("org.apache.felix.karaf").artifactId("apache-felix-karaf").type("xml").classifier("features").versionAsInProject(),
-                    "obr", "wrapper"
-            ),
-
-            felix()
-        );
-        return options;
     }
 
 }
--- itests/src/test/java/org/apache/felix/karaf/gshell/itests/FeaturesTest.java
+++ itests/src/test/java/org/apache/felix/karaf/gshell/itests/FeaturesTest.java
@@ -16,10 +16,25 @@
  */
 package org.apache.felix.karaf.gshell.itests;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.geronimo.gshell.shell.Shell;
+import org.apache.geronimo.gshell.command.Command;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.waitForRBCFor;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.systemPackages;
+import static org.ops4j.pax.exam.CoreOptions.bootClasspathLibrary;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.maven;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 
@@ -28,14 +43,69 @@ public class FeaturesTest extends AbstractIntegrationTest {
 
     @Test
     public void testFeatures() throws Exception {
+        Thread.sleep(5000);
+
         Shell shell = getOsgiService(Shell.class);
-        shell.execute("obr");
-        shell.execute("wrapper");
+        shell.execute("obr/listUrl");
+        shell.execute("wrapper/install --help");
     }
 
     @Configuration
     public static Option[] configuration() {
-        return AbstractIntegrationTest.configuration();
+        Option[] options = options(
+            // this is how you set the default log level when using pax logging (logProfile)
+            systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("DEBUG"),
+            systemProperty("karaf.name").value("root"),
+            systemProperty("karaf.home").value("target/karaf.home"),
+            systemProperty("karaf.base").value("target/karaf.home"),
+            systemProperty("karaf.startLocalConsole").value("false"),
+            systemProperty("karaf.startRemoteShell").value("false"),
+
+            // hack system packages
+            systemPackages("org.apache.felix.karaf.main.spi;version=1.0.0", "org.apache.felix.karaf.jaas.boot;version=1.2.0"),
+            bootClasspathLibrary(mavenBundle("org.apache.felix.karaf.jaas", "org.apache.felix.karaf.jaas.boot")).afterFramework(),
+            bootClasspathLibrary(mavenBundle("org.apache.felix.karaf", "org.apache.felix.karaf.main")).afterFramework(),
+
+            // Log
+            mavenBundle("org.ops4j.pax.logging", "pax-logging-api"),
+            mavenBundle("org.ops4j.pax.logging", "pax-logging-service"),
+            // Felix Config Admin
+            mavenBundle("org.apache.felix", "org.apache.felix.configadmin"),
+            // Felix Preferences Service
+            mavenBundle("org.apache.felix", "org.apache.felix.prefs"),
+            // Spring-DM
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.aopalliance"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.cglib"),
+            mavenBundle("org.springframework", "spring-aop"),
+            mavenBundle("org.springframework", "spring-beans"),
+            mavenBundle("org.springframework", "spring-context"),
+            mavenBundle("org.springframework", "spring-core"),
+            mavenBundle("org.springframework.osgi", "spring-osgi-core"),
+            mavenBundle("org.springframework.osgi", "spring-osgi-extender"),
+            mavenBundle("org.springframework.osgi", "spring-osgi-io"),
+
+            // Bundles
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.jline"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-httpclient"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-jexl"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-vfs"),
+            mavenBundle("org.apache.mina", "mina-core"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.oro"),
+            mavenBundle("org.apache.felix.karaf.jaas", "org.apache.felix.karaf.jaas.config"),
+            mavenBundle("org.apache.sshd", "sshd-core"),
+            mavenBundle("org.apache.felix.karaf.gshell", "org.apache.felix.karaf.gshell.core"),
+            mavenBundle("org.apache.felix.karaf.gshell", "org.apache.felix.karaf.gshell.run"),
+            mavenBundle("org.apache.felix.karaf.gshell", "org.apache.felix.karaf.gshell.osgi"),
+            mavenBundle("org.apache.felix.karaf.gshell", "org.apache.felix.karaf.gshell.log").noStart(),
+
+            scanFeatures(
+                    maven().groupId("org.apache.felix.karaf").artifactId("apache-felix-karaf").type("xml").classifier("features").versionAsInProject(),
+                    "obr", "wrapper"
+            ),
+
+            felix()
+        );
+        return options;
     }
 
 }
