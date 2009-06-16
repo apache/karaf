abed2af1d677cfee5876a7b8199f69a5dc25f909
Gert Vanthienen <gertv@apache.org>
2009-06-16 11:58:10 +0000

    Update to felix-parent 1.2.0 and use EasyMock instead of JMock
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@785170 13f79535-47bb-0310-9956-ffa450edef68

--- karaf/tooling/features-maven-plugin/src/test/java/org/apache/felix/karaf/tooling/features/GenerateFeaturesFileMojoTest.java
+++ karaf/tooling/features-maven-plugin/src/test/java/org/apache/felix/karaf/tooling/features/GenerateFeaturesFileMojoTest.java
@@ -17,6 +17,9 @@
  */
 package org.apache.felix.karaf.tooling.features;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,57 +29,50 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.metadata.ArtifactMetadataRetrievalException;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
-import org.jmock.Expectations;
-import org.jmock.Mockery;
+import org.easymock.EasyMock;
 
 /**
  * Test cases for {@link GenerateFeaturesFileMojo} 
  */
 public class GenerateFeaturesFileMojoTest extends TestCase {
     
-    private Mockery mockery;
     private GenerateFeaturesFileMojo mojo;
     
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mojo = new GenerateFeaturesFileMojo();
-        mockery = new Mockery();
     }
     
     public void testGetBestVersionForArtifactWithOneVersion() throws Exception {       
-        final Artifact artifact = mockery.mock(Artifact.class);
-        mockery.checking(new Expectations() {{
-            allowing(artifact).getVersion(); will(returnValue("2.1.6"));
-        }});
+        final Artifact artifact = EasyMock.createMock(Artifact.class);
+        expect(artifact.getVersion()).andReturn("2.1.6");
+        replay(artifact);
 
         assertEquals("2.1.6_1-SNAPSHOT", mojo.getBestVersionForArtifact(artifact, createVersionList("2.1.6_1-SNAPSHOT")));
     }
     
     public void testGetBestVersionForArtifactWithTwoVersions() throws Exception {       
-        final Artifact artifact = mockery.mock(Artifact.class);
-        mockery.checking(new Expectations() {{
-            allowing(artifact).getVersion(); will(returnValue("2.1.6"));
-        }});
+        final Artifact artifact = EasyMock.createMock(Artifact.class);
+        expect(artifact.getVersion()).andReturn("2.1.6");
+        replay(artifact);
 
         assertEquals("2.1.6_2-SNAPSHOT", mojo.getBestVersionForArtifact(artifact, createVersionList("2.1.6_1-SNAPSHOT", "2.1.6_2-SNAPSHOT")));
     }
     
     public void testGetBestVersionForArtifactWithSameMajorMinor() throws Exception {       
-        final Artifact artifact = mockery.mock(Artifact.class);
-        mockery.checking(new Expectations() {{
-            allowing(artifact).getVersion(); will(returnValue("3.8.2"));
-        }});
+        final Artifact artifact = EasyMock.createMock(Artifact.class);
+        expect(artifact.getVersion()).andReturn("3.8.2").anyTimes();
+        replay(artifact);        
 
         assertEquals("3.8.1_1-SNAPSHOT", mojo.getBestVersionForArtifact(artifact, createVersionList("3.8.1_1-SNAPSHOT")));
     }
 
     
     public void testGetBestVersionForArtifactWithOneNonMatchingVersion() throws Exception {
-        final Artifact artifact = mockery.mock(Artifact.class);
-        mockery.checking(new Expectations() {{
-            allowing(artifact).getVersion(); will(returnValue("9.1.0.1"));
-        }});
+        final Artifact artifact = EasyMock.createMock(Artifact.class);
+        expect(artifact.getVersion()).andReturn("9.1.0.1").anyTimes();
+        replay(artifact);        
         
         try {
             mojo.getBestVersionForArtifact(artifact, createVersionList("9.0_1-SNAPSHOT"));
@@ -88,7 +84,7 @@ public class GenerateFeaturesFileMojoTest extends TestCase {
     
     public void testGetBestVersionForArtifactWithNoVersions() throws Exception {
         try {
-            mojo.getBestVersionForArtifact(mockery.mock(Artifact.class), createVersionList());
+            mojo.getBestVersionForArtifact(EasyMock.createMock(Artifact.class), createVersionList());
             fail("ArtifactMetadataRetrievalException should have been thrown if no matching version was found");
         } catch (ArtifactMetadataRetrievalException e) {
             // this is expected
