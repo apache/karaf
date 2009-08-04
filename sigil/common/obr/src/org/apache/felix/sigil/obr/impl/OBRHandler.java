f1612cbc3c4ee08ec14b7a8dcac7f12265cc868b
David Morgan Spenser Savage <dsavage@apache.org>
2009-08-04 18:36:17 +0000

    tidy up unused import
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@800916 13f79535-47bb-0310-9956-ffa450edef68

--- sigil/common/obr/src/org/apache/felix/sigil/obr/impl/OBRHandler.java
+++ sigil/common/obr/src/org/apache/felix/sigil/obr/impl/OBRHandler.java
@@ -27,7 +27,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.felix.sigil.model.ModelElementFactory;
