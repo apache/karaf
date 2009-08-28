7c80c1741e20a0672c9bd5fa33d8bf778e47ade2
Clement Escoffier <clement@apache.org>
2009-08-28 08:20:47 +0000

    Add tests about logger configuration.
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@808809 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ipojo/tests/core/logger/src/test/java/org/apache/felix/ipojo/tests/core/component/MyComponent.java
@@ -0,0 +1,11 @@
+package org.apache.felix.ipojo.tests.core.component;
+
+import org.apache.felix.ipojo.tests.core.service.MyService;
+
+public class MyComponent implements MyService {
+
+    public void foo() {
+        // Nothing to do.
+    }
+
+}
