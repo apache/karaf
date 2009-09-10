5b802907778c777d77053d548f4a42802190c7a8
Guillaume Nodet <gnodet@apache.org>
2009-09-10 16:34:24 +0000

    Make sure we use the same AFS header everywhere
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@813503 13f79535-47bb-0310-9956-ffa450edef68

--- gogo/console/src/main/java/org/apache/felix/gogo/console/stdio/Console.java
+++ gogo/console/src/main/java/org/apache/felix/gogo/console/stdio/Console.java
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.felix.gogo.console.stdio;
 
 import org.osgi.service.command.CommandSession;
