8807b7ec31b2d17d24996bc5cf931649bb3e1fa9
Guillaume Nodet <gnodet@apache.org>
2009-10-07 11:05:14 +0000

    FELIX-1660: karaf should not hardcode the 'system' location of its maven-like repository, patch provided by Richard Stone
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@822663 13f79535-47bb-0310-9956-ffa450edef68

--- karaf/main/src/main/java/org/apache/felix/karaf/main/Bootstrap.java
+++ karaf/main/src/main/java/org/apache/felix/karaf/main/Bootstrap.java
@@ -29,13 +29,8 @@ import java.lang.reflect.Method;
 
 public class Bootstrap {
 
-    public static final String FRAMEWORK_PROPERTIES_FILE_NAME = "config.properties";
-
-    public static final String KARAF_FRAMEWORK = "karaf.framework";
-
     public static void main(String[] args) {
         try {
-            updateClassLoader();
             Main.main(args);
         } catch (Throwable t) {
             t.printStackTrace();
@@ -44,46 +39,9 @@ public class Bootstrap {
     }
 
     public static Main launch(String[] args) throws Exception {
-        updateClassLoader();
         Main main = new Main(args);
         main.launch();
         return main;
     }
 
-    private static void updateClassLoader() throws Exception {
-        File home = Utils.getKarafHome();
-        File base = Utils.getKarafBase(home);
-        File file = new File(new File(base, "etc"), FRAMEWORK_PROPERTIES_FILE_NAME);
-        if (!file.exists()) {
-            file = new File(new File(home, "etc"), FRAMEWORK_PROPERTIES_FILE_NAME);
-        }
-        if (!file.exists()) {
-            throw new FileNotFoundException(file.getAbsolutePath());
-        }
-        Properties props = new Properties();
-        InputStream is = new FileInputStream(file);
-        props.load(is);
-        is.close();
-
-        String framework = props.getProperty(KARAF_FRAMEWORK);
-        if (framework == null) {
-            throw new IllegalArgumentException("Property " + KARAF_FRAMEWORK + " must be set in the etc/" + FRAMEWORK_PROPERTIES_FILE_NAME + " configuration file");
-        }
-        String bundle = props.getProperty(KARAF_FRAMEWORK + "." + framework);
-        if (bundle == null) {
-            throw new IllegalArgumentException("Property " + KARAF_FRAMEWORK + "." + framework + " must be set in the etc/" + FRAMEWORK_PROPERTIES_FILE_NAME + " configuration file");
-        }
-        File bundleFile = new File(base, bundle);
-        if (!bundleFile.exists()) {
-            bundleFile = new File(home, bundle);
-        }
-        if (!bundleFile.exists()) {
-            throw new FileNotFoundException(bundleFile.getAbsolutePath());
-        }
-
-        URLClassLoader classLoader = (URLClassLoader) Bootstrap.class.getClassLoader();
-        Method mth = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-        mth.setAccessible(true);
-        mth.invoke(classLoader, bundleFile.toURL());
-    }
 }
