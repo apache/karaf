40c25367adc5af3eb2872cb4a22a3a5ec8365189
Richard S. Hall <rickhall@apache.org>
2010-05-05 21:06:19 +0000

    Add some basic support for descriptive info; this will likely need to be improved in the future, but I need something for the next framework release. (FELIX-2042)
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@941487 13f79535-47bb-0310-9956-ffa450edef68

--- gogo/runtime/src/main/java/org/osgi/service/command/Descriptor.java
+++ gogo/runtime/src/main/java/org/osgi/service/command/Descriptor.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -24,10 +24,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.PARAMETER})
-public @interface Option
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER})
+public @interface Descriptor
 {
-    String name();
-    String description() default "";
-    String dflt();
+    String description();
 }
--- gogo/runtime/src/main/java/org/osgi/service/command/Flag.java
+++ gogo/runtime/src/main/java/org/osgi/service/command/Flag.java
@@ -28,4 +28,5 @@ import java.lang.annotation.Target;
 public @interface Flag
 {
     String name();
+    String description() default "";
 }
