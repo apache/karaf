314167d7bc6e2303371169d4a04d4267abe9c2ab
Guillaume Nodet <gnodet@apache.org>
2010-09-21 17:05:46 +0000

    [KARAF-218] Add a more pager command
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@999499 13f79535-47bb-0310-9956-ffa450edef68

--- shell/commands/src/main/java/org/apache/karaf/shell/commands/MoreAction.java
+++ shell/commands/src/main/java/org/apache/karaf/shell/commands/MoreAction.java
@@ -16,8 +16,138 @@
  */
 package org.apache.karaf.shell.commands;
 
-import org.apache.felix.gogo.commands.Command;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.Reader;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-@Command(scope = "shell", name = "more", description = "")
-public class MoreAction {
+import jline.Terminal;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
+import org.apache.karaf.shell.console.AbstractAction;
+
+@Command(scope = "shell", name = "more", description = "file pager")
+public class MoreAction extends AbstractAction {
+
+    @Option(name = "--lines", description = "stop after N lines")
+    int lines;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        Terminal term = (Terminal) session.get(".jline.terminal");
+        if (term == null || !isTty(System.out)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            return null;
+        } else {
+            boolean echo = term.getEcho();
+            term.disableEcho();
+            try {
+                if (lines == 0) {
+                    lines = term.getTerminalHeight();
+                }
+                LineSplitter reader = new LineSplitter(new BufferedReader(new InputStreamReader(System.in)), term.getTerminalWidth());
+                int count = 0;
+                int c;
+                do {
+                    do {
+                        String line;
+                        if ((line = reader.readLine()) == null) {
+                            return null;
+                        }
+                        System.out.println(line);
+                    } while (++count < lines - 2);
+                    c = -1;
+                    while (c == -1) {
+                        System.out.flush();
+                        System.out.print("--More--");
+                        System.out.flush();
+                        c = term.readVirtualKey(session.getKeyboard());
+                        switch (c) {
+                            case 'q':
+                            case -1:
+                                c = 'q';
+                                break;
+                            case '\r':
+                            case '\n':
+                            case 14: // Down arrow
+                                count--;
+                                System.out.print("\r          \r");
+                                break;
+                            case ' ':
+                                count = 0;
+                                System.out.print("\r          \r");
+                                break;
+                            case 16: // Up arrow
+                                // fall through
+                            default:
+                                c = -1;
+                                System.out.print("\r          \r");
+                                break;
+                        }
+                        if (c == 'q') {
+                            break;
+                        }
+                    }
+                } while (c != 'q');
+                return null;
+            } finally {
+                if (echo) {
+                    term.enableEcho();
+                } else {
+                    term.disableEcho();
+                }
+            }
+        }
+    }
+
+    public static class LineSplitter {
+
+        private final BufferedReader reader;
+        private final int width;
+        private final List<String> lines = new LinkedList<String>();
+
+        public LineSplitter(BufferedReader reader, int width) {
+            this.reader = reader;
+            this.width = width;
+        }
+
+        public String readLine() throws IOException {
+            if (lines.isEmpty()) {
+                String str = reader.readLine();
+                if (str == null) {
+                    return null;
+                }
+                while (str.length() > width) {
+                    lines.add(str.substring(0, width));
+                    str = str.substring(width);
+                }
+                lines.add(str);
+            }
+            return lines.remove(0);
+        }
+    }
+
+    protected boolean isTty(OutputStream out) {
+        try {
+            Method mth = out.getClass().getDeclaredMethod("getCurrent");
+            mth.setAccessible(true);
+            Object current = mth.invoke(out);
+            return current == session.getConsole();
+        } catch (Throwable t) {
+            return false;
+        }
+    }
+
 }
