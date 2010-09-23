4a5eb45a385217b9fd5789bef8f648634b6af30a
Guillaume Nodet <gnodet@apache.org>
2010-09-23 09:21:08 +0000

    KARAF-151: Add a karaf archive deployer Patch provided by Adrian Trenaman
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1000375 13f79535-47bb-0310-9956-ffa450edef68

---
+++ deployer/kar/src/main/java/org/apache/karaf/deployer/kar/KarArtifactInstaller.java
@@ -0,0 +1,246 @@
+package org.apache.karaf.deployer.kar;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.felix.fileinstall.ArtifactInstaller;
+import org.apache.karaf.features.FeaturesService;
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class KarArtifactInstaller implements ArtifactInstaller {
+
+	private static Log logger = LogFactory.getLog(KarArtifactInstaller.class);
+	
+	private static final String KAR_PREFIX = ".kar";
+	private static final String ZIP_PREFIX = ".zip";
+
+	private String localRepoPath = "./local-repo";
+
+	private String timestampPath;
+	
+	private byte[] buffer = new byte[5 * 1024];
+	
+	private DocumentBuilderFactory dbf;
+	
+	private FeaturesService featuresService;
+	
+	public void init() {         
+		dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+    
+		timestampPath = localRepoPath + File.separator + ".timestamps";
+		if (new File(timestampPath).mkdirs()) { 
+			logger.warn("Unable to create directory for Karaf Archive timestamps. Results may vary...");
+		}
+		
+		if (logger.isInfoEnabled()) { 
+			logger.info("Karaf archives will be extracted to " + localRepoPath); 
+			logger.info("Timestamps for Karaf archives will be extracted to " + timestampPath); 
+			
+		} 
+	}
+
+	public void destroy() { 
+		logger.info("Karaf archive installer destroyed.");
+	}
+
+
+	public void install(File file) throws Exception {
+		// Check to see if this file has already been extracted. For example, on restart of Karaf,
+		// we don't necessarily want to re-extract all the Karaf Archives!
+		//
+		if (alreadyExtracted(file)) { 
+			logger.info("Ignoring '" + file + "'; timestamp indicates it's already been deployed.");
+			return; 
+		}
+		
+		if (logger.isInfoEnabled()) 
+			logger.info("Installing " + file);
+		
+		ZipFile zipFile = new ZipFile(file);
+		
+		Enumeration<ZipEntry> entries = (Enumeration<ZipEntry>) zipFile.entries();
+		while (entries.hasMoreElements()) { 
+			ZipEntry entry = (ZipEntry) entries.nextElement();
+
+			if (! entry.getName().startsWith("META-INF")) { 
+				if (entry.isDirectory()) { 
+					java.io.File directory = new File(localRepoPath + File.separator + entry.getName());
+					if (logger.isDebugEnabled())
+						logger.debug("Creating directory '" + directory.getName());
+					directory.mkdirs();
+				} else { 
+					File extract = new File(localRepoPath + File.separator + entry.getName());
+			        BufferedOutputStream bos = new BufferedOutputStream(
+			                new FileOutputStream(extract));
+			        
+		            int count = 0;
+		            int totalBytes = 0;
+		            InputStream inputStream = zipFile.getInputStream(entry);
+		            while ((count = inputStream.read(buffer)) > 0)
+		            {
+		            	bos.write(buffer, 0, count);
+		            	totalBytes += count;
+		            }
+		            
+		            if (logger.isDebugEnabled()) 
+		            	logger.debug("Extracted " + totalBytes + " bytes to " + extract);
+		            
+		            bos.close();	
+		            inputStream.close();
+		            
+		            if (isFeaturesRepository(extract)) { 
+						addToFeaturesRepositories(extract);
+		            }
+				}
+			}
+		}
+		
+		zipFile.close();
+		
+		updateTimestamp(file);
+	}
+
+	public void uninstall(File file) throws Exception {
+		logger.warn("Karaf archive '" + file + "' has been removed; however, it's feature URLs have not been deregistered, and it's bundles are still available in '" + localRepoPath + "'.");
+	}
+
+	public void update(File file) throws Exception {
+		logger.warn("Karaf archive " + file + " has been updated; redeploying.");
+		install(file);
+	}
+	
+	protected void updateTimestamp(File karafArchive) throws Exception {
+		File timestamp = getArchiveTimestampFile(karafArchive);
+		
+		if (timestamp.exists()) {
+			if (logger.isDebugEnabled())
+				logger.debug("Deleting old timestamp file '" + timestamp + "");
+			
+			if (!timestamp.delete()) { 
+				throw new Exception("Unable to delete archive timestamp '" + timestamp + "'");
+			}
+		}
+		
+		logger.debug("Creating timestamp file '" + timestamp + "'");
+		timestamp.createNewFile();		
+	}
+	
+	protected boolean alreadyExtracted(File karafArchive) {
+		File timestamp = getArchiveTimestampFile(karafArchive);
+		if (timestamp.exists()) { 
+			return timestamp.lastModified() >= karafArchive.lastModified();
+		}
+		return false;
+	}
+
+	protected File getArchiveTimestampFile(File karafArchive) {
+		return new File(localRepoPath + File.separator + ".timestamps" + File.separator + karafArchive.getName());	
+	}
+	
+	protected boolean isFeaturesRepository(File artifact)  { 
+        try {
+			if (artifact.isFile() && artifact.getName().endsWith(".xml")) {
+			    Document doc = parse(artifact);
+			    String name = doc.getDocumentElement().getLocalName();
+			    String uri  = doc.getDocumentElement().getNamespaceURI();
+			    if ("features".equals(name) && (uri == null || "".equals(uri))) {
+			        return true;
+			    }
+			}
+		} catch (Exception e) {
+			if (logger.isDebugEnabled()) 
+				logger.debug("File " + artifact.getName() + " is not a features file.", e);
+		}
+		return false;
+	}
+	
+    protected Document parse(File artifact) throws Exception {
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        db.setErrorHandler(new ErrorHandler() {
+            public void warning(SAXParseException exception) throws SAXException {
+            }
+            public void error(SAXParseException exception) throws SAXException {
+            }
+            public void fatalError(SAXParseException exception) throws SAXException {
+                throw exception;
+            }
+        });
+        return db.parse(artifact);
+    }	
+	
+	private void addToFeaturesRepositories(File file)  {
+		try {
+			featuresService.addRepository(file.toURI());
+			if (logger.isInfoEnabled())  
+				logger.info("Added feature repository '" + file.toURI() + "'.");
+		} catch (Exception e) {
+			logger.error("Unable to add repository '" + file.getName() + "'", e);
+		}
+	}
+	
+	public boolean canHandle(File file) {
+		// If the file ends with .kar, then we can handle it!
+		//
+		if (file.isFile() && file.getName().endsWith(KAR_PREFIX)) {
+			logger.info("Found a .kar file to deploy.");
+			return true;
+		}	
+		// Otherwise, check to see if it's a zip file containing a META-INF/KARAF.MF manifest.
+		//
+		else if (file.isFile() && file.getName().endsWith(ZIP_PREFIX)) {		
+			logger.debug("Found a .zip file to deploy; checking contents to see if it's a Karaf archive.");
+			try {
+				if (new ZipFile(file).getEntry("META-INF/KARAF.MF") != null) { 
+					logger.info("Found a Karaf archive with .zip prefix; will deploy.");
+					return true;
+				}
+			} catch (Exception e) {
+				logger.warn("Problem extracting zip file '" + file.getName() + "'; ignoring.", e);
+			}
+		}
+				
+		return false;	
+	}
+
+	public boolean deleteLocalRepository() {
+		return deleteDirectory(new File(localRepoPath));
+	}
+	
+	private boolean deleteDirectory(File path) {
+		if (path.exists()) {
+			File[] files = path.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				} else {
+					files[i].delete();
+				}
+			}
+		}
+		return (path.delete());
+	}
+
+	public void setLocalRepoPath(String localRepoPath) {
+		this.localRepoPath = localRepoPath;
+	}
+
+	public void setFeaturesService(FeaturesService featuresService) {
+		this.featuresService = featuresService;
+	}
+	
+}
---
+++ deployer/kar/src/test/java/org/apache/karaf/deployer/kar/KarArtifactInstallerTest.java
@@ -0,0 +1,183 @@
+package org.apache.karaf.deployer.kar;
+
+import static org.easymock.EasyMock.createMock;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URI;
+
+import org.apache.karaf.features.FeaturesService;
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KarArtifactInstallerTest {
+	private KarArtifactInstaller karArtifactInstaller;
+	private FeaturesService featuresService;
+
+	private URI goodKarFile;
+	private URI zipFileWithKarafManifest;
+	private URI zipFileWithoutKarafManifest;
+	private URI badZipFile;
+	
+	@Before
+	public void setUp() throws Exception {
+		featuresService = createMock(FeaturesService.class);
+
+		karArtifactInstaller = new KarArtifactInstaller();
+
+		karArtifactInstaller.setFeaturesService(featuresService);
+		karArtifactInstaller.setLocalRepoPath("./target/local-repo");
+		
+		karArtifactInstaller.init();
+		
+		goodKarFile = getClass().getClassLoader().getResource("goodKarFile.kar").toURI();
+		zipFileWithKarafManifest = getClass().getClassLoader().getResource("karFileAsZip.zip").toURI();
+		zipFileWithoutKarafManifest = getClass().getClassLoader().getResource("karFileAsZipNoManifest.zip").toURI();
+		badZipFile = getClass().getClassLoader().getResource("badZipFile.zip").toURI();
+	}
+	
+	@After
+	public void destroy() throws Exception { 
+		karArtifactInstaller.destroy();
+		karArtifactInstaller.deleteLocalRepository();
+	}
+	
+	
+	
+	@Test
+	public void shouldHandleKarFile() throws Exception {
+		assertTrue(karArtifactInstaller.canHandle(new File(goodKarFile)));
+	}
+	
+	@Test
+	public void shouldHandleZipFileWithKarafManifest() throws Exception {
+		assertTrue(karArtifactInstaller.canHandle(new File(zipFileWithKarafManifest)));
+	}
+
+	@Test
+	public void shouldIgnoreZipFileWithoutKarafManifest() throws Exception { 		
+		assertFalse(karArtifactInstaller.canHandle(new File(zipFileWithoutKarafManifest)));
+	}
+
+	@Test
+	public void shouldIgnoreBadZipFile() throws Exception { 		
+		assertFalse(karArtifactInstaller.canHandle(new File(badZipFile)));
+	}
+
+	@Test 
+	public void shouldRecognizeGoodFeaturesFile() throws Exception
+	{
+		File goodFeaturesXml = new File(getClass().getClassLoader().getResource("goodKarFile/org/foo/goodFeaturesXml.xml").getFile());
+		Assert.assertTrue(karArtifactInstaller.isFeaturesRepository(goodFeaturesXml));
+	}
+	
+	@Test 
+	public void shouldRejectNonFeaturesXMLFile() throws Exception
+	{
+		File goodFeaturesXml = new File(getClass().getClassLoader().getResource("badFeaturesXml.xml").toURI());
+		Assert.assertFalse(karArtifactInstaller.isFeaturesRepository(goodFeaturesXml));
+	}
+	
+	
+	@Test 
+	public void shouldRejectMalformedXMLFile() throws Exception
+	{
+		File malformedXml = new File(getClass().getClassLoader().getResource("malformedXml.xml").toURI());
+		Assert.assertFalse(karArtifactInstaller.isFeaturesRepository(malformedXml));
+	}
+	
+	@Test
+	public void shouldExtractAndRegisterFeaturesFromKar() throws Exception { 
+		// Setup expectations on the features service
+		featuresService.addRepository(EasyMock.anyObject(URI.class));
+		EasyMock.replay(featuresService);
+		
+		// Test
+		//
+		File goodKarFile = new File(getClass().getClassLoader().getResource("goodKarFile.kar").getFile());
+		karArtifactInstaller.install(goodKarFile);
+		
+		// Verify expectations.
+		//
+		EasyMock.verify(featuresService);
+	}
+	
+	@Test
+	public void shouldLogAndNotThrowExceptionIfCannotAddToFeaturesRepository() throws Exception { 
+		// Setup expectations on the features service
+		featuresService.addRepository(EasyMock.anyObject(URI.class));
+		EasyMock.expectLastCall().andThrow(new Exception("Unable to add to repository."));
+		EasyMock.replay(featuresService);
+		
+		// Test
+		//
+		File goodKarFile = new File(getClass().getClassLoader().getResource("goodKarFile.kar").getFile());
+		karArtifactInstaller.install(goodKarFile);
+		
+		// Verify expectations.
+		//
+		EasyMock.verify(featuresService);
+	}	
+	
+	@Test
+	public void shouldIgnoreUpdateIfFileHasNotChanged() throws Exception { 
+		// Setup expectations on the features service: the addRepository 
+		// should only be added once, as the update command should be ignored! 
+		//
+		featuresService.addRepository(EasyMock.anyObject(URI.class));
+		EasyMock.replay(featuresService);
+		
+		// Test
+		//
+		File goodKarFile = new File(getClass().getClassLoader().getResource("goodKarFile.kar").getFile());
+		karArtifactInstaller.install(goodKarFile);
+		karArtifactInstaller.update(goodKarFile);
+		
+		// Verify expectations.
+		//
+		EasyMock.verify(featuresService);
+	}		
+	
+	@Test
+	public void shouldExtractAndRegisterFeaturesFromZip() throws Exception { 
+		// Setup expectations on the features service
+		featuresService.addRepository(EasyMock.anyObject(URI.class));
+		EasyMock.replay(featuresService);
+		
+		// Test
+		//
+		File karFileAsZip = new File(getClass().getClassLoader().getResource("karFileAsZip.zip").getFile());
+		karArtifactInstaller.install(karFileAsZip);
+		
+		// Verify expectations.
+		//
+		EasyMock.verify(featuresService);
+	}
+	
+	@Test (expected = java.io.IOException.class) 
+	public void shouldThrowExceptionIfFileDoesNotExist() throws Exception
+	{
+		File nonExistantFile = new File("DoesNotExist");
+		karArtifactInstaller.install(nonExistantFile);
+	}	
+	
+	@Test
+	public void uninstallShouldDoNothing() throws Exception
+	{
+		EasyMock.replay(featuresService);
+		
+		// Test
+		//
+		File karFileAsZip = new File(getClass().getClassLoader().getResource("karFileAsZip.zip").getFile());
+		karArtifactInstaller.uninstall(karFileAsZip);
+		
+		// Verify expectations.
+		//
+		EasyMock.verify(featuresService);
+	}
+			
+}
