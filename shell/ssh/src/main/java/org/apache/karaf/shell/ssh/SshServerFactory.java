ac92f3d73a621fb1f864dc2e900979e0ee2d52ff
Guillaume Nodet <gnodet@apache.org>
2010-09-29 10:33:42 +0000

    [KARAF-235][KARAF-195][KARAF-197] Upgrade to latest aries blueprint/jmx, revert the use of cade and use the new update-strategy='reload' attribute for ConfigAdmin property-placeholder
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1002569 13f79535-47bb-0310-9956-ffa450edef68

--- shell/ssh/src/main/java/org/apache/karaf/shell/ssh/SshServerFactory.java
+++ shell/ssh/src/main/java/org/apache/karaf/shell/ssh/SshServerFactory.java
@@ -18,44 +18,20 @@
  */
 package org.apache.karaf.shell.ssh;
 
-import java.util.Map;
-
 import org.apache.sshd.SshServer;
-import org.apache.sshd.server.jaas.JaasPasswordAuthenticator;
-import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
-import org.fusesource.cade.Configurable;
-import org.osgi.service.blueprint.container.BlueprintContainer;
-import org.osgi.service.cm.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SshServerFactory implements Configurable<SshConfig> {
+public class SshServerFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SshServerFactory.class);
 
-    private String serverId;
-    private BlueprintContainer blueprintContainer;
     private boolean start;
 
     private SshServer server;
 
-    public SshServerFactory() {
-    }
-
-    public String getServerId() {
-        return serverId;
-    }
-
-    public void setServerId(String serverId) {
-        this.serverId = serverId;
-    }
-
-    public BlueprintContainer getBlueprintContainer() {
-        return blueprintContainer;
-    }
-
-    public void setBlueprintContainer(BlueprintContainer blueprintContainer) {
-        this.blueprintContainer = blueprintContainer;
+    public SshServerFactory(SshServer server) {
+        this.server = server;
     }
 
     public boolean isStart() {
@@ -66,25 +42,14 @@ public class SshServerFactory implements Configurable<SshConfig> {
         this.start = start;
     }
 
-    public void start(final SshConfig config) {
+    public void start() {
         if (start) {
             try {
-                server = (SshServer) blueprintContainer.getComponentInstance(serverId);
-                server.setPort(config.sshPort());
-                server.setHost(config.sshHost());
-                server.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(config.hostKey()));
-                server.setPasswordAuthenticator(newJaasPasswordAuthenticator((config.sshRealm())));
                 server.start();
             } catch (Exception e) {
                 LOGGER.info("Error updating SSH server", e);
             }
         }
-    }
-
-    private JaasPasswordAuthenticator newJaasPasswordAuthenticator(String domain) {
-        JaasPasswordAuthenticator auth = new JaasPasswordAuthenticator();
-        auth.setDomain(domain);
-        return auth;
     }
 
     public void stop() {
@@ -97,15 +62,6 @@ public class SshServerFactory implements Configurable<SshConfig> {
                 server = null;
             }
         }
-    }
-
-    public void setup(SshConfig sshConfig) {
-        stop();
-        start(sshConfig);
-    }
-
-    public void deleted() {
-        stop();
     }
 
 }
