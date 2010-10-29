ad2601b4d6bdb7cb150992f988571f8d16205cc1
Ioannis Canellos <iocanel@apache.org>
2010-10-29 14:03:59 +0000

    [KARAF-188] Added Jaas shell commands for adding/ removing users and roles.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1028753 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jaas/command/src/main/java/org/apache/karaf/jaas/command/ListCommand.java
@@ -0,0 +1,50 @@
+package org.apache.karaf.jaas.command;
+
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.jaas.boot.ProxyLoginModule;
+import org.apache.karaf.jaas.config.JaasRealm;
+import org.apache.karaf.jaas.modules.BackingEngine;
+
+import javax.security.auth.login.AppConfigurationEntry;
+import java.util.Queue;
+
+/**
+ * @author iocanel
+ */
+
+/**
+ * Lists the commands the are in the command queue, for the active realm/module.
+ *
+ * @author iocanel
+ */
+@Command(scope = "jaas", name = "list", description = "Lists the modification on the active realm/module.")
+public class ListCommand extends JaasCommandSupport {
+
+    @Override
+    protected Object doExecute() throws Exception {
+        JaasRealm realm = (JaasRealm) session.get(JAAS_REALM);
+        AppConfigurationEntry entry = (AppConfigurationEntry) session.get(JAAS_ENTRY);
+        Queue<JaasCommandSupport> commandQueue = (Queue<JaasCommandSupport>) session.get(JAAS_CMDS);
+
+        if (realm != null && entry != null) {
+            String moduleClass = (String) entry.getOptions().get(ProxyLoginModule.PROPERTY_MODULE);
+            System.out.println(String.format("Jaas Realm:%s Jaas Module:%s", realm.getName(), moduleClass));
+
+            if (commandQueue != null && !commandQueue.isEmpty()) {
+                for (JaasCommandSupport command : commandQueue) {
+                    System.out.println(command);
+                }
+            } else {
+                System.err.println("No JAAS command in queue.");
+            }
+        } else {
+            System.err.println("No JAAS Realm / Module has been selected.");
+        }
+        return null;
+    }
+
+    @Override
+    protected Object doExecute(BackingEngine engine) throws Exception {
+        return null;
+    }
+}
