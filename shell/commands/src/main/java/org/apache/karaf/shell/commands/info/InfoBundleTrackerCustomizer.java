99bc76950c4db14a3a5dbde8943b0d0f3999647c
Ioannis Canellos <iocanel@apache.org>
2010-11-23 14:20:33 +0000

    [KARAF-243] Some statements are now logged as debug.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1038127 13f79535-47bb-0310-9956-ffa450edef68

--- shell/commands/src/main/java/org/apache/karaf/shell/commands/info/InfoBundleTrackerCustomizer.java
+++ shell/commands/src/main/java/org/apache/karaf/shell/commands/info/InfoBundleTrackerCustomizer.java
@@ -16,10 +16,6 @@
  */
 package org.apache.karaf.shell.commands.info;
 
-import java.util.Dictionary;
-import java.util.Properties;
-import java.util.StringTokenizer;
-
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
@@ -28,77 +24,85 @@ import org.osgi.util.tracker.BundleTrackerCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Dictionary;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
 /**
  * @author splatch
- * Bundle tracker which check manifest headers for informations.
+ *         Bundle tracker which check manifest headers for informations.
  */
 public class InfoBundleTrackerCustomizer implements BundleTrackerCustomizer {
 
-	/**
-	 * Logger.
-	 */
-	private Logger logger = LoggerFactory.getLogger(InfoBundleTrackerCustomizer.class);
+    /**
+     * Logger.
+     */
+    private Logger logger = LoggerFactory.getLogger(InfoBundleTrackerCustomizer.class);
 
-	/**
-	 * Bundle context.
-	 */
-	private final BundleContext context;
+    /**
+     * Bundle context.
+     */
+    private final BundleContext context;
 
-	public InfoBundleTrackerCustomizer(BundleContext context) {
-		this.context = context;
-	}
+    public InfoBundleTrackerCustomizer(BundleContext context) {
+        this.context = context;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@SuppressWarnings("unchecked")
-	public Object addingBundle(Bundle bundle, BundleEvent event) {
-		Dictionary headers = bundle.getHeaders();
-		String headerEntry = (String) headers.get("Karaf-Info");
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public Object addingBundle(Bundle bundle, BundleEvent event) {
+        Dictionary headers = bundle.getHeaders();
+        String headerEntry = (String) headers.get("Karaf-Info");
 
-		InfoProvider provider = createInfo(headerEntry);
-		if (provider == null) {
-			logger.info("Ignore incorrect info {} provided by bundle {}",
-					headerEntry, bundle.getSymbolicName());
-			return null;
-		}
-		return bundle.getBundleContext().registerService(InfoProvider.class.getName(),
-				provider, null);
-	}
+        InfoProvider provider = createInfo(headerEntry);
+        if (provider == null) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Ignore incorrect info {} provided by bundle {}",
+                        headerEntry, bundle.getSymbolicName());
+            }
+            return null;
+        }
+        return bundle.getBundleContext().registerService(InfoProvider.class.getName(),
+                provider, null);
+    }
 
-	public void modifiedBundle(Bundle bundle, BundleEvent event, Object object) {
-	}
+    public void modifiedBundle(Bundle bundle, BundleEvent event, Object object) {
+    }
 
-	public void removedBundle(Bundle bundle, BundleEvent event, Object object) {
-		if (object instanceof ServiceRegistration) {
-			ServiceRegistration service = (ServiceRegistration) object;
-			service.unregister();
-		}
-	}
+    public void removedBundle(Bundle bundle, BundleEvent event, Object object) {
+        if (object instanceof ServiceRegistration) {
+            ServiceRegistration service = (ServiceRegistration) object;
+            service.unregister();
+        }
+    }
 
-	private InfoProvider createInfo(String entry) {
-		if (entry == null) {
-			return null;
-		}
+    private InfoProvider createInfo(String entry) {
+        if (entry == null) {
+            return null;
+        }
 
-		StringTokenizer tokenizer = new StringTokenizer(entry, ";=");
-		if (tokenizer.countTokens() < 3) {
-			return null;
-		}
+        StringTokenizer tokenizer = new StringTokenizer(entry, ";=");
+        if (tokenizer.countTokens() < 3) {
+            return null;
+        }
 
-		String name = tokenizer.nextToken();
-		Properties properties = new Properties();
+        String name = tokenizer.nextToken();
+        Properties properties = new Properties();
 
-		do {
-			String property = tokenizer.nextToken();
-			if (tokenizer.hasMoreTokens()) {
-				properties.put(property, tokenizer.nextElement());
-			} else {
-				logger.debug("Ignore property {} without value", property);
-			}
-		} while (tokenizer.hasMoreTokens());
+        do {
+            String property = tokenizer.nextToken();
+            if (tokenizer.hasMoreTokens()) {
+                properties.put(property, tokenizer.nextElement());
+            } else {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Ignore property {} without value", property);
+                }
+            }
+        } while (tokenizer.hasMoreTokens());
 
-		return new PojoInfoProvider(name, properties);
-	}
+        return new PojoInfoProvider(name, properties);
+    }
 
 }
