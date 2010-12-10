689c4e0b52bc057a37b2fd308493cca7efc9316c
Guillaume Nodet <gnodet@apache.org>
2010-12-10 16:04:48 +0000

    [KARAF-316][FELIX-2723] Fix problem with interrupting commands
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1044414 13f79535-47bb-0310-9956-ffa450edef68

--- shell/commands/src/main/java/org/apache/karaf/shell/commands/TailAction.java
+++ shell/commands/src/main/java/org/apache/karaf/shell/commands/TailAction.java
@@ -21,6 +21,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.LinkedList;
