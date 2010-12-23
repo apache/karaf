65e4e3220a80d4770a528050c24245407fea101a
Guillaume Nodet <gnodet@apache.org>
2010-12-23 16:12:51 +0000

    [KARAF-340] Use the authenticated subject to run the webconsole code
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1052310 13f79535-47bb-0310-9956-ffa450edef68

--- webconsole/branding/src/main/java/org/apache/karaf/webconsole/JaasSecurityProvider.java
+++ webconsole/branding/src/main/java/org/apache/karaf/webconsole/JaasSecurityProvider.java
@@ -17,6 +17,7 @@
 package org.apache.karaf.webconsole;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 
 import javax.security.auth.Subject;
@@ -28,14 +29,25 @@ import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.security.auth.login.AccountException;
 import javax.security.auth.login.FailedLoginException;
 import javax.security.auth.login.LoginContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.apache.felix.webconsole.WebConsoleSecurityProvider;
+import org.apache.felix.webconsole.WebConsoleSecurityProvider2;
+import org.apache.felix.webconsole.internal.KarafOsgiManager;
+import org.apache.felix.webconsole.internal.servlet.Base64;
+import org.osgi.service.http.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JaasSecurityProvider implements WebConsoleSecurityProvider {
+public class JaasSecurityProvider implements WebConsoleSecurityProvider2 {
 
-	private static final Logger LOG = LoggerFactory.getLogger(WebConsoleSecurityProvider.class);
+	private static final Logger LOG = LoggerFactory.getLogger(JaasSecurityProvider.class);
+
+    private static final String HEADER_WWW_AUTHENTICATE = "WWW-Authenticate";
+
+    private static final String HEADER_AUTHORIZATION = "Authorization";
+
+    private static final String AUTHENTICATION_SCHEME_BASIC = "Basic";
 
     private String realm;
 
@@ -48,6 +60,10 @@ public class JaasSecurityProvider implements WebConsoleSecurityProvider {
     }
 
     public Object authenticate(final String username, final String password) {
+        return doAuthenticate( username, password );
+    }
+
+    public Subject doAuthenticate(final String username, final String password) {
         try {
             Subject subject = new Subject();
             LoginContext loginContext = new LoginContext(realm, subject, new CallbackHandler() {
@@ -80,4 +96,89 @@ public class JaasSecurityProvider implements WebConsoleSecurityProvider {
     public boolean authorize(Object o, String s) {
         return true;
     }
+
+    public boolean authenticate( HttpServletRequest request, HttpServletResponse response )
+    {
+        // Return immediately if the header is missing
+        String authHeader = request.getHeader( HEADER_AUTHORIZATION );
+        if ( authHeader != null && authHeader.length() > 0 )
+        {
+
+            // Get the authType (Basic, Digest) and authInfo (user/password)
+            // from the header
+            authHeader = authHeader.trim();
+            int blank = authHeader.indexOf( ' ' );
+            if ( blank > 0 )
+            {
+                String authType = authHeader.substring( 0, blank );
+                String authInfo = authHeader.substring( blank ).trim();
+
+                // Check whether authorization type matches
+                if ( authType.equalsIgnoreCase( AUTHENTICATION_SCHEME_BASIC ) )
+                {
+                    try
+                    {
+                        String srcString = base64Decode( authInfo );
+                        int i = srcString.indexOf( ':' );
+                        String username = srcString.substring( 0, i );
+                        String password = srcString.substring( i + 1 );
+
+                        // authenticate
+                        Subject subject = doAuthenticate( username, password );
+                        if ( subject != null )
+                        {
+                            // as per the spec, set attributes
+                            request.setAttribute( HttpContext.AUTHENTICATION_TYPE, HttpServletRequest.BASIC_AUTH );
+                            request.setAttribute( HttpContext.REMOTE_USER, username );
+
+                            // set web console user attribute
+                            request.setAttribute( WebConsoleSecurityProvider2.USER_ATTRIBUTE, username );
+
+                            // set the JAAS subject
+                            request.setAttribute( KarafOsgiManager.SUBJECT_RUN_AS, subject );
+
+                            // succeed
+                            return true;
+                        }
+                    }
+                    catch ( Exception e )
+                    {
+                        // Ignore
+                    }
+                }
+            }
+        }
+
+        // request authentication
+        try
+        {
+            response.setHeader( HEADER_WWW_AUTHENTICATE, AUTHENTICATION_SCHEME_BASIC + " realm=\"" + this.realm + "\"" );
+            response.setStatus( HttpServletResponse.SC_UNAUTHORIZED );
+            response.setContentLength( 0 );
+            response.flushBuffer();
+        }
+        catch ( IOException ioe )
+        {
+            // failed sending the response ... cannot do anything about it
+        }
+
+        // inform HttpService that authentication failed
+        return false;
+    }
+
+
+    private static String base64Decode( String srcString )
+    {
+        byte[] transformed = Base64.decodeBase64(srcString);
+        try
+        {
+            return new String( transformed, "ISO-8859-1" );
+        }
+        catch ( UnsupportedEncodingException uee )
+        {
+            return new String( transformed );
+        }
+    }
+
+
 }
