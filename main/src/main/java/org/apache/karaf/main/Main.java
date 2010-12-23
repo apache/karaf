83fd6b4760b7992b4de1b382c80ab944f6eb38c2
Guillaume Nodet <gnodet@apache.org>
2010-12-23 13:17:26 +0000

    [KARAF-334] Revert karaf-334 for now
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.1.x@1052260 13f79535-47bb-0310-9956-ffa450edef68

--- main/src/main/java/org/apache/karaf/main/Main.java
+++ main/src/main/java/org/apache/karaf/main/Main.java
@@ -538,7 +538,6 @@ public class Main {
         for (Integer startLevel : autoStart.keySet()) {
             StringTokenizer st = new StringTokenizer(autoStart.get(startLevel), "\" ", true);
             if (st.countTokens() > 0) {
-                List<Bundle> bundlesLevel = new ArrayList<Bundle>();
                 String location;
                 do {
                     location = nextLocation(st);
@@ -548,7 +547,6 @@ public class Main {
                             Bundle b = context.installBundle(parts[0], new URL(parts[1]).openStream());
                             sl.setBundleStartLevel(b, startLevel);
                             bundles.add(b);
-                            bundlesLevel.add(b);
                         }
                         catch (Exception ex) {
                             System.err.println("Error installing bundle  " + location + ": " + ex);
@@ -556,19 +554,19 @@ public class Main {
                     }
                 }
                 while (location != null);
-                // Now loop through and start the installed bundles.
-                if (start) {
-                    for (Bundle b : bundlesLevel) {
-                        try {
-                            String fragmentHostHeader = (String) b.getHeaders().get(Constants.FRAGMENT_HOST);
-                            if (fragmentHostHeader == null || fragmentHostHeader.trim().length() == 0) {
-                                b.start();
-                            }
-                        }
-                        catch (Exception ex) {
-                            System.err.println("Error starting bundle " + b.getSymbolicName() + ": " + ex);
-                        }
+            }
+        }
+        // Now loop through and start the installed bundles.
+        if (start) {
+            for (Bundle b : bundles) {
+                try {
+                    String fragmentHostHeader = (String) b.getHeaders().get(Constants.FRAGMENT_HOST);
+                    if (fragmentHostHeader == null || fragmentHostHeader.trim().length() == 0) {
+                        b.start();
                     }
+                }
+                catch (Exception ex) {
+                    System.err.println("Error starting bundle " + b.getSymbolicName() + ": " + ex);
                 }
             }
         }
