b794cd38c89d771e63d256151271b6d2ec99d211
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-01-21 08:04:25 +0000

    [KARAF-403] Add osgi:info command to display bundle OSGI-INF/bundle.info entry.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1061670 13f79535-47bb-0310-9956-ffa450edef68

---
+++ shell/osgi/src/main/java/org/apache/karaf/shell/osgi/Info.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.shell.osgi;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.fusesource.jansi.Ansi;
+import org.osgi.framework.Bundle;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+@Command(scope = "osgi", name = "info", description = "Displays detailed information of a given bundle")
+public class Info extends OsgiCommandSupport {
+
+    @Argument(index = 0, name = "ids", description = "A list of bundle IDs separated by whitespaces", required = false, multiValued = true)
+    List<Long> ids;
+
+    protected Object doExecute() throws Exception {
+        if (ids != null && !ids.isEmpty()) {
+            for (long id : ids) {
+                Bundle bundle = getBundleContext().getBundle(id);
+                if (bundle != null) {
+                    printInfo(bundle);
+                } else {
+                    System.err.println("Bundle ID " + id + " is invalid.");
+                }
+            }
+        } else {
+            Bundle[] bundles = getBundleContext().getBundles();
+            for (Bundle bundle : bundles) {
+                printInfo(bundle);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * <p>
+     * Get the OSGI-INF/bundle.info entry from the bundle and display it.
+     * </p>
+     *
+     * @param bundle the bundle.
+     */
+    protected void printInfo(Bundle bundle) {
+        String title = Util.getBundleName(bundle);
+        System.out.println("\n" + title);
+        System.out.println(Util.getUnderlineString(title));g
+        URL bundleInfo = bundle.getEntry("OSGI-INF/bundle.info");
+        if (bundleInfo != null) {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(bundleInfo.openStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                reader.close();
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+    }
+
+}
