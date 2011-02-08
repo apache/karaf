01d1db9aabdd813ef331515467db927581589409
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-02-08 11:18:33 +0000

    [KARAF-216] Add admin:clone command.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1068334 13f79535-47bb-0310-9956-ffa450edef68

---
+++ admin/command/src/main/java/org/apache/karaf/admin/command/CloneCommand.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.admin.command;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
+import org.apache.karaf.admin.InstanceSettings;
+import org.apache.karaf.admin.command.AdminCommandSupport;
+
+/**
+ * <p>
+ * Clone an existing Karaf instance.
+ * </p>
+ */
+@Command(scope = "admin", name = "clone", description = "Clones an existing container instance.")
+public class CloneCommand extends AdminCommandSupport {
+
+    @Option(name = "-s", aliases = {"--ssh-port"}, description = "Port number for remote secure shell connection", required = false, multiValued = false)
+    int sshPort = 0;
+
+    @Option(name = "-r", aliases = {"--rmi-port", "--rmi-registry-port"}, description = "Port number for RMI registry connection", required = false, multiValued = false)
+    int rmiPort = 0;
+
+    @Option(name = "-l", aliases = {"--location"}, description = "Location of the clone container instance in the file system", required = false, multiValued = false)
+    String location;
+
+    @Option(name = "-o", aliases = {"--java-opts"}, description = "JVM options to use when launching the clone instance", required = false, multiValued = false)
+    String javaOpts;
+
+    @Argument(index = 0, name = "name", description = "The name of the source container instance", required = true, multiValued = false)
+    String name;
+
+    @Argument(index = 1, name = "cloneName", description = "The name of the clone container instance", required = true, multiValued = false)
+    String cloneName;
+
+    protected Object doExecute() throws Exception {
+        InstanceSettings settings = new InstanceSettings(sshPort, rmiPort, location, javaOpts, null, null);
+        getAdminService().cloneInstance(name, cloneName, settings);
+        return null;
+    }
+
+}
