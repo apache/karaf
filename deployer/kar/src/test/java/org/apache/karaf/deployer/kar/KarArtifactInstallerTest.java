a281e2d4edf9443c5f2be62aa0aba92bcf21dc63
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-03-14 08:40:09 +0000

    Fix KAR deployer unit test failure.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1081283 13f79535-47bb-0310-9956-ffa450edef68

--- deployer/kar/src/test/java/org/apache/karaf/deployer/kar/KarArtifactInstallerTest.java
+++ deployer/kar/src/test/java/org/apache/karaf/deployer/kar/KarArtifactInstallerTest.java
@@ -201,8 +201,9 @@ public class KarArtifactInstallerTest {
 
     @Test
     public void testPathToMvnUri() throws Exception {
-        URI uri = KarArtifactInstaller.pathToMvnUri("org/apache/geronimo/features/org.apache.geronimo.transaction.kar/3.1.1-SNAPSHOT/org.apache.geronimo.transaction.kar-3.1.1-SNAPSHOT-feature.xml");
-        assert "mvn:org.apache.geronimo.features/org.apache.geronimo.transaction.kar/3.1.1-SNAPSHOT/xml/feature".equals(uri.toString());
+        URI uri = KarArtifactInstaller.pathToMvnUri("org/apache/geronimo/features/org.apache.geronimo.transaction.kar/3.1.1-SNAPSHOT/org.apache.geronimo.transaction.kar-3.1.1-SNAPSHOT-features.xml");
+        System.out.println(uri.toString());
+        assert "mvn:org.apache.geronimo.features/org.apache.geronimo.transaction.kar/3.1.1-SNAPSHOT/xml/features".equals(uri.toString());
     }
 			
 }
