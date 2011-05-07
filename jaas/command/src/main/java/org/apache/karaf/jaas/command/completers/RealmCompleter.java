83a484c9ced5c1fe5a93508e48a7db26587c64dc
Ioannis Canellos <iocanel@apache.org>
2011-05-07 19:39:48 +0000

    [KARAF-623] Added a shell completer for realm. Jaas manage command now uses the realm completer.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1100605 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jaas/command/src/main/java/org/apache/karaf/jaas/command/completers/RealmCompleter.java
@@ -0,0 +1,38 @@
+package org.apache.karaf.jaas.command.completers;
+
+import org.apache.karaf.jaas.config.JaasRealm;
+import org.apache.karaf.shell.console.Completer;
+import org.apache.karaf.shell.console.completer.StringsCompleter;
+
+import java.util.List;
+
+/**
+ * @author: iocanel
+ */
+public class RealmCompleter implements Completer {
+
+    private final StringsCompleter delegate = new StringsCompleter();
+
+    private List<JaasRealm> realms;
+
+    public int complete(String buffer, int cursor, List<String> candidates) {
+        StringsCompleter delegate = new StringsCompleter();
+        try {
+            if (realms != null && !realms.isEmpty())
+                for (JaasRealm realm : realms) {
+                    delegate.getStrings().add(realm.getName());
+                }
+        } catch (Exception e) {
+            // Ignore
+        }
+        return delegate.complete(buffer, cursor, candidates);
+    }
+
+    public List<JaasRealm> getRealms() {
+        return realms;
+    }
+
+    public void setRealms(List<JaasRealm> realms) {
+        this.realms = realms;
+    }
+}
