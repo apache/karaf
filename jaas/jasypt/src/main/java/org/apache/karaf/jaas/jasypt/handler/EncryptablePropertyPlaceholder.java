04005ba9ac7dd9c10a433d2954b347d179e37c63
Guillaume Nodet <gnodet@apache.org>
2011-09-01 16:43:15 +0000

    [KARAF-845] Add a jasypt based property placeholder to support encrypted values
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1164155 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jaas/jasypt/src/main/java/org/apache/karaf/jaas/jasypt/handler/EncryptablePropertyPlaceholder.java
@@ -0,0 +1,41 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.apache.karaf.jaas.jasypt.handler;
+
+import org.apache.aries.blueprint.ext.AbstractPropertyPlaceholder;
+import org.jasypt.encryption.StringEncryptor;
+
+public class EncryptablePropertyPlaceholder extends AbstractPropertyPlaceholder {
+
+    private StringEncryptor encryptor;
+
+    public StringEncryptor getEncryptor() {
+        return encryptor;
+    }
+
+    public void setEncryptor(StringEncryptor encryptor) {
+        this.encryptor = encryptor;
+    }
+
+    public void init() {
+
+    }
+
+    @Override
+    protected String getProperty(String val) {
+        return encryptor.decrypt(val);
+    }
+
+}
