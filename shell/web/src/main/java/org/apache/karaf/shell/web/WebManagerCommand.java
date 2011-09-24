e66acb6480de5c76f382e1d0b73e5b2eb349598b
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-09-24 06:04:17 +0000

    Add missing Apache header in the WebManagerCommand
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1175112 13f79535-47bb-0310-9956-ffa450edef68

--- shell/web/src/main/java/org/apache/karaf/shell/web/WebManagerCommand.java
+++ shell/web/src/main/java/org/apache/karaf/shell/web/WebManagerCommand.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.karaf.shell.web;
 
 import java.util.List;
@@ -13,7 +29,6 @@ public abstract class WebManagerCommand extends OsgiCommandSupport {
 	WebEventHandler eventHandler;
 	private WarManager warManager;
 
-	
     @Argument(index = 0, name = "ids", description = "The list of bundle IDs separated by whitespaces", required = true, multiValued = true)
     List<Long> ids;
 
