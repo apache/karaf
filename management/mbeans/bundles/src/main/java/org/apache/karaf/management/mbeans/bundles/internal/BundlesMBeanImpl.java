b26caa3f314875c2497b749e90b4f169dadf4b80
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-10-12 14:33:12 +0000

    [KARAF-930] Display the bundle state with a String
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1182402 13f79535-47bb-0310-9956-ffa450edef68

--- management/mbeans/bundles/src/main/java/org/apache/karaf/management/mbeans/bundles/internal/BundlesMBeanImpl.java
+++ management/mbeans/bundles/src/main/java/org/apache/karaf/management/mbeans/bundles/internal/BundlesMBeanImpl.java
@@ -51,7 +51,7 @@ public class BundlesMBeanImpl extends StandardMBean implements BundlesMBean {
         CompositeType bundleType = new CompositeType("Bundle", "OSGi Bundle",
                 new String[]{ "ID", "Name", "Version", "Start Level", "State" },
                 new String[]{ "ID of the Bundle", "Name of the Bundle", "Version of the Bundle", "Start Level of the Bundle", "Current State of the Bundle" },
-                new OpenType[]{ SimpleType.LONG, SimpleType.STRING, SimpleType.STRING, SimpleType.INTEGER, SimpleType.INTEGER });
+                new OpenType[]{ SimpleType.LONG, SimpleType.STRING, SimpleType.STRING, SimpleType.INTEGER, SimpleType.STRING });
         TabularType tableType = new TabularType("Bundles", "Tables of all Bundles", bundleType, new String[]{ "ID" });
         TabularData table = new TabularDataSupport(tableType);
 
@@ -62,9 +62,22 @@ public class BundlesMBeanImpl extends StandardMBean implements BundlesMBean {
             if (startLevel != null) {
                 bundleStartLevel = startLevel.getBundleStartLevel(bundles[i]);
             }
+            int bundleState = bundles[i].getState();
+            String bundleStateString;
+            if (bundleState == Bundle.ACTIVE)
+                bundleStateString = "ACTIVE";
+            else if (bundleState == Bundle.INSTALLED)
+                bundleStateString = "INSTALLED";
+            else if (bundleState == Bundle.RESOLVED)
+                bundleStateString = "RESOLVED";
+            else if (bundleState == Bundle.STARTING)
+                bundleStateString = "STARTING";
+            else if (bundleState == Bundle.STOPPING)
+                bundleStateString = "STOPPING";
+            else bundleStateString = "UNKNOWN";
             CompositeData data = new CompositeDataSupport(bundleType,
                     new String[]{ "ID", "Name", "Version", "Start Level", "State" },
-                    new Object[]{ bundles[i].getBundleId(), bundles[i].getSymbolicName(), bundles[i].getVersion().toString(), bundleStartLevel, bundles[i].getState() });
+                    new Object[]{ bundles[i].getBundleId(), bundles[i].getSymbolicName(), bundles[i].getVersion().toString(), bundleStartLevel, bundleStateString });
             table.put(data);
         }
 
