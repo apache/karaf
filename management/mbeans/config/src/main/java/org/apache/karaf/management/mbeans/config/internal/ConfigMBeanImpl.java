77fd8f44dfc378b481a5b81c914ba5c10c2264f6
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-10-13 06:03:03 +0000

    [KARAF-932] Fix checkstyle (use braces)
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1182671 13f79535-47bb-0310-9956-ffa450edef68

--- management/mbeans/config/src/main/java/org/apache/karaf/management/mbeans/config/internal/ConfigMBeanImpl.java
+++ management/mbeans/config/src/main/java/org/apache/karaf/management/mbeans/config/internal/ConfigMBeanImpl.java
@@ -84,9 +84,12 @@ public class ConfigMBeanImpl extends StandardMBean implements ConfigMBean {
         if (configuration == null) {
             throw new IllegalArgumentException("Configuration PID " + pid + " doesn't exist");
         }
+
         Dictionary dictionary = configuration.getProperties();
-        if (dictionary == null)
+        if (dictionary == null) {
             dictionary = new java.util.Properties();
+        }
+
         Map<String, String> propertiesMap = new HashMap<String, String>();
         for (Enumeration e = dictionary.keys(); e.hasMoreElements(); ) {
             Object key = e.nextElement();
@@ -101,9 +104,12 @@ public class ConfigMBeanImpl extends StandardMBean implements ConfigMBean {
         if (configuration == null) {
             throw new IllegalArgumentException("Configuration PID " + pid + " doesn't exist");
         }
+
         Dictionary dictionary = configuration.getProperties();
-        if (dictionary == null)
+        if (dictionary == null) {
             dictionary = new java.util.Properties();
+        }
+
         dictionary.remove(key);
         store(pid, dictionary, false);
     }
@@ -113,9 +119,12 @@ public class ConfigMBeanImpl extends StandardMBean implements ConfigMBean {
         if (configuration == null) {
             throw new IllegalArgumentException("Configuration PID " + pid + " doesn't exist");
         }
+
         Dictionary dictionary = configuration.getProperties();
-        if (dictionary == null)
+        if (dictionary == null) {
             dictionary = new java.util.Properties();
+        }
+
         Object currentValue = dictionary.get(key);
         if (currentValue == null) {
             dictionary.put(key, value);
@@ -132,9 +141,12 @@ public class ConfigMBeanImpl extends StandardMBean implements ConfigMBean {
         if (configuration == null) {
             throw new IllegalArgumentException("Configuration PID " + pid + " doesn't exist");
         }
+
         Dictionary dictionary = configuration.getProperties();
-        if (dictionary == null)
+        if (dictionary == null) {
             dictionary = new java.util.Properties();
+        }
+
         dictionary.put(key, value);
         store(pid, dictionary, false);
     }
