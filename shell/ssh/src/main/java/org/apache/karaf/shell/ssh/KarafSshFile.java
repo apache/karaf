c642b368d59fe5e46f8f3ddbc2d6cf3f005a0996
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-11-08 21:47:22 +0000

    [KARAF-987] Add scp support
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1199473 13f79535-47bb-0310-9956-ffa450edef68

---
+++ shell/ssh/src/main/java/org/apache/karaf/shell/ssh/KarafSshFile.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.karaf.shell.ssh;
+
+import org.apache.sshd.server.SshFile;
+import org.apache.sshd.server.filesystem.NameEqualsFileFilter;
+import org.apache.sshd.server.filesystem.NativeSshFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Karaf ssh file support.
+ */
+public class KarafSshFile extends NativeSshFile {
+
+    /**
+     * Constructor, internal do not use directly.
+     */
+    public KarafSshFile(String fileName, final File file) {
+        super(fileName, file, null);
+    }
+
+}
