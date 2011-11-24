645425dc500e5223504052e5bdec4018bb03339c
Ioannis Canellos <iocanel@apache.org>
2011-11-24 21:59:28 +0000

    [KARAF-1035] Modified config commands to accept pid as an option, in order make single command interactions possible.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.2.x@1205991 13f79535-47bb-0310-9956-ffa450edef68

--- shell/config/src/main/java/org/apache/karaf/shell/config/PropListCommand.java
+++ shell/config/src/main/java/org/apache/karaf/shell/config/PropListCommand.java
@@ -18,23 +18,26 @@ package org.apache.karaf.shell.config;
 
 import java.util.Dictionary;
 import java.util.Enumeration;
-
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.apache.felix.gogo.commands.Command;
 
 @Command(scope = "config", name = "proplist", description = "Lists properties from the currently edited configuration.")
-public class PropListCommand extends ConfigCommandSupport {
+public class PropListCommand extends ConfigPropertyCommandSupport {
 
-    protected void doExecute(ConfigurationAdmin admin) throws Exception {
-        Dictionary props = getEditedProps();
-        if (props == null) {
-            System.err.println("No configuration is being edited--run the edit command first");
-        } else {
-            for (Enumeration e = props.keys(); e.hasMoreElements();) {
-                Object key = e.nextElement();
-                System.out.println("   " + key + " = " + props.get(key));
-            }
+    @Override
+    public void propertyAction(Dictionary props) {
+        for (Enumeration e = props.keys(); e.hasMoreElements(); ) {
+            Object key = e.nextElement();
+            System.out.println("   " + key + " = " + props.get(key));
         }
     }
 
+    /**
+     * List commands never requires an update, so it always returns false.
+     * @param pid
+     * @return
+     */
+    @Override
+    protected boolean requiresUpdate(String pid) {
+        return false;
+    }
 }
