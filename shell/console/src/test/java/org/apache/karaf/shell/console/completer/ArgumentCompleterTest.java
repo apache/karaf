d18f044b95895214c2178a04d960ec6947810492
Ioannis Canellos <iocanel@apache.org>
2011-11-29 17:30:15 +0000

    [KARAF-1029] Fixed minor issues and tests.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.2.x@1207986 13f79535-47bb-0310-9956-ffa450edef68

--- shell/console/src/test/java/org/apache/karaf/shell/console/completer/ArgumentCompleterTest.java
+++ shell/console/src/test/java/org/apache/karaf/shell/console/completer/ArgumentCompleterTest.java
@@ -88,7 +88,7 @@ public class ArgumentCompleterTest extends CompleterTestSupport {
     public void testCompleteOptions() throws Exception {
         CommandSession session = new DummyCommandSession();
         Completer comp = new ArgumentCompleter(session, new MyFunction(), "my:action");
-        assertEquals(Arrays.asList("--check", "--foo", "--help", "--string", "--integer", "-c", "-f","-s","-i"), complete(comp, "action -"));
+        assertEquals(Arrays.asList("--check", "--foo", "--help", "--int", "--string", "-c", "-f", "-i", "-s"), complete(comp, "action -"));
         assertEquals(Arrays.asList(), complete(comp, "action --foo "));
         assertEquals(Arrays.asList("action "), complete(comp, "acti"));
         assertEquals(Arrays.asList("my:action "), complete(comp, "my:ac"));
@@ -99,10 +99,10 @@ public class ArgumentCompleterTest extends CompleterTestSupport {
         assertEquals(Arrays.asList("foo1 "), complete(comp, "action -f 2 --check foo1"));
         assertEquals(Arrays.asList("bar1", "bar2"), complete(comp, "action -f 2 --check foo1 "));
         assertEquals(Arrays.asList("one", "two"), complete(comp, "action -s "));
-        assertEquals(Arrays.asList("one", "two"), complete(comp, "action -string "));
+        assertEquals(Arrays.asList("one", "two"), complete(comp, "action --string "));
         assertEquals(Arrays.asList("two "), complete(comp, "action -s t"));
         assertEquals(Arrays.asList("1", "2"), complete(comp, "action -i "));
-        assertEquals(Arrays.asList("1", "2"), complete(comp, "action --integer "));
+        assertEquals(Arrays.asList("1", "2"), complete(comp, "action --int "));
         assertEquals(Arrays.asList("2 "), complete(comp, "action -i 2"));
     }
 
@@ -120,7 +120,7 @@ public class ArgumentCompleterTest extends CompleterTestSupport {
         public Map<String,Completer> getOptionalCompleters() {
             Map<String,Completer> completers = new HashMap<String,Completer>();
             completers.put("-s",new StringsCompleter(Arrays.asList("one","two")));
-            completers.put("--integer",new StringsCompleter(Arrays.asList("1","2")));
+            completers.put("--int",new StringsCompleter(Arrays.asList("1","2")));
             return completers;
         }
     }
