a89bb8e5d8bd3284d9cfa38c76a70fb6a32e9232
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-11-30 21:54:26 +0000

    [KARAF-963] Refactoring of the system module
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1208819 13f79535-47bb-0310-9956-ffa450edef68

---
+++ system/core/src/main/java/org/apache/karaf/system/SystemService.java
@@ -0,0 +1,53 @@
+ /*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.system;
+
+/**
+ * Describe a system service
+ */
+public interface SystemService {
+
+    /**
+     * Shutdown the Karaf container.
+     */
+    void shutdown() throws Exception;
+
+    /**
+     * Shutdown the Karaf container.
+     *
+     * @param time shutdown delay. The time argument can have different formats.
+     *  First, it can be an abolute time in the format hh:mm, in which hh is the hour (1 or 2 digits) and mm
+     *  is the minute of the hour (in two digits). Second, it can be in the format +m, in which m is the number of minutes
+     *  to wait. The word now is an alias for +0.
+     */
+    void shutdown(String time) throws Exception;
+
+    /**
+     * Set the system start level.
+     *
+     * @param startLevel the new system start level.
+     */
+    void setStartLevel(int startLevel) throws Exception;
+
+    /**
+     * Get the system start level.
+     *
+     * @return the current system start level.
+     */
+    int getStartLevel() throws Exception;
+
+}
