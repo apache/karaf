1981cc5a8addb369f12df316f7ec13751b910a38
Ioannis Canellos <iocanel@apache.org>
2012-02-22 14:27:39 +0000

    [KARAF-1196] Moved command to the correct package for the branch.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.2.x@1292308 13f79535-47bb-0310-9956-ffa450edef68

--- shell/commands/src/main/java/org/apache/karaf/shell/commands/WatchAction.java
+++ shell/commands/src/main/java/org/apache/karaf/shell/commands/WatchAction.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.karaf.shell.shell;
+package org.apache.karaf.shell.commands;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
