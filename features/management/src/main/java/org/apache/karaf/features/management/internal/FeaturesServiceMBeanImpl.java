f2467a7841f3ae445f20e4177cbd1a9f27a7bcfe
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-03-08 09:54:43 +0000

    [KARAF-1175] Catch exception populating TabularData in MBean to avoid empty tables
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.2.x@1298331 13f79535-47bb-0310-9956-ffa450edef68

--- features/management/src/main/java/org/apache/karaf/features/management/internal/FeaturesServiceMBeanImpl.java
+++ features/management/src/main/java/org/apache/karaf/features/management/internal/FeaturesServiceMBeanImpl.java
@@ -91,7 +91,11 @@ public class FeaturesServiceMBeanImpl extends StandardEmitterMBean implements
             List<Feature> insFeatures = Arrays.asList(featuresService.listInstalledFeatures());
             ArrayList<JmxFeature> features = new ArrayList<JmxFeature>();
             for (Feature feature : allFeatures) {
-                features.add(new JmxFeature(feature, insFeatures.contains(feature)));
+                try {
+                    features.add(new JmxFeature(feature, insFeatures.contains(feature)));
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
             }
             TabularData table = JmxFeature.tableFrom(features);
             return table;
