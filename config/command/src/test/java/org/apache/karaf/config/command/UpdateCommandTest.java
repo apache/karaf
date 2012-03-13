16420e9ac3dd647526b0aa6930de731e093ae7b9
Christian Schneider <cschneider@apache.org>
2012-03-13 16:05:10 +0000

    KARAF-963 restructuring of config to new module layout
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1300211 13f79535-47bb-0310-9956-ffa450edef68

---
+++ config/command/src/test/java/org/apache/karaf/config/command/UpdateCommandTest.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.config.command;
+
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.apache.felix.service.command.CommandSession;
+import org.apache.karaf.config.core.impl.ConfigRepositoryImpl;
+import org.easymock.EasyMock;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * Test cases for {@link EditCommand}
+ */
+public class UpdateCommandTest extends TestCase {
+
+    private static final String PID = "my.test.persistent.id-other";
+
+    private UpdateCommand command;
+    private BundleContext context;
+    private ConfigurationAdmin admin;
+    private CommandSession session;
+
+    @Override
+    protected void setUp() throws Exception {
+        command = new UpdateCommand();
+
+        context = EasyMock.createMock(BundleContext.class);
+        command.setBundleContext(context);
+
+        admin = createMock(ConfigurationAdmin.class);
+        command.setConfigRepository(new ConfigRepositoryImpl(null, admin, null));
+        expect(context.getBundle(0)).andReturn(null).anyTimes();
+
+        replay(context);
+
+        session = new MockCommandSession();
+    }
+
+    public void testupdateOnNewFactoryPid() throws Exception {
+        Properties props = new Properties();
+
+        session.put(ConfigCommandSupport.PROPERTY_CONFIG_PID, PID);
+        session.put(ConfigCommandSupport.PROPERTY_CONFIG_PROPS, props);
+
+        Configuration configNew = createMock(Configuration.class);
+        expect(admin.getConfiguration(PID, null)).andReturn(configNew);
+        expect(configNew.getProperties()).andReturn(null);
+
+
+        Configuration configFac = createMock(Configuration.class);
+        expect(admin.createFactoryConfiguration(PID.substring(0, PID.indexOf('-')), null)).andReturn(configFac);
+        configFac.update(props);
+        expect(configFac.getBundleLocation()).andReturn(null);
+        replay(admin);
+        replay(configNew);
+        replay(configFac);
+
+        command.execute(session);
+
+    }
+
+}
