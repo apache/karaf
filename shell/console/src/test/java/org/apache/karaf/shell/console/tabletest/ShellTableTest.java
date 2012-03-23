6323857d4f2e3d0581717c5a3634870b83c05d4e
Christian Schneider <cschneider@apache.org>
2012-03-23 12:31:07 +0000

    KARAF-1281 Add a new ShellTable impl
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1304314 13f79535-47bb-0310-9956-ffa450edef68

---
+++ shell/console/src/test/java/org/apache/karaf/shell/console/tabletest/ShellTableTest.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.karaf.shell.console.tabletest;
+
+import org.apache.karaf.shell.console.table2.Col;
+import org.apache.karaf.shell.console.table2.Row;
+import org.apache.karaf.shell.console.table2.ShellTable;
+import org.junit.Test;
+
+public class ShellTableTest {
+
+    @Test
+    public void testTable() {
+        ShellTable table = new ShellTable();
+        table.column(new Col("id").alignRight().maxSize(5));
+        table.column(new Col("Name").maxSize(20));
+        table.column(new Col("Centered").alignCenter());
+        
+        table.addRow().addContent(1, "Test", "Description");
+        table.addRow().addContent(20, "My name", "Description");
+        
+        Row row = table.addRow();
+        row.addContent(123456789);
+        row.addContent("A very long text that should be cut");
+        row.addContent("A very long text that should not be cut");
+        
+        table.print(System.out);
+    }
+}
