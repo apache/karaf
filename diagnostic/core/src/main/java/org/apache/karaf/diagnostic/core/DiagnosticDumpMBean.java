4f627f66e1e0393a43a48da013fe90fcca2cb719
Christian Schneider <cschneider@apache.org>
2012-04-10 20:42:52 +0000

    KARAF-1302 Use aries jmx for diagnostic module
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1311978 13f79535-47bb-0310-9956-ffa450edef68

--- diagnostic/core/src/main/java/org/apache/karaf/diagnostic/core/DiagnosticDumpMBean.java
+++ diagnostic/core/src/main/java/org/apache/karaf/diagnostic/core/DiagnosticDumpMBean.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.karaf.diagnostic.management;
+package org.apache.karaf.diagnostic.core;
 
 /**
  * Diagnostic MBean which allows to create dumps over JMX.
--- diagnostic/core/src/main/java/org/apache/karaf/diagnostic/core/internal/ThreadDumpProvider.java
+++ diagnostic/core/src/main/java/org/apache/karaf/diagnostic/core/internal/ThreadDumpProvider.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.karaf.diagnostic.common;
+package org.apache.karaf.diagnostic.core.internal;
 
 import java.io.OutputStreamWriter;
 import java.lang.management.ManagementFactory;
