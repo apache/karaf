3f7e6b3c41c5ead62c6b51d1ce3f4de09ee1328d
Andreas Pieber <pieber@apache.org>
2012-04-26 08:59:58 +0000

    [KARAF-1402] Corrected the order in test framework generated features.xml
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1330721 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/exam/regression/src/test/java/org/apache/karaf/tooling/exam/regression/UseFeatureInsteadOfDeployFolderTest.java
+++ tooling/exam/regression/src/test/java/org/apache/karaf/tooling/exam/regression/UseFeatureInsteadOfDeployFolderTest.java
@@ -17,9 +17,13 @@
 
 package org.apache.karaf.tooling.exam.regression;
 
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
 import static org.apache.karaf.tooling.exam.options.KarafDistributionOption.karafDistributionConfiguration;
 import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.provision;
+
+import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,32 +32,31 @@ import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
 public class UseFeatureInsteadOfDeployFolderTest {
 
+    @Inject
+    private BundleContext bc;
+
     @Configuration
     public Option[] config() {
         return new Option[]{ karafDistributionConfiguration().frameworkUrl(
             maven().groupId("org.apache.karaf").artifactId("apache-karaf").type("zip").versionAsInProject())
-            .useDeployFolder(false) };
+            .useDeployFolder(false), provision(mavenBundle("org.slf4j", "slf4j-api", "1.6.1")) };
     }
 
     @Test
     public void test() throws Exception {
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        System.out.println("===========================================");
-        assertTrue(true);
+        for (Bundle b : bc.getBundles()) {
+            if (b.getSymbolicName().equals("slf4j.api")) {
+                return;
+            }
+        }
+        fail("slf4j-api is not provisioned");
     }
 
 }
