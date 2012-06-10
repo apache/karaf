f6c2ac9a710672eaa76c4daf00b0e02c4fcf7984
Ioannis Canellos <iocanel@apache.org>
2012-06-10 16:57:56 +0000

    [KARAF-1529] Added supprot for aborting a watch task when a users stops the command-watch.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.2.x@1348624 13f79535-47bb-0310-9956-ffa450edef68

--- shell/commands/src/main/java/org/apache/karaf/shell/commands/WatchAction.java
+++ shell/commands/src/main/java/org/apache/karaf/shell/commands/WatchAction.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
@@ -57,6 +58,7 @@ public class WatchAction extends AbstractAction {
             executorService.scheduleAtFixedRate(watchTask, 0, interval, TimeUnit.SECONDS);
             try {
                 session.getKeyboard().read();
+                watchTask.abort();
             } finally {
                 executorService.shutdownNow();
                 watchTask.close();
@@ -73,6 +75,7 @@ public class WatchAction extends AbstractAction {
         CommandSession session;
         ByteArrayOutputStream byteArrayOutputStream = null;
         PrintStream printStream = null;
+        boolean doDisplay = true;
 
         public WatchTask(CommandProcessor processor, String command) {
             this.processor = processor;
@@ -81,16 +84,19 @@ public class WatchAction extends AbstractAction {
 
         public void run() {
             try {
-                 byteArrayOutputStream = new ByteArrayOutputStream();
+                byteArrayOutputStream = new ByteArrayOutputStream();
                 printStream = new PrintStream(byteArrayOutputStream);
                 session = commandProcessor.createSession(null, printStream, printStream);
                 String output = "";
                 session.execute(command);
                 output = byteArrayOutputStream.toString();
-                System.out.print("\33[2J");
-                System.out.print("\33[1;1H");
-                System.out.print(output);
-                System.out.flush();
+                //Make sure before displaying that this is not a forgotten long running task.
+                if (doDisplay) {
+                    System.out.print("\33[2J");
+                    System.out.print("\33[1;1H");
+                    System.out.print(output);
+                    System.out.flush();
+                }
                 byteArrayOutputStream.close();
                 session.close();
             } catch (Exception e) {
@@ -98,6 +104,18 @@ public class WatchAction extends AbstractAction {
             }
         }
 
+        /**
+         * Aborts the task that it currently running.
+         * It will prevent it from displaying anything.
+         */
+        public void abort() {
+           doDisplay = false;
+        }
+
+        /**
+         * Closes the the {@CommandSession} and the streams for I/O.
+         * @throws IOException
+         */
         public void close() throws IOException {
             if (this.session != null) {
                 this.session.close();
