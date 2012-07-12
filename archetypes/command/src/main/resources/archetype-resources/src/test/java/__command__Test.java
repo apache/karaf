f707121e60b4986310b04967140578a628437073
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-07-12 15:19:57 +0000

    [KARAF-1624] Fix RAT check failures
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1360720 13f79535-47bb-0310-9956-ffa450edef68

--- archetypes/command/src/main/resources/archetype-resources/src/test/java/__command__Test.java
+++ archetypes/command/src/main/resources/archetype-resources/src/test/java/__command__Test.java
@@ -17,7 +17,6 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-
 package ${package};
 
 
--- main/src/main/java/org/apache/karaf/main/KarafActivatorManager.java
+++ main/src/main/java/org/apache/karaf/main/KarafActivatorManager.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.karaf.main;
 
 import java.io.IOException;
@@ -15,7 +33,9 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.launch.Framework;
 
 public class KarafActivatorManager {
+
     public static final String KARAF_ACTIVATOR = "Karaf-Activator";
+
     Logger LOG = Logger.getLogger(this.getClass().getName());
 
     private List<BundleActivator> karafActivators = new ArrayList<BundleActivator>();
@@ -67,4 +87,5 @@ public class KarafActivatorManager {
             }
         }
     }
+
 }
