2572aea1e59648b33693bf695746b459474fabb4
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-07-12 16:27:11 +0000

    [KARAF-1624] Fix RAT checks failure
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1360754 13f79535-47bb-0310-9956-ffa450edef68

--- shell/console/src/main/java/org/apache/karaf/shell/console/jline/KarafFileHistory.java
+++ shell/console/src/main/java/org/apache/karaf/shell/console/jline/KarafFileHistory.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.karaf.shell.console.jline;
 
 import java.io.File;
@@ -13,6 +31,7 @@ import org.slf4j.LoggerFactory;
  * user does not having write access to the history file.
  */
 public final class KarafFileHistory extends FileHistory {
+
 	static final Logger LOGGER = LoggerFactory.getLogger(KarafFileHistory.class);
 	boolean failed = false;
     boolean loading = false;
@@ -62,4 +81,5 @@ public final class KarafFileHistory extends FileHistory {
 	        }
 	    }
 	}
+
 }
