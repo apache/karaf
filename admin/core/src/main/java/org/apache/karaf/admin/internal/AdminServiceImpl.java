ab459068964c782923f19d0f44468318b515bd26
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-07-12 07:48:17 +0000

    [KARAF-1566] Fix admin clone operation
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1360564 13f79535-47bb-0310-9956-ffa450edef68

--- admin/core/src/main/java/org/apache/karaf/admin/internal/AdminServiceImpl.java
+++ admin/core/src/main/java/org/apache/karaf/admin/internal/AdminServiceImpl.java
@@ -320,7 +320,7 @@ public class AdminServiceImpl implements AdminService {
         }
 
         // define the clone instance location
-        String cloneLocationPath = settings.getLocation() != null ? settings.getLocation() : name;
+        String cloneLocationPath = settings.getLocation() != null ? settings.getLocation() : cloneName;
         File cloneLocation = new File(cloneLocationPath);
         if (!cloneLocation.isAbsolute()) {
             cloneLocation = new File(storageLocation, cloneLocationPath);
@@ -340,7 +340,7 @@ public class AdminServiceImpl implements AdminService {
         if (settings.getRmiServerPort() > 0)
             props.put(new Integer(instance.getRmiServerPort()).toString(), new Integer(settings.getRmiServerPort()).toString());
         // filtering clone files
-        filterResource(cloneLocation, "etc/customer.properties", props);
+        filterResource(cloneLocation, "etc/custom.properties", props);
         filterResource(cloneLocation, "etc/org.apache.karaf.management.cfg", props);
         filterResource(cloneLocation, "etc/org.apache.karaf.shell.cfg", props);
         filterResource(cloneLocation, "etc/org.ops4j.pax.logging.cfg", props);
@@ -356,8 +356,8 @@ public class AdminServiceImpl implements AdminService {
         if (javaOpts == null || javaOpts.length() == 0) {
             javaOpts = "-server -Xmx512M -Dcom.sun.management.jmxremote";
         }
-        Instance cloneInstance = new InstanceImpl(this, name, cloneLocation.toString(), settings.getJavaOpts());
-        instances.put(name, instance);
+        Instance cloneInstance = new InstanceImpl(this, cloneName, cloneLocation.toString(), settings.getJavaOpts());
+        instances.put(cloneName, cloneInstance);
         saveState();
         return cloneInstance;
     }
