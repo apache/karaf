63ab05bcf5f7f7c8de691877ab6a461f5d9f2afd
Guillaume Nodet <gnodet@apache.org>
2012-08-01 19:59:04 +0000

    [KARAF-1702] Stopping the console bundle should not shutdown the osgi framework
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1368223 13f79535-47bb-0310-9956-ffa450edef68

--- shell/console/src/main/java/org/apache/karaf/shell/console/impl/jline/LocalConsoleManager.java
+++ shell/console/src/main/java/org/apache/karaf/shell/console/impl/jline/LocalConsoleManager.java
@@ -121,8 +121,11 @@ public class LocalConsoleManager {
         if (registration != null) {
             registration.unregister();
         }
+        // The bundle is stopped
+        // so close the console and remove the callback so that the
+        // osgi framework isn't stopped
         if (console != null) {
-            console.close();
+            console.close(false);
         }
     }
 
