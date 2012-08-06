fa64a4c746d6c9afc1d7e38a7366fc70f98b9b8f
Freeman Yue Fang <ffang@apache.org>
2012-08-06 06:02:50 +0000

    [KARAF-1050]osgi:start
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1369726 13f79535-47bb-0310-9956-ffa450edef68

--- shell/osgi/src/main/java/org/apache/karaf/shell/osgi/RestartBundle.java
+++ shell/osgi/src/main/java/org/apache/karaf/shell/osgi/RestartBundle.java
@@ -27,6 +27,10 @@ import org.apache.felix.gogo.commands.Command;
 public class RestartBundle extends BundlesCommand {
 
     protected void doExecute(List<Bundle> bundles) throws Exception {
+        if (bundles.isEmpty()) {
+            System.err.println("No bundles specified.");
+            return;
+        }
         List<Exception> exceptions = new ArrayList<Exception>();
         for (Bundle bundle : bundles) {
             try {
