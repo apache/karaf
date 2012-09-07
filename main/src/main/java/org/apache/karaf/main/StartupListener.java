beda0c01cf128388c222d12948006a31017c6878
Guillaume Nodet <gnodet@apache.org>
2012-09-07 13:21:03 +0000

    Fix compilation problem and missing header
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1382013 13f79535-47bb-0310-9956-ffa450edef68

--- main/src/main/java/org/apache/karaf/main/StartupListener.java
+++ main/src/main/java/org/apache/karaf/main/StartupListener.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.karaf.main;
 
 import org.osgi.framework.Bundle;
@@ -42,7 +60,7 @@ class StartupListener implements FrameworkListener, SynchronousBundleListener {
     public synchronized void frameworkEvent(FrameworkEvent frameworkEvent) {
         if (frameworkEvent.getType() == FrameworkEvent.STARTLEVEL_CHANGED) {
             int defStartLevel = Integer.parseInt(System.getProperty(Constants.FRAMEWORK_BEGINNING_STARTLEVEL));
-            int startLevel = context.getBundle(0).adapt(FrameworkStartLevel.class).getStartLevel();
+            int startLevel = ((FrameworkStartLevel) context.getBundle(0).adapt(FrameworkStartLevel.class)).getStartLevel();
             if (startLevel >= defStartLevel) {
                 context.removeBundleListener(this);
                 context.removeFrameworkListener(this);
