695af8f089baee3e8b66aa9d64ca34f62b182248
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-07 12:17:53 +0000

    [KARAF-1798] Add enterprise and spring features itest
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1381992 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itests/src/test/java/org/apache/karaf/itests/KarafEnterpriseFeaturesTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.itests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public class KarafEnterpriseFeaturesTest extends KarafTestSupport {
+
+    @Test
+    public void installTransactionFeature() throws Exception {
+        executeCommand("feature:install transaction");
+        String transactionFeatureStatus = executeCommand("feature:list -i | grep transaction");
+        assertTrue("transaction feature is not installed", !transactionFeatureStatus.isEmpty());
+    }
+
+    @Test
+    public void installJpaFeature() throws Exception {
+        executeCommand("feature:install jpa");
+        String jpaFeatureStatus = executeCommand("feature:list -i | grep jpa");
+        assertTrue("jpa feature is not installed", !jpaFeatureStatus.isEmpty());
+    }
+
+    @Test
+    public void installJndiFeature() throws Exception {
+        executeCommand("feature:install jndi");
+        String jndiFeatureStatus = executeCommand("feature:list -i | grep jndi");
+        assertTrue("jndi feature is not installed", !jndiFeatureStatus.isEmpty());
+    }
+
+    @Test
+    public void installApplicationWithoutIsolationFeature() throws Exception {
+        executeCommand("feature:install application-without-isolation");
+        String applicationWithoutIsolationFeatureStatus = executeCommand("feature:list -i | grep application-without-isolation");
+        assertTrue("application-without-isolation feature is not installed", !applicationWithoutIsolationFeatureStatus.isEmpty());
+    }
+
+}
--- itests/src/test/java/org/apache/karaf/itests/KarafStandardFeaturesTest.java
+++ itests/src/test/java/org/apache/karaf/itests/KarafStandardFeaturesTest.java
@@ -20,8 +20,6 @@ import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4TestRunner.class)
@@ -55,77 +53,77 @@ public class KarafStandardFeaturesTest extends KarafTestSupport {
 
     @Test
     public void installWrapperFeature() throws Exception {
-        System.out.println(executeCommand("feature:install wrapper"));
+        executeCommand("feature:install wrapper");
         String wrapperFeatureStatus = executeCommand("feature:list -i | grep wrapper");
         assertTrue("wrapper feature is not installed", !wrapperFeatureStatus.isEmpty());
     }
 
     @Test
     public void installObrFeature() throws Exception {
-        System.out.println(executeCommand("feature:install obr"));
+        executeCommand("feature:install obr");
         String obrFeatureStatus = executeCommand("feature:list -i | grep obr");
         assertTrue("obr feature is not installed", !obrFeatureStatus.isEmpty());
     }
 
     @Test
     public void installJettyFeature() throws Exception {
-        System.out.println(executeCommand("feature:install jetty"));
+        executeCommand("feature:install jetty");
         String jettyFeatureStatus = executeCommand("feature:list -i | grep jetty");
         assertTrue("jetty feature is not installed", !jettyFeatureStatus.isEmpty());
     }
 
     @Test
     public void installHttpFeature() throws Exception {
-        System.out.println(executeCommand("feature:install http"));
+        executeCommand("feature:install http");
         String httpFeatureStatus = executeCommand("feature:list -i | grep http");
         assertTrue("http feature is not installed", !httpFeatureStatus.isEmpty());
     }
 
     @Test
     public void installHttpWhiteboardFeature() throws Exception {
-        System.out.println(executeCommand("feature:install http-whiteboard"));
+        executeCommand("feature:install http-whiteboard");
         String httpWhiteboardFeatureStatus = executeCommand("feature:list -i | grep http-whiteboard");
         assertTrue("http-whiteboard feature is not installed", !httpWhiteboardFeatureStatus.isEmpty());
     }
 
     @Test
     public void installWarFeature() throws Exception {
-        System.out.println(executeCommand("feature:install war"));
+        executeCommand("feature:install war");
         String warFeatureStatus = executeCommand("feature:list -i | grep war");
         assertTrue("war feature is not installed", !warFeatureStatus.isEmpty());
     }
 
     @Test
     public void installWebConsoleFeature() throws Exception {
-        System.out.println(executeCommand("feature:install webconsole"));
+        executeCommand("feature:install webconsole");
         String webConsoleFeatureStatus = executeCommand("feature:list -i | grep webconsole");
         assertTrue("webconsole feature is not installed", !webConsoleFeatureStatus.isEmpty());
     }
 
     @Test
     public void installSchedulerFeature() throws Exception {
-        System.out.println(executeCommand("feature:install scheduler"));
+        executeCommand("feature:install scheduler");
         String schedulerFeatureStatus = executeCommand("feature:list -i | grep scheduler");
         assertTrue("scheduler feature is not installed", !schedulerFeatureStatus.isEmpty());
     }
 
     @Test
     public void installEventAdminFeature() throws Exception {
-        System.out.println(executeCommand("feature:install eventadmin"));
+        executeCommand("feature:install eventadmin");
         String eventAdminFeatureStatus = executeCommand("feature:list -i | grep eventadmin");
         assertTrue("eventadmin feature is not installed", !eventAdminFeatureStatus.isEmpty());
     }
 
     @Test
     public void installJasyptEncryptionFeature() throws Exception {
-        System.out.println(executeCommand("feature:install jasypt-encryption"));
+        executeCommand("feature:install jasypt-encryption");
         String jasyptEncryptionFeatureStatus = executeCommand("feature:list -i | grep jasypt-encryption");
         assertTrue("jasypt-encryption feature is not installed", !jasyptEncryptionFeatureStatus.isEmpty());
     }
 
     @Test
     public void installScrFeature() throws Exception {
-        System.out.println(executeCommand("feature:install scr"));
+        executeCommand("feature:install scr");
         String scrFeatureStatus = executeCommand("feature:list -i | grep scr");
         assertTrue("scr feature is not installed", !scrFeatureStatus.isEmpty());
     }
