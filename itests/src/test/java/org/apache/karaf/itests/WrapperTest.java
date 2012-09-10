e9cc3a5695b205cdf6ecbe48b2d67d643423493d
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-10 19:56:52 +0000

    [KARAF-1798] Add wrapper MBean itest
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1383089 13f79535-47bb-0310-9956-ffa450edef68

--- itests/src/test/java/org/apache/karaf/itests/WrapperTest.java
+++ itests/src/test/java/org/apache/karaf/itests/WrapperTest.java
@@ -20,11 +20,15 @@ import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+
 import static org.junit.Assert.assertFalse;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
-public class KarafWrapperCommandsTest extends KarafTestSupport {
+public class WrapperTest extends KarafTestSupport {
 
     @Before
     public void installWrapperFeature() throws Exception {
@@ -32,10 +36,24 @@ public class KarafWrapperCommandsTest extends KarafTestSupport {
     }
 
     @Test
-    public void install() throws Exception {
+    public void installCommand() throws Exception {
         String installOutput = executeCommand("wrapper:install");
         System.out.println(installOutput);
         assertFalse(installOutput.isEmpty());
+    }
+
+    @Test
+    public void installViaMBean() throws Exception {
+        JMXConnector connector = null;
+        try {
+            connector = this.getJMXConnector();
+            MBeanServerConnection connection = connector.getMBeanServerConnection();
+            ObjectName name = new ObjectName("org.apache.karaf:type=wrapper,name=root");
+            connection.invoke(name, "install", new Object[]{}, new String[]{});
+        } finally {
+            if (connector != null)
+                connector.close();
+        }
     }
 
 }
