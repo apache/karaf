6f39b4b1b2fc69aa3e9a8bdb90c26d0512ab77af
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-10 13:15:32 +0000

    [KARAF-1798] Add MBean itest for config and diagnostic
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1382819 13f79535-47bb-0310-9956-ffa450edef68

--- itests/src/test/java/org/apache/karaf/itests/ConfigTest.java
+++ itests/src/test/java/org/apache/karaf/itests/ConfigTest.java
@@ -19,20 +19,46 @@ import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
-public class KarafConfigCommandsTest extends KarafTestSupport {
+public class ConfigTest extends KarafTestSupport {
 
     @Test
-    public void testConfigListCommand() throws Exception {
+    public void listCommand() throws Exception {
         String configListOutput = executeCommand("config:list");
         System.out.println(configListOutput);
         assertFalse(configListOutput.isEmpty());
         configListOutput = executeCommand("config:list \"(service.pid=org.apache.karaf.features)\"");
         System.out.println(configListOutput);
         assertFalse(configListOutput.isEmpty());
+    }
+
+    @Test
+    public void configsViaMBean() throws Exception {
+        JMXConnector connector = null;
+        try {
+            connector = this.getJMXConnector();
+            MBeanServerConnection connection = connector.getMBeanServerConnection();
+            ObjectName name = new ObjectName("org.apache.karaf:type=config,name=root");
+            List<String> configs = (List<String>) connection.getAttribute(name, "Configs");
+            assertTrue(configs.size() > 0);
+            assertTrue(configs.contains("org.apache.karaf.features"));
+            Map<String, String> properties = (Map<String, String>) connection.invoke(name, "listProperties", new Object[]{ "org.apache.karaf.features" }, new String[]{ "java.lang.String" });
+            assertTrue(properties.keySet().size() > 0);
+        } finally {
+            if (connector != null)
+                connector.close();
+        }
     }
 
 }
--- itests/src/test/java/org/apache/karaf/itests/DiagnosticTest.java
+++ itests/src/test/java/org/apache/karaf/itests/DiagnosticTest.java
@@ -19,17 +19,35 @@ import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
-public class KarafDiagCommandsTest extends KarafTestSupport {
+public class DiagnosticTest extends KarafTestSupport {
 
     @Test
-    public void testDumpCreateCommand() throws Exception {
+    public void dumpCreateCommand() throws Exception {
         String dumpCreateOutput = executeCommand("dev:dump-create");
         System.out.println(dumpCreateOutput);
         assertTrue(dumpCreateOutput.contains("Diagnostic dump created."));
+    }
+
+    @Test
+    public void createDumpViaMBean() throws Exception {
+        JMXConnector connector = null;
+        try {
+            connector = this.getJMXConnector();
+            MBeanServerConnection connection = connector.getMBeanServerConnection();
+            ObjectName name = new ObjectName("org.apache.karaf:type=diagnostic,name=root");
+            connection.invoke(name, "createDump", new Object[]{ "itest" }, new String[]{ "java.lang.String" });
+        } finally {
+            if (connector != null)
+                connector.close();
+        }
     }
 
 }
