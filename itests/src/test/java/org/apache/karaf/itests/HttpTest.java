e47e2230b9b33069bdce9caa3b39ba4eace1605c
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-10 13:47:14 +0000

    [KARAF-1798] Add http management itest
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1382839 13f79535-47bb-0310-9956-ffa450edef68

--- itests/src/test/java/org/apache/karaf/itests/HttpTest.java
+++ itests/src/test/java/org/apache/karaf/itests/HttpTest.java
@@ -14,17 +14,23 @@
 package org.apache.karaf.itests;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.openmbean.TabularData;
+import javax.management.remote.JMXConnector;
+
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
-public class KarafHttpCommandsTest extends KarafTestSupport {
+public class HttpTest extends KarafTestSupport {
 
     @Before
     public void installHttpFeature() {
@@ -33,10 +39,27 @@ public class KarafHttpCommandsTest extends KarafTestSupport {
     }
 
     @Test
-    public void httpListCommand() throws Exception {
-        String httpListOutput = executeCommand("http:list");
-        System.out.println(httpListOutput);
-        assertTrue(httpListOutput.contains("/system/console"));
+    public void list() throws Exception {
+        String listOutput = executeCommand("http:list");
+        System.out.println(listOutput);
+        assertTrue(listOutput.contains("/system/console"));
+    }
+
+    @Ignore
+    @Test
+    // TODO remove ignore flag when the HttpMBean is fixed
+    public void listViaMBean() throws Exception {
+        JMXConnector connector = null;
+        try {
+            connector = this.getJMXConnector();
+            MBeanServerConnection connection = connector.getMBeanServerConnection();
+            ObjectName name = new ObjectName("org.apache.karaf:type=http,name=root");
+            TabularData servlets = (TabularData) connection.getAttribute(name, "Servlets");
+            assertTrue(servlets.size() > 0);
+        } finally {
+            if (connector != null)
+                connector.close();
+        }
     }
 
 }
