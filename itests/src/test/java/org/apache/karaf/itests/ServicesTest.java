b129157b1c357a1d77c73dda2ea7718fdabf41a8
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-14 13:49:07 +0000

    [KARAF-1798] Add services itests
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1384786 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itests/src/test/java/org/apache/karaf/itests/ServicesTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.itests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.openmbean.TabularData;
+import javax.management.remote.JMXConnector;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public class ServicesTest extends KarafTestSupport {
+
+    @Test
+    public void listCommand() throws Exception {
+        String listOutput = executeCommand("ls");
+        System.out.println(listOutput);
+        assertFalse(listOutput.isEmpty());
+    }
+
+    @Test
+    public void listViaMBean() throws Exception {
+        JMXConnector connector = null;
+        try {
+            connector = this.getJMXConnector();
+            MBeanServerConnection connection = connector.getMBeanServerConnection();
+            ObjectName name = new ObjectName("org.apache.karaf:type=services,name=root");
+            TabularData services = (TabularData) connection.invoke(name, "list", new Object[]{ }, new String[]{ });
+            assertTrue(services.size() > 0);
+        } finally {
+            if (connector != null)
+                connector.close();
+        }
+    }
+
+}
