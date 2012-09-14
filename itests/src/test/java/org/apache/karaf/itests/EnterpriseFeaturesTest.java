f4aed48a69f66060ba621207dcfaa909c3ba23cf
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-14 13:03:10 +0000

    [KARAF-1798] Add enterprise features itests
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1384752 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itests/src/test/java/org/apache/karaf/itests/EnterpriseFeaturesTest.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.itests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+
+import static org.junit.Assert.assertFalse;
+
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public class EnterpriseFeaturesTest extends KarafTestSupport {
+
+    @Test
+    public void testTransactionFeature() throws Exception {
+        System.out.println(executeCommand("feature:install transaction"));
+        String transactionFeatureStatus = executeCommand("feature:list | grep transaction");
+        System.out.println(transactionFeatureStatus);
+        assertFalse("transaction feature is not installed", transactionFeatureStatus.isEmpty());
+    }
+
+    @Test
+    public void testJpaFeature() throws Exception {
+        System.out.println(executeCommand("feature:install jpa"));
+        String jpaFeatureStatus = executeCommand("feature:list | grep jpa");
+        System.out.println(jpaFeatureStatus);
+        assertFalse("jpa feature is not installed", jpaFeatureStatus.isEmpty());
+    }
+
+    @Test
+    public void testJndiFeature() throws Exception {
+        System.out.println(executeCommand("feature:install jndi"));
+        String jndiFeatureStatus = executeCommand("feature:list | grep jndi");
+        System.out.println(jndiFeatureStatus);
+        assertFalse("jndi feature is not installed", jndiFeatureStatus.isEmpty());
+    }
+
+    @Test
+    public void testApplicationWithoutIsolationFeature() throws Exception {
+        System.out.println(executeCommand("feature:install application-without-isolation"));
+        String applicationWithoutIsolationFeatureStatus = executeCommand("feature:list | grep application-without-isolation");
+        System.out.println(applicationWithoutIsolationFeatureStatus);
+        assertFalse("application-without-isolation feature is not installed", applicationWithoutIsolationFeatureStatus.isEmpty());
+    }
+
+}
