5bf18f6327b5792dbf706cadbc94c4aa8c7d740c
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-18 12:28:18 +0000

    [KARAF-1825] Add startLevel attribute and rename the others operation for migration purpose
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1387114 13f79535-47bb-0310-9956-ffa450edef68

--- management/mbeans/system/src/main/java/org/apache/karaf/management/mbeans/system/SystemMBean.java
+++ management/mbeans/system/src/main/java/org/apache/karaf/management/mbeans/system/SystemMBean.java
@@ -21,11 +21,98 @@ package org.apache.karaf.management.mbeans.system;
  */
 public interface SystemMBean {
 
+    /**
+     * Shutdown the Karaf instance.
+     *
+     * @throws Exception
+     */
+    void halt() throws Exception;
+
+    /**
+     * Shutdown the Karaf instance at a certain time.
+     *
+     * @param time the time when to shutdown the Karaf instance.
+     * @throws Exception
+     */
+    void halt(String time) throws Exception;
+
+    /**
+     * Reboot the Karaf instance.
+     *
+     * @throws Exception
+     */
+    void reboot() throws Exception;
+
+    /**
+     * Reboot the Karaf instance at a certain time and eventually cleanup the instance files.
+     *
+     * @param time the time when to reboot the Karaf instance.
+     * @param clean true to clean the instance files, false else.
+     * @throws Exception
+     */
+    void reboot(String time, boolean clean) throws Exception;
+
+    /**
+     * Get the name of the Karaf instance.
+     *
+     * @return the Karaf instance name.
+     */
     String getName();
+
+    /**
+     * Set the name of the Karaf instance.
+     *
+     * @param name the new Karaf instance name. NB: the change require an instance reboot.
+     */
     void setName(String name);
 
+    /**
+     * Get the Karaf version used by the Karaf instance.
+     *
+     * @return the Karaf version.
+     */
     String getVersion();
 
+    /**
+     * Get the current OSGi framework in use.
+     *
+     * @return the name of the OSGi framework in use.
+     */
+    String getFramework();
+
+    /**
+     * Change the OSGi framework to run Karaf.
+     *
+     * @param framework the name of the framework to use (felix or equinox).
+     */
+    void setFramework(String framework) throws Exception;
+
+    /**
+     * Enable or disable the debug option on the OSGi framework.
+     *
+     * @param debug true to enable debug, false else.
+     */
+    void setFrameworkDebug(boolean debug) throws Exception;
+
+    /**
+     * Get the system start level.
+     *
+     * @return the current system start level.
+     */
+    int getStartLevel();
+
+    /**
+     * Set the system start level.
+     *
+     * @param startLevel the new system start level.
+     */
+    void setStartLevel(int startLevel);
+
+    /* for backward compatibility */
+
+    /**
+     * @deprecated use halt() instead.
+     */
     void shutdown() throws Exception;
 
 }
