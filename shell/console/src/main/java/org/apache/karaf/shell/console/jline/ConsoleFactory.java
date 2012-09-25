74d76f107b2846115dd4290b093b46839434acdf
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-09-25 07:05:00 +0000

    [KARAF-1845] Set sshd agent as optional in order to be able to start the minimal distribution
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1389730 13f79535-47bb-0310-9956-ffa450edef68

--- shell/console/src/main/java/org/apache/karaf/shell/console/jline/ConsoleFactory.java
+++ shell/console/src/main/java/org/apache/karaf/shell/console/jline/ConsoleFactory.java
@@ -161,12 +161,7 @@ public class ConsoleFactory {
 
     protected String startAgent(String user) {
         try {
-            local = new AgentImpl();
-            URL url = bundleContext.getBundle().getResource("karaf.key");
-            InputStream is = url.openStream();
-            ObjectInputStream r = new ObjectInputStream(is);
-            KeyPair keyPair = (KeyPair) r.readObject();
-            local.addIdentity(keyPair, "karaf");
+            local = SshAgentLoader.load(bundleContext);
             String agentId = "local:" + user;
             Hashtable properties = new Hashtable();
             properties.put("id", agentId);
@@ -207,4 +202,22 @@ public class ConsoleFactory {
             return stream;
         }
     }
+
+    static class SshAgentLoader {
+        static SshAgent load(BundleContext bundleContext) {
+            try {
+                SshAgent agent = new AgentImpl();
+                URL url = bundleContext.getBundle().getResource("karaf.key");
+                InputStream is = url.openStream();
+                ObjectInputStream r = new ObjectInputStream(is);
+                KeyPair keyPair = (KeyPair) r.readObject();
+                agent.addIdentity(keyPair, "karaf");
+                return agent;
+            } catch (Throwable e) {
+                LOGGER.warn("Error starting ssh agent for local console", e);
+                return null;
+            }
+        }
+    }
+
 }
