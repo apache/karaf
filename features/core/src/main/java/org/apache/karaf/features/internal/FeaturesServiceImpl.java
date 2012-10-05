4b547e2fbcffba559fbe059307385617de6bdc9c
Jean-Baptiste Onofre <jbonofre@apache.org>
2012-10-05 07:47:43 +0000

    [KARAF-1796] Change the install/start order of features
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1394398 13f79535-47bb-0310-9956-ffa450edef68

--- features/core/src/main/java/org/apache/karaf/features/internal/FeaturesServiceImpl.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/FeaturesServiceImpl.java
@@ -582,10 +582,17 @@ public class FeaturesServiceImpl implements FeaturesService, FrameworkListener {
                     configFile.getFinalname(), configFile.isOverride(), verbose);
         }
         Set<Long> bundles = new TreeSet<Long>();
+        List<InstallResult> installResultList = new LinkedList<InstallResult>();
         for (BundleInfo bInfo : resolve(feature)) {
-            Bundle b = installBundleIfNeeded(state, bInfo, verbose);
-            bundles.add(b.getBundleId());
-            state.bundleInfos.put(b.getBundleId(), bInfo);
+            InstallResult result = installBundleIfNeeded(state, bInfo, verbose);
+            bundles.add(result.getBundle().getBundleId());
+            state.bundleInfos.put(result.getBundle().getBundleId(), bInfo);
+            installResultList.add(result);
+        }
+        for (InstallResult result : installResultList) {
+            if (!result.isPreviouslyInstalled()) {
+                startBundleIfNeeded(result.getBundle(), result.getStartLevel());
+            }
         }
         state.features.put(feature, bundles);
     }
@@ -750,7 +757,33 @@ public class FeaturesServiceImpl implements FeaturesService, FrameworkListener {
         return result;
     }
 
-    protected Bundle installBundleIfNeeded(InstallationState state, BundleInfo bundleInfo, boolean verbose) throws IOException, BundleException {
+    protected static class InstallResult {
+
+        private final boolean previouslyInstalled;
+        private final Bundle bundle;
+        private final int startLevel;
+
+        protected InstallResult(boolean previouslyInstalled, Bundle bundle, int startLevel) {
+            this.previouslyInstalled = previouslyInstalled;
+            this.bundle = bundle;
+            this.startLevel = startLevel;
+        }
+
+        public boolean isPreviouslyInstalled() {
+            return previouslyInstalled;
+        }
+
+        public Bundle getBundle() {
+            return bundle;
+        }
+
+        public int getStartLevel() {
+            return startLevel;
+        }
+
+    }
+
+    protected InstallResult installBundleIfNeeded(InstallationState state, BundleInfo bundleInfo, boolean verbose) throws IOException, BundleException {
         InputStream is;
         String bundleLocation = bundleInfo.getLocation();
         LOGGER.debug("Checking " + bundleLocation);
@@ -788,7 +821,7 @@ public class FeaturesServiceImpl implements FeaturesService, FrameworkListener {
                             System.out.println("Found installed bundle: " + b);
                         }
                         state.bundles.add(b);
-                        return b;
+                        return new InstallResult(true, b, 0);
                     }
                 }
             }
@@ -804,17 +837,17 @@ public class FeaturesServiceImpl implements FeaturesService, FrameworkListener {
             }
             Bundle b = getBundleContext().installBundle(bundleLocation, is);
 
-            // Define the startLevel for the bundle when defined
-            int ibsl = bundleInfo.getStartLevel();
-            if (ibsl > 0) {
-                getStartLevel().setBundleStartLevel(b, ibsl);
-            }
-
             state.bundles.add(b);
             state.installed.add(b);
-            return b;
+            return new InstallResult(false, b, bundleInfo.getStartLevel());
         } finally {
             is.close();
+        }
+    }
+
+    private void startBundleIfNeeded(Bundle bundle, int startLevel) {
+        if (startLevel > 0) {
+            getStartLevel().setBundleStartLevel(bundle, startLevel);
         }
     }
 
