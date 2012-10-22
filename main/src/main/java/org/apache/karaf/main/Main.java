bf63956b5710b2a464b1d10f5ea991a868533844
Freeman Yue Fang <ffang@apache.org>
2012-10-22 08:21:48 +0000

    [KARAF-1938]Lock logic should wait for start level change to occur
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.2.x@1400794 13f79535-47bb-0310-9956-ffa450edef68

--- main/src/main/java/org/apache/karaf/main/Main.java
+++ main/src/main/java/org/apache/karaf/main/Main.java
@@ -49,6 +49,7 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
@@ -199,6 +200,8 @@ public class Main {
     private int shutdownTimeout = 5 * 60 * 1000;
     private boolean exiting = false;
     private ShutdownCallback shutdownCallback;
+    private Object startLevelLock = new Object();
+    private StartLevelListener startLevelListener;
 
     public Main(String[] args) {
         this.args = args;
@@ -270,7 +273,15 @@ public class Main {
         // Process properties
         loadStartupProperties(configProps);
         processAutoProperties(framework.getBundleContext());
-        framework.start();
+        
+        // Use a separate class for getting framework events to keep Main signature same
+        // TODO: in a larger release version (not a patch release) we can possibly just 
+        // implement FrameworkListener on Main
+        startLevelListener = new StartLevelListener(startLevelLock);
+        framework.getBundleContext().addFrameworkListener(startLevelListener);
+
+        framework.start();       
+        
         // Start lock monitor
         new Thread() {
             public void run() {
@@ -1302,8 +1313,16 @@ public class Main {
                     Thread.sleep(lockDelay);
                 }
                 if (framework.getState() == Bundle.ACTIVE && !exiting) {
-                    LOG.info("Lost the lock, stopping this instance ...");
-                    setStartLevel(lockStartLevel);
+                    LOG.info("Lost the lock, reducing start level to " + lockStartLevel);
+                    synchronized (startLevelLock) {
+                        setStartLevel(lockStartLevel);
+                        
+                        // we have to wait for the start level to be reduced here because
+                        // if the lock is regained before the start level is fully changed
+                        // things may not come up as expected
+                        LOG.fine("Waiting for start level change to complete...");
+                        startLevelLock.wait(shutdownTimeout);
+                    }
                 }
             } else if (!lockLogged) {
                 LOG.info("Waiting for the lock ...");
@@ -1374,6 +1393,10 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Lock getLock() {
+        return lock;
     }
 
     private class ShutdownSocketThread extends Thread {
