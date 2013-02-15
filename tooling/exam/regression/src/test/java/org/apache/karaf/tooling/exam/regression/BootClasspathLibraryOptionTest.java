1738b09e55853412478da4974684d1e75d71ab33
hkesler <hkesler@unknown>
2013-02-15 16:53:35 +0000

    karaf-2194: changed the assertions in the test to match the file count
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1446685 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/exam/regression/src/test/java/org/apache/karaf/tooling/exam/regression/BootClasspathLibraryOptionTest.java
+++ tooling/exam/regression/src/test/java/org/apache/karaf/tooling/exam/regression/BootClasspathLibraryOptionTest.java
@@ -55,7 +55,7 @@ public class BootClasspathLibraryOptionTest {
                 foundJarFiles++;
             }
         }
-        assertEquals(5, foundJarFiles);
+        assertEquals(6, foundJarFiles);
     }
 
 }
