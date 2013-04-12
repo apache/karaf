a16db6d7971a4b30dc909a905f10b0e7fe01a891
Jean-Baptiste Onofre <jbonofre@apache.org>
2013-04-12 16:57:06 +0000

    Revert help support for Gogo commands (impact on completion)
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1467354 13f79535-47bb-0310-9956-ffa450edef68

--- shell/help/src/main/java/org/apache/karaf/shell/help/impl/SingleCommandHelpProvider.java
+++ shell/help/src/main/java/org/apache/karaf/shell/help/impl/SingleCommandHelpProvider.java
@@ -20,19 +20,12 @@ package org.apache.karaf.shell.help.impl;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.lang.reflect.Field;
-import java.util.Hashtable;
 import java.util.Set;
 
-import org.apache.felix.gogo.runtime.CommandProxy;
 import org.apache.felix.gogo.runtime.CommandSessionImpl;
 import org.apache.felix.service.command.CommandSession;
-import org.apache.felix.service.command.Function;
 import org.apache.felix.service.threadio.ThreadIO;
-import org.apache.karaf.shell.console.BundleContextAware;
 import org.apache.karaf.shell.console.HelpProvider;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 
 public class SingleCommandHelpProvider implements HelpProvider {
 
@@ -56,19 +49,7 @@ public class SingleCommandHelpProvider implements HelpProvider {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             io.setStreams(new ByteArrayInputStream(new byte[0]), new PrintStream(baos, true), new PrintStream(baos, true));
             try {
-                Function function = (Function) session.get(path);
-                function = unproxy(function);
-                if (function != null && function instanceof CommandProxy) {
-                    Hashtable<String, HelpSystem.GogoCommandHelper> helpers = (Hashtable<String, HelpSystem.GogoCommandHelper>) session.get(HelpSystem.GOGO_COMMAND_HELPERS);
-                    if (helpers != null) {
-                        HelpSystem.GogoCommandHelper gogoCommandHelper = helpers.get(path);
-                        if (gogoCommandHelper != null) {
-                            gogoCommandHelper.printUsage(session, session.getConsole());
-                        }
-                    }
-                } else {
-                    session.execute(path + " --help");
-                }
+                session.execute(path + " --help");
             } catch (Throwable t) {
                 t.printStackTrace();
             } finally {
@@ -78,29 +59,4 @@ public class SingleCommandHelpProvider implements HelpProvider {
         }
         return null;
     }
-
-    protected Function unproxy(Function function) {
-        try {
-            if (function.getClass().getName().contains("CommandProxy")) {
-                Field contextField = function.getClass().getDeclaredField("context");
-                Field referenceField = function.getClass().getDeclaredField("reference");
-                contextField.setAccessible(true);
-                referenceField.setAccessible(true);
-                BundleContext context = (BundleContext) contextField.get(function);
-                ServiceReference reference = (ServiceReference) referenceField.get(function);
-                Object target = context.getService(reference);
-                try {
-                    if (target instanceof Function) {
-                        function = (Function) target;
-                    }
-                } finally {
-                    context.ungetService(reference);
-                }
-            }
-        } catch (Throwable t) {
-            // nothing to do
-        }
-        return function;
-    }
-
 }
