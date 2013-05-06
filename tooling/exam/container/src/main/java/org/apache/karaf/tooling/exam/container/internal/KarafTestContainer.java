acac07c5b022fac41ff1c01301ec05a5130c1ff4
Jean-Baptiste Onofre <jbonofre@apache.org>
2013-05-06 08:34:06 +0000

    [KARAF-2306] Define the BootClasspathLibraries before setting classpath in the Exam container
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.x@1479490 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/exam/container/src/main/java/org/apache/karaf/tooling/exam/container/internal/KarafTestContainer.java
+++ tooling/exam/container/src/main/java/org/apache/karaf/tooling/exam/container/internal/KarafTestContainer.java
@@ -140,7 +140,10 @@ public class KarafTestContainer implements TestContainer {
                     Lists.newArrayList("-Dkaraf.startLocalConsole=" + shouldLocalConsoleBeStarted(subsystem),
                             "-Dkaraf.startRemoteShell=" + shouldRemoteShellBeStarted(subsystem));
 
+
+            copyBootClasspathLibraries(karafHome, subsystem);
             String[] classPath = buildKarafClasspath(karafHome);
+
             String main = "org.apache.karaf.main.Main";
             String options = "";
             String[] environment = new String[]{};
@@ -148,7 +151,6 @@ public class KarafTestContainer implements TestContainer {
 
             updateLogProperties(karafHome, subsystem);
             updateUserSetProperties(karafHome, subsystem);
-            copyBootClasspathLibraries(karafHome, subsystem);
             setupExamProperties(karafHome, subsystem);
             makeScriptsInBinExec(karafBin);
 
