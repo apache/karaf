6d2df7a03abda227b4131f3bb23171309afe30dc
Jean-Baptiste Onofre <jbonofre@apache.org>
2013-07-01 20:00:53 +0000

    [KARAF-2370] Add the alias command
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.x@1498652 13f79535-47bb-0310-9956-ffa450edef68

---
+++ shell/commands/src/main/java/org/apache/karaf/shell/commands/AliasAction.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.shell.commands;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.AbstractAction;
+
+@Command(scope = "shell", name = "alias", description = "Create an alias to a command")
+public class AliasAction extends AbstractAction {
+
+    @Argument(index = 0, name = "command", description = "The command to alias, e.g. 'ldn = { log:display -n $args }'", required = true, multiValued = false)
+    private String alias;
+
+    protected Object doExecute() throws Exception {
+        session.execute(alias);
+        return null;
+    }
+
+}
