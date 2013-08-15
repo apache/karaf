67f0006fea8e730d0c051e9c2c5fcb4fc2422bc2
Freeman Yue Fang <ffang@apache.org>
2013-08-15 06:41:57 +0000

    [KARAF-2444]add a empty line check
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1514177 13f79535-47bb-0310-9956-ffa450edef68

--- features/core/src/main/java/org/apache/karaf/features/internal/FeaturesServiceImpl.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/FeaturesServiceImpl.java
@@ -191,7 +191,10 @@ public class FeaturesServiceImpl implements FeaturesService, FrameworkListener {
         String[] s = uris.split(",");
         this.uris = new HashSet<URI>();
         for (String value : s) {
-            this.uris.add(new URI(value.trim()));
+            value = value.trim();
+            if (!value.isEmpty()) {
+                this.uris.add(new URI(value));
+            }
         }
     }
 
