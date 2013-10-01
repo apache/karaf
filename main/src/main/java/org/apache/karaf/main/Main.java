bfab77ebb344f97f6aa81acc550e23ac1b3345d3
Guillaume Nodet <gnodet@apache.org>
2013-10-01 07:30:12 +0000

    [KARAF-2492] Possible UnsatisfiedLinkError when using dev:restart with native libraries
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.x@1527953 13f79535-47bb-0310-9956-ffa450edef68

--- main/src/main/java/org/apache/karaf/main/Main.java
+++ main/src/main/java/org/apache/karaf/main/Main.java
@@ -571,6 +571,8 @@ public class Main {
             } finally {
                 if (!restart) {
                     System.exit(main.getExitCode());
+                } else {
+                    System.gc();
                 }
             }
         }
