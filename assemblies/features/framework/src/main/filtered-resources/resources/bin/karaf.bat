e3659947a2c3b5fd23427f98a8a1261ab4b2d055
Jean-Baptiste Onofre <jbonofre@apache.org>
2013-10-02 18:27:05 +0000

    [KARAF-2435] Add role-based security for the JMX layer
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1528587 13f79535-47bb-0310-9956-ffa450edef68

--- assemblies/features/framework/src/main/filtered-resources/resources/bin/karaf.bat
+++ assemblies/features/framework/src/main/filtered-resources/resources/bin/karaf.bat
@@ -306,7 +306,7 @@ if "%KARAF_PROFILER%" == "" goto :RUN
     SET ARGS=%1 %2 %3 %4 %5 %6 %7 %8
     rem Execute the Java Virtual Machine
     cd "%KARAF_BASE%"
-    "%JAVA%" %JAVA_OPTS% %OPTS% -classpath "%CLASSPATH%" -Djava.endorsed.dirs="%JAVA_HOME%\jre\lib\endorsed;%JAVA_HOME%\lib\endorsed;%KARAF_HOME%\lib\endorsed" -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%JAVA_HOME%\lib\ext;%KARAF_HOME%\lib\ext" -Dkaraf.instances="%KARAF_HOME%\instances" -Dkaraf.home="%KARAF_HOME%" -Dkaraf.base="%KARAF_BASE%" -Djava.io.tmpdir="%KARAF_DATA%\tmp" -Dkaraf.data="%KARAF_DATA%" -Djava.util.logging.config.file="%KARAF_BASE%\etc\java.util.logging.properties" %KARAF_OPTS% %MAIN% %ARGS%
+    "%JAVA%" %JAVA_OPTS% %OPTS% -classpath "%CLASSPATH%" -Djava.endorsed.dirs="%JAVA_HOME%\jre\lib\endorsed;%JAVA_HOME%\lib\endorsed;%KARAF_HOME%\lib\endorsed" -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%JAVA_HOME%\lib\ext;%KARAF_HOME%\lib\ext" -Dkaraf.instances="%KARAF_HOME%\instances" -Dkaraf.home="%KARAF_HOME%" -Dkaraf.base="%KARAF_BASE%" -Djava.io.tmpdir="%KARAF_DATA%\tmp" -Dkaraf.data="%KARAF_DATA%" -Djava.util.logging.config.file="%KARAF_BASE%\etc\java.util.logging.properties" -Djavax.management.builder.initial=org.apache.karaf.management.boot.KarafMBeanServerBuilder %KARAF_OPTS% %MAIN% %ARGS%
 
 rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
---
+++ management/boot/src/test/java/org/apache/karaf/management/boot/KarafMBeanServerBuilderTest.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.management.boot;
+
+import junit.framework.TestCase;
+import org.easymock.EasyMock;
+
+import javax.management.AttributeList;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KarafMBeanServerBuilderTest extends TestCase {
+
+    public void testMBeanServerBuilderBlocking() throws Exception {
+        MBeanServer mbs = EasyMock.createMock(MBeanServer.class);
+        EasyMock.replay(mbs);
+
+        KarafMBeanServerBuilder mbsb = new KarafMBeanServerBuilder();
+        MBeanServer kmbs = mbsb.newMBeanServer("test", mbs, null);
+
+        final List<Object> handlerArgs = new ArrayList<Object>();
+        InvocationHandler guard = new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                handlerArgs.add(proxy);
+                handlerArgs.add(method);
+                handlerArgs.add(args);
+
+                throw new SecurityException("Access Denied");
+            }
+        };
+
+        KarafMBeanServerBuilder.setGuard(guard);
+
+        assertEquals("Precondition", 0, handlerArgs.size());
+
+        ObjectName on = ObjectName.getInstance("foo.bar:type=TestObject");
+
+        try {
+            kmbs.getAttribute(on, "myAttr");
+            fail("Should have access denied");
+        } catch (SecurityException se) {
+            // good
+            assertEquals(3, handlerArgs.size());
+            assertSame(kmbs, handlerArgs.get(0));
+            assertEquals("getAttribute", ((Method) handlerArgs.get(1)).getName());
+            Object[] args = (Object[]) handlerArgs.get(2);
+            assertEquals(2, args.length);
+            assertSame(on, args[0]);
+            assertEquals("myAttr", args[1]);
+        }
+
+        try {
+            kmbs.getAttributes(on, new String[]{"foo", "bar"});
+            fail("Should have access denied");
+        } catch (SecurityException se) {
+            // good
+        }
+
+        try {
+            kmbs.getAttributes(on, new String[]{ "goo", "far" });
+            fail("Should have access denied");
+        } catch (SecurityException se) {
+            // good
+        }
+
+        try {
+            kmbs.setAttributes(on, new AttributeList());
+            fail("Should have access denied");
+        } catch (SecurityException se) {
+            // good
+        }
+
+        try {
+            kmbs.setAttributes(on, new AttributeList());
+            fail("Should have access denied");
+        } catch (SecurityException se) {
+            // good
+        }
+
+        // try some MBeanServer operations that are not guarded
+        assertTrue(kmbs.getDomains().length > 0);
+        assertTrue(kmbs.getMBeanCount() > 0);
+        assertTrue(kmbs.getDefaultDomain().length() > 0);
+    }
+
+    public void testMBeanServerBuilderNonBlocking() throws Exception {
+        MBeanServer mbs = EasyMock.createMock(MBeanServer.class);
+        EasyMock.replay(mbs);
+
+        KarafMBeanServerBuilder mbsb = new KarafMBeanServerBuilder();
+        MBeanServer kmbs = mbsb.newMBeanServer("test", mbs, null);
+
+        final List<Object> handlerArgs = new ArrayList<Object>();
+        InvocationHandler guard = new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                handlerArgs.add(proxy);
+                handlerArgs.add(method);
+                handlerArgs.add(args);
+                return null;
+            }
+        };
+
+        KarafMBeanServerBuilder.setGuard(guard);
+
+        assertEquals("Precondition", 0, handlerArgs.size());
+
+        ObjectName on = ObjectName.getInstance("foo.bar:type=TestObject");
+
+        try {
+            kmbs.getAttribute(on, "myAttr");
+        } catch (Exception e) {
+            Throwable th = getInnermostException(e);
+            assertTrue("Expected exception as the object in question is not registered with the MBeanServer", th instanceof InstanceNotFoundException);
+            // good
+            assertEquals(3, handlerArgs.size());
+            assertSame(kmbs, handlerArgs.get(0));
+            assertEquals("getAttribute", ((Method) handlerArgs.get(1)).getName());
+            Object[] args = (Object[]) handlerArgs.get(2);
+            assertEquals(2, args.length);
+            assertSame(on, args[0]);
+            assertEquals("myAttr", args[1]);
+        }
+    }
+
+    private Throwable getInnermostException(Throwable th) {
+        if (th.getCause() != null) {
+            return getInnermostException(th.getCause());
+        } else {
+            return th;
+        }
+    }
+
+}
