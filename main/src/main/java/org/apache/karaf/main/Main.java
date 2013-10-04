c63d20e12d06ca9a72787b878bc0084b996a2d7e
Guillaume Nodet <gnodet@apache.org>
2013-10-04 07:13:01 +0000

    [KARAF-2499] Karaf should exit if the data directory is deleted
    git-svn-id: https://svn.apache.org/repos/asf/karaf/branches/karaf-2.3.x@1529093 13f79535-47bb-0310-9956-ffa450edef68

--- main/src/main/java/org/apache/karaf/main/Main.java
+++ main/src/main/java/org/apache/karaf/main/Main.java
@@ -1390,6 +1390,7 @@ public class Main {
     }
 
     private void doLock(Properties props) throws Exception {
+        File dataDir = new File(System.getProperty(PROP_KARAF_DATA));
         String clz = props.getProperty(PROPERTY_LOCK_CLASS, PROPERTY_LOCK_CLASS_DEFAULT);
         lock = (Lock) Class.forName(clz).getConstructor(Properties.class).newInstance(props);
         boolean lockLogged = false;
@@ -1402,6 +1403,12 @@ public class Main {
                 setupShutdown(props);
                 setStartLevel(defaultStartLevel);
                 for (;;) {
+                    if (!dataDir.isDirectory()) {
+                        LOG.info("Data directory does not exist anymore, halting");
+                        framework.stop();
+                        System.exit(-1);
+                        return;
+                    }
                     if (!lock.isAlive()) {
                         break;
                     }
