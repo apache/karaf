6a8b9ceb3b7c48088e1ea6d4816e5e25389e97c2
Jean-Baptiste Onofre <jbonofre@apache.org>
2013-11-07 20:49:47 +0000

    Echo a message when enabling ACL support
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1539810 13f79535-47bb-0310-9956-ffa450edef68

--- assemblies/features/framework/src/main/filtered-resources/resources/bin/karaf.bat
+++ assemblies/features/framework/src/main/filtered-resources/resources/bin/karaf.bat
@@ -315,7 +315,7 @@ if "%KARAF_PROFILER%" == "" goto :RUN
     if "%KARAF_ACL%" == "" (
         "%JAVA%" %JAVA_OPTS% %OPTS% -classpath "%CLASSPATH%" -Djava.endorsed.dirs="%JAVA_HOME%\jre\lib\endorsed;%JAVA_HOME%\lib\endorsed;%KARAF_HOME%\lib\endorsed" -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%JAVA_HOME%\lib\ext;%KARAF_HOME%\lib\ext" -Dkaraf.instances="%KARAF_HOME%\instances" -Dkaraf.home="%KARAF_HOME%" -Dkaraf.base="%KARAF_BASE%" -Djava.io.tmpdir="%KARAF_DATA%\tmp" -Dkaraf.data="%KARAF_DATA%" -Djava.util.logging.config.file="%KARAF_BASE%\etc\java.util.logging.properties" %KARAF_OPTS% %MAIN% %ARGS%
     ) else (
-        rem Enabling Security ACL support
+        echo Enabling Security ACL support
         "%JAVA%" %JAVA_OPTS% %OPTS% -classpath "%CLASSPATH%" -Djava.endorsed.dirs="%JAVA_HOME%\jre\lib\endorsed;%JAVA_HOME%\lib\endorsed;%KARAF_HOME%\lib\endorsed" -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%JAVA_HOME%\lib\ext;%KARAF_HOME%\lib\ext" -Dkaraf.instances="%KARAF_HOME%\instances" -Dkaraf.home="%KARAF_HOME%" -Dkaraf.base="%KARAF_BASE%" -Djava.io.tmpdir="%KARAF_DATA%\tmp" -Dkaraf.data="%KARAF_DATA%" -Djava.util.logging.config.file="%KARAF_BASE%\etc\java.util.logging.properties" -Djavax.management.builder.initial=org.apache.karaf.management.boot.KarafMBeanServerBuilder %KARAF_OPTS% %MAIN% %ARGS%
     )
 
