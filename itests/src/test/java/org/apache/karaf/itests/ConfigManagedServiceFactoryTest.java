9fb3571c7b03e6c29448a22c975733e058abf48f
Christian Schneider <chris@die-schneider.net>
2014-01-10 10:24:54 +0100

    KARAF-2676 Adding failing test for issue with thanks to Achim Finke

---
+++ itests/src/test/java/org/apache/karaf/itests/ConfigManagedServiceFactoryTest.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.itests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.apache.karaf.jaas.boot.principal.RolePrincipal;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class ConfigManagedServiceFactoryTest extends KarafTestSupport {  
+    
+    @Inject
+    ConfigurationAdmin configAdmin;
+    
+    @Test
+    public void updateProperties() throws IOException, InvalidSyntaxException {
+        executeCommand("config:edit myconfig-itest \n" +
+                       "config:property-set test1 test \n" +
+                       "config:property-set test2 test \n" +
+                       "config:update", new RolePrincipal("manager"));
+        Configuration config = readConfig();
+        assertThatPropertiesAreCorrect(config);
+        
+        executeCommand("config:edit myconfig-itest \n" +
+            "config:property-set test1 test \n" +
+            "config:update", new RolePrincipal("manager"));
+        config = readConfig();
+        // fails at property test2. See KARAF-2676
+        assertThatPropertiesAreCorrect(config);
+    }
+
+    private void assertThatPropertiesAreCorrect(Configuration config) {
+        assertEquals("test", config.getProperties().get("test1"));
+        assertEquals("test", config.getProperties().get("test2"));
+    }
+
+    private Configuration readConfig() throws IOException, InvalidSyntaxException {
+        Configuration[]configs = configAdmin.listConfigurations("(service.factorypid=myconfig)");
+        return configs[0];
+    }
+
+}
