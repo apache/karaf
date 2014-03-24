2e085742a52619554582c5624640a942d9092981
Jonathan Anstey <janstey@gmail.com>
2014-03-24 11:27:48 -0230

    Fallback to empty string in case KARAF_OPTS is null

--- instance/core/src/main/java/org/apache/karaf/instance/core/internal/InstanceServiceImpl.java
+++ instance/core/src/main/java/org/apache/karaf/instance/core/internal/InstanceServiceImpl.java
@@ -412,7 +412,8 @@ public class InstanceServiceImpl implements InstanceService {
                 }
 
                 // fallback and read karafOpts from KARAF_OPTS environment if no System property present
-                String karafOpts = System.getProperty("karaf.opts", System.getenv("KARAF_OPTS"));
+                String karafOptsEnv = System.getenv("KARAF_OPTS");
+                String karafOpts = System.getProperty("karaf.opts", karafOptsEnv != null ? karafOptsEnv : "");
 
                 File libDir = new File(System.getProperty("karaf.home"), "lib");
                 File[] jars = libDir.listFiles(new FilenameFilter() {
