3fe68988b2bf41808cd7647ebc82ae4092b8191a
Guillaume Nodet <gnodet@gmail.com>
2014-04-14 11:06:40 +0200

    [KARAF-2833] Make jdbc independent of blueprint. Note that blueprint is still required to deploy the datasources

---
+++ jdbc/src/main/java/org/apache/karaf/jdbc/internal/osgi/Activator.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.jdbc.internal.osgi;
+
+import org.apache.karaf.jdbc.JdbcService;
+import org.apache.karaf.jdbc.internal.JdbcMBeanImpl;
+import org.apache.karaf.jdbc.internal.JdbcServiceImpl;
+import org.apache.karaf.util.tracker.BaseActivator;
+
+public class Activator extends BaseActivator {
+    @Override
+    protected void doStart() throws Exception {
+        JdbcServiceImpl service = new JdbcServiceImpl();
+        service.setBundleContext(bundleContext);
+        register(JdbcService.class, service);
+
+        JdbcMBeanImpl mbean = new JdbcMBeanImpl();
+        mbean.setJdbcService(service);
+        registerMBean(mbean, "type=jdbc");
+    }
+}
