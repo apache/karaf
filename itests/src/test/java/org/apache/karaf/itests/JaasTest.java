39fe5563261a7a3a7b0865d6eb008ec1f2478f50
Freeman Fang <freeman.fang@gmail.com>
2014-04-15 16:36:05 +0800

    [KARAF-2910]add a testcase (cherry picked from commit 70d14e6e10560e5128a9637b3d135948aa42c1ce)

--- itests/src/test/java/org/apache/karaf/itests/JaasTest.java
+++ itests/src/test/java/org/apache/karaf/itests/JaasTest.java
@@ -13,11 +13,28 @@
  */
 package org.apache.karaf.itests;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginContext;
+import org.apache.felix.fileinstall.ArtifactInstaller;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+
+import org.osgi.framework.BundleContext;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
@@ -29,5 +46,45 @@ public class JaasTest extends KarafTestSupport {
         assertContains("PropertiesLoginModule", listRealmsOutput);
         assertContains("PublickeyLoginModule", listRealmsOutput);
     }
+
+    @Ignore
+    //ignore it as this is too time consuming
+    public void testLoginNoLeak() throws Exception {
+        for (int i = 0; i<200000; i++) {
+            doLogin();
+        }
+    }
+
+    @Inject
+    protected BundleContext bundleContext;
+
+    @Test  // shows the leak afaics
+    public void testLoginSingleReg() throws Exception {
+        for (int i=0; i<10; i++) {
+            doLogin();
+        }
+        assertEquals(3, bundleContext.getServiceReferences(ArtifactInstaller.class.getName(), null).length);
+    }
+
+    private void doLogin() throws Exception {
+        final String userPassRealm = "karaf";
+        LoginContext lc = new LoginContext(userPassRealm, new CallbackHandler() {
+            public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+                for (int i = 0; i < callbacks.length; i++) {
+                    Callback callback = callbacks[i];
+                    if (callback instanceof PasswordCallback) {
+                        PasswordCallback passwordCallback = (PasswordCallback)callback;
+                        passwordCallback.setPassword(userPassRealm.toCharArray());
+                    } else if (callback instanceof NameCallback) {
+                        NameCallback nameCallback = (NameCallback)callback;
+                        nameCallback.setName(userPassRealm);
+                    }
+                }
+            }
+        });
+        lc.login();
+        assertNotNull(lc.getSubject());
+    }
+
 
 }
