8a3090b5cf2fa3a82b44e3f9681e8964504b146f
Jonathan Anstey <janstey@gmail.com>
2014-06-04 16:39:56 -0230

    KARAF-3021 - Keyboard still read after log:tail cancelled

--- log/src/main/java/org/apache/karaf/log/command/LogTail.java
+++ log/src/main/java/org/apache/karaf/log/command/LogTail.java
@@ -46,8 +46,9 @@ public class LogTail extends DisplayLog {
     @Override
     public Object execute() throws Exception {
         PrintEventThread printThread = new PrintEventThread();
+        ReadKeyBoardThread readKeyboardThread = new ReadKeyBoardThread(this, Thread.currentThread());
         executorService.execute(printThread);
-        new Thread(new ReadKeyBoardThread(this, Thread.currentThread())).start();
+        executorService.execute(readKeyboardThread);
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(200);
@@ -56,6 +57,7 @@ public class LogTail extends DisplayLog {
             }
         }
         printThread.abort();
+        readKeyboardThread.abort();
         executorService.shutdownNow();  
         return null;      
     }
@@ -63,12 +65,18 @@ public class LogTail extends DisplayLog {
     class ReadKeyBoardThread implements Runnable {
         private LogTail logTail;
         private Thread sessionThread;
+        boolean readKeyboard = true;
         public ReadKeyBoardThread(LogTail logtail, Thread thread) {
             this.logTail = logtail;
             this.sessionThread = thread;
         }
+
+        public void abort() {
+            readKeyboard = false;            
+        }
+
         public void run() {
-            for (;;) {
+            while (readKeyboard) {
                 try {
                     int c = this.logTail.session.getKeyboard().read();
                     if (c < 0) {
