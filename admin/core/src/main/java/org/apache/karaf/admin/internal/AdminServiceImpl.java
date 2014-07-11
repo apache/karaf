f6ebe33c719c2c230d2d281a5e154862e4d56dd0
Freeman Fang <freeman.fang@gmail.com>
2014-07-11 17:38:33 +0800

    [KARAF-3112]make stopTimeout of AdminService configurable

--- admin/core/src/main/java/org/apache/karaf/admin/internal/AdminServiceImpl.java
+++ admin/core/src/main/java/org/apache/karaf/admin/internal/AdminServiceImpl.java
@@ -74,6 +74,8 @@ public class AdminServiceImpl implements AdminService {
     private static final String KARAF_SHUTDOWN_COMMAND = "karaf.shutdown.command";
 
     private static final String KARAF_SHUTDOWN_PID_FILE = "karaf.shutdown.pid.file";
+    
+    private static final String KARAF_SHUTDOWN_TIMEOUT = "karaf.shutdown.timeout";
 
     private static final String DEFAULT_SHUTDOWN_COMMAND = "SHUTDOWN";
 
@@ -657,7 +659,9 @@ public class AdminServiceImpl implements AdminService {
                 Socket s = new Socket(host, port);
                 s.getOutputStream().write(shutdown.getBytes());
                 s.close();
-                long t = System.currentTimeMillis() + getStopTimeout();
+                long stopTimeout = Long.parseLong(props.getProperty(KARAF_SHUTDOWN_TIMEOUT, 
+                                                                    Long.toString(getStopTimeout())));
+                long t = System.currentTimeMillis() + stopTimeout;
                 do {
                     Thread.sleep(100);
                     checkPid(instance);
