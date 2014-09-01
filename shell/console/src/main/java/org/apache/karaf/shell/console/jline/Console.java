71e9ab96d93f81ce8707eb43a12d4010e8b678e4
Jean-Baptiste Onofré <jbonofre@apache.org>
2014-09-01 11:23:32 +0200

    [KARAF-2878] Revert this change as it breaks the console on Windows platform

--- shell/console/src/main/java/org/apache/karaf/shell/console/jline/Console.java
+++ shell/console/src/main/java/org/apache/karaf/shell/console/jline/Console.java
@@ -465,12 +465,6 @@ public class Console implements Runnable
                 {
                     try
                     {
-                        while (in.available() == 0) {
-                            if (!running) {
-                                return;
-                            }
-                            Thread.sleep(50);
-                        }
                         int c = in.read();
                         if (c == -1)
                         {
