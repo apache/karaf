d46eea6edc214957815185943fea122a53f6d199
Jean-Baptiste Onofré <jbonofre@apache.org>
2014-11-03 21:25:15 +0100

    [KARAF-3327] Improve LC_CTYPE parsing

--- shell/console/src/main/java/org/apache/karaf/shell/console/impl/jline/LocalConsoleManager.java
+++ shell/console/src/main/java/org/apache/karaf/shell/console/impl/jline/LocalConsoleManager.java
@@ -92,13 +92,7 @@ public class LocalConsoleManager {
                 }
             }
         };
-        String ctype = System.getenv("LC_CTYPE");
-        final String encoding;
-        if (ctype != null && ctype.indexOf('.') > 0) {
-            encoding = ctype.substring(ctype.indexOf('.') + 1);
-        } else {
-            encoding = System.getProperty("input.encoding", Charset.defaultCharset().name());
-        }
+        final String encoding = getEncoding();
 
         Runnable consoleStarter = new Runnable() {
             public void run() {
@@ -140,6 +134,40 @@ public class LocalConsoleManager {
         if (console != null) {
             console.close(false);
         }
+    }
+
+    /**
+     * Get the default encoding.  Will first look at the LC_CTYPE environment variable, then the input.encoding
+     * system property, then the default charset according to the JVM.
+     *
+     * @return The default encoding to use when none is specified.
+     */
+    public static String getEncoding() {
+        // LC_CTYPE is usually in the form en_US.UTF-8
+        String envEncoding = extractEncodingFromCtype(System.getenv("LC_CTYPE"));
+        if (envEncoding != null) {
+            return envEncoding;
+        }
+        return System.getProperty("input.encoding", Charset.defaultCharset().name());
+    }
+
+    /**
+     * Parses the LC_CTYPE value to extract the encoding according to the POSIX standard, which says that the LC_CTYPE
+     * environment variable may be of the format <code>[language[_territory][.codeset][@modifier]]</code>
+     *
+     * @param ctype The ctype to parse, may be null
+     * @return The encoding, if one was present, otherwise null
+     */
+    static String extractEncodingFromCtype(String ctype) {
+        if (ctype != null && ctype.indexOf('.') > 0) {
+            String encodingAndModifier = ctype.substring(ctype.indexOf('.') + 1);
+            if (encodingAndModifier.indexOf('@') > 0) {
+                return encodingAndModifier.substring(0, encodingAndModifier.indexOf('@'));
+            } else {
+                return encodingAndModifier;
+            }
+        }
+        return null;
     }
 
 }
