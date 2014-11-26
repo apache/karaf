c1586ecefd0af95aac424b98506c700d56e92d1c
Guillaume Nodet <gnodet@gmail.com>
2014-11-26 14:15:54 +0100

    [KARAF-3391] Move Base64 into karaf util

--- jaas/modules/src/main/java/org/apache/karaf/jaas/modules/encryption/BasicEncryption.java
+++ jaas/modules/src/main/java/org/apache/karaf/jaas/modules/encryption/BasicEncryption.java
@@ -17,12 +17,15 @@ package org.apache.karaf.jaas.modules.encryption;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 import org.apache.karaf.jaas.modules.Encryption;
 import org.apache.karaf.jaas.modules.EncryptionService;
+import org.apache.karaf.util.base64.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -113,86 +116,14 @@ public class BasicEncryption implements Encryption {
         return new String(out);
     }
 
-    private static final byte[] encodingTable = {
-        (byte)'A', (byte)'B', (byte)'C', (byte)'D', (byte)'E', (byte)'F', (byte)'G',
-        (byte)'H', (byte)'I', (byte)'J', (byte)'K', (byte)'L', (byte)'M', (byte)'N',
-        (byte)'O', (byte)'P', (byte)'Q', (byte)'R', (byte)'S', (byte)'T', (byte)'U',
-        (byte)'V', (byte)'W', (byte)'X', (byte)'Y', (byte)'Z',
-        (byte)'a', (byte)'b', (byte)'c', (byte)'d', (byte)'e', (byte)'f', (byte)'g',
-        (byte)'h', (byte)'i', (byte)'j', (byte)'k', (byte)'l', (byte)'m', (byte)'n',
-        (byte)'o', (byte)'p', (byte)'q', (byte)'r', (byte)'s', (byte)'t', (byte)'u',
-        (byte)'v',
-        (byte)'w', (byte)'x', (byte)'y', (byte)'z',
-        (byte)'0', (byte)'1', (byte)'2', (byte)'3', (byte)'4', (byte)'5', (byte)'6',
-        (byte)'7', (byte)'8', (byte)'9',
-        (byte)'+', (byte)'/'
-    };
-
-    private static byte padding = (byte)'=';
-
     /**
      * encode the input data producing a base 64 encoded byte array.
      *
      * @return a byte array containing the base 64 encoded data.
      */
-    public static String base64Encode(byte[] data) {
-        ByteArrayOutputStream    bOut = new ByteArrayOutputStream();
-        try {
-            base64Encode(data, 0, data.length, bOut);
-        } catch (IOException e) {
-            throw new RuntimeException("exception encoding base64 string: " + e.getMessage(), e);
-        }
-        return new String(bOut.toByteArray());
-    }
-
-    /**
-     * encode the input data producing a base 64 output stream.
-     *
-     * @return the number of bytes produced.
-     */
-    public static int base64Encode(byte[] data, int off, int length, OutputStream out) throws IOException {
-        int modulus = length % 3;
-        int dataLength = (length - modulus);
-        int a1, a2, a3;
-        for (int i = off; i < off + dataLength; i += 3) {
-            a1 = data[i] & 0xff;
-            a2 = data[i + 1] & 0xff;
-            a3 = data[i + 2] & 0xff;
-            out.write(encodingTable[(a1 >>> 2) & 0x3f]);
-            out.write(encodingTable[((a1 << 4) | (a2 >>> 4)) & 0x3f]);
-            out.write(encodingTable[((a2 << 2) | (a3 >>> 6)) & 0x3f]);
-            out.write(encodingTable[a3 & 0x3f]);
-        }
-        /*
-         * process the tail end.
-         */
-        int b1, b2, b3;
-        int d1, d2;
-        switch (modulus) {
-            case 0:        /* nothing left to do */
-                break;
-            case 1:
-                d1 = data[off + dataLength] & 0xff;
-                b1 = (d1 >>> 2) & 0x3f;
-                b2 = (d1 << 4) & 0x3f;
-                out.write(encodingTable[b1]);
-                out.write(encodingTable[b2]);
-                out.write(padding);
-                out.write(padding);
-                break;
-            case 2:
-                d1 = data[off + dataLength] & 0xff;
-                d2 = data[off + dataLength + 1] & 0xff;
-                b1 = (d1 >>> 2) & 0x3f;
-                b2 = ((d1 << 4) | (d2 >>> 4)) & 0x3f;
-                b3 = (d2 << 2) & 0x3f;
-                out.write(encodingTable[b1]);
-                out.write(encodingTable[b2]);
-                out.write(encodingTable[b3]);
-                out.write(padding);
-                break;
-        }
-        return (dataLength / 3) * 4 + ((modulus == 0) ? 0 : 4);
+    public static String base64Encode(byte[] input) {
+        byte[] transformed = Base64.encodeBase64(input);
+        return new String(transformed, StandardCharsets.ISO_8859_1);
     }
 
 }
