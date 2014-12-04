194ff4c28b15051f1ffc550cbd6e7c615534f574
Jean-Baptiste Onofré <jbonofre@apache.org>
2014-12-05 00:12:31 +0100

    [KARAF-3397] Fix FeaturesMBean on the features configurations retrieval

--- features/core/src/main/java/org/apache/karaf/features/management/codec/JmxFeature.java
+++ features/core/src/main/java/org/apache/karaf/features/management/codec/JmxFeature.java
@@ -129,12 +129,8 @@ public class JmxFeature {
         TabularDataSupport table = new TabularDataSupport(FEATURE_CONFIG_TABLE);
         for (ConfigInfo configInfo : config) {
         	String[] itemNames = FeaturesServiceMBean.FEATURE_CONFIG;
-        	Object[] itemValues = new Object[3];
-        	itemValues[0] = configInfo.getName();
-			itemValues[1] = getConfigElementTable(configInfo.getProperties());
-			itemValues[2] = configInfo.isAppend();
-			CompositeData configComposite = new CompositeDataSupport(
-					FEATURE_CONFIG, itemNames, itemValues);
+        	Object[] itemValues = { configInfo.getName(), getConfigElementTable(configInfo.getProperties()), new Boolean(configInfo.isAppend()) };
+			CompositeData configComposite = new CompositeDataSupport(FEATURE_CONFIG, itemNames, itemValues);
 			table.put(configComposite);
 		}
         return table;
@@ -255,9 +251,11 @@ public class JmxFeature {
             String[] itemDescriptions = new String[itemNames.length];
             itemTypes[0] = SimpleType.STRING;
             itemTypes[1] = FEATURE_CONFIG_ELEMENT_TABLE;
+            itemTypes[2] = SimpleType.BOOLEAN;
 
             itemDescriptions[0] = "The PID of the config";
             itemDescriptions[1] = "The configuration elements";
+            itemDescriptions[2] = "The append flag";
 
             return new CompositeType("Config", description, itemNames,
                     itemDescriptions, itemTypes);
