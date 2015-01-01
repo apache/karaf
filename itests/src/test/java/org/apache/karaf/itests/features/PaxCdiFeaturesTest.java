5ca71be37c6302fefd98f1544681227f98bb62f7
Jean-Baptiste Onofré <jbonofre@apache.org>
2015-01-01 16:30:54 +0100

    [KARAF-3428] Cleanup CDI dependencies and add Pax CDI itests

---
+++ itests/src/test/java/org/apache/karaf/itests/features/PaxCdiFeaturesTest.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.itests.features;
+
+import org.apache.karaf.itests.KarafTestSupport;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class PaxCdiFeaturesTest extends KarafTestSupport {
+
+    @Test
+    public void installPaxCdiFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi");
+    }
+
+    @Test
+    public void installPaxCdi11Feature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.1");
+    }
+
+    @Test
+    public void installPaxCdi12Feature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.2");
+    }
+
+    @Test
+    public void installPaxCdiWeldFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-weld");
+    }
+
+    @Test
+    public void installPaxCdiWeld11Feature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.1-weld");
+    }
+
+    @Test
+    public void installPaxCdiWeld12Feature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.2-weld");
+    }
+
+    @Test
+    public void installPaxCdiOpenwebbeansFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-openwebbeans");
+    }
+
+    @Test
+    @Ignore("PAXCDI-146 and Servlet API dependency")
+    public void installPaxCdiWebFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-web");
+    }
+
+    @Test
+    @Ignore("PAXCDI-146 and Servlet API dependency")
+    public void installPaxCdi11WebFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.1-web");
+    }
+
+    @Test
+    @Ignore("PAXCDI-146 and Servlet API dependency")
+    public void installPaxCdi12WebFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.2-web");
+    }
+
+    @Test
+    @Ignore("PAXCDI-146 and Servlet API dependency")
+    public void installPaxCdiWebWeldFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-web-weld");
+    }
+
+    @Test
+    @Ignore("PAXCDI-146 and Servlet API dependency")
+    public void installPaxCdi11WebWeldFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.1-web-weld");
+    }
+
+    @Test
+    @Ignore("PAXCDI-146 and Servlet API dependency")
+    public void installPaxCdi12WebWeldFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-1.2-web-weld");
+    }
+
+    @Test
+    @Ignore("PAXCDI-146 and Servlet API dependency")
+    public void installPaxCdiWebOpenwebbeansFeature() throws Exception {
+        installAssertAndUninstallFeatures("pax-cdi-web-openwebbeans");
+    }
+
+    @Test
+    public void installDeltaspikeCoreFeature() throws Exception {
+        installAssertAndUninstallFeatures("deltaspike-core");
+    }
+
+    @Test
+    public void installDeltaspikeJpaFeature() throws Exception {
+        installAssertAndUninstallFeatures("deltaspike-jpa");
+    }
+
+}
