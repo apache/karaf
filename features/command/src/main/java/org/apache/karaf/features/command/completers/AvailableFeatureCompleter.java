db1143e8966acd535d039cd2b2dbd61ffe9e2092
Guillaume Nodet <gnodet@gmail.com>
2015-01-08 10:00:16 +0100

    Fix javadoc link

--- features/command/src/main/java/org/apache/karaf/features/command/completers/AvailableFeatureCompleter.java
+++ features/command/src/main/java/org/apache/karaf/features/command/completers/AvailableFeatureCompleter.java
@@ -20,7 +20,7 @@ import org.apache.karaf.features.Feature;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 
 /**
- * {@link org.apache.karaf.shell.console.Completer} for features not installed yet.
+ * {@link org.apache.karaf.shell.api.console.Completer} for features not installed yet.
  */
 @Service
 public class AvailableFeatureCompleter extends FeatureCompleterSupport {
