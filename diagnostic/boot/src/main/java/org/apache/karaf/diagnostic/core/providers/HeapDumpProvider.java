17178f29da72cc7ab17a8e1e2c3eb14487a5a333
Jonathan Anstey <janstey@gmail.com>
2015-02-16 15:51:34 -0500

    KARAF-3536 - remove compile-time dependency on com.sun.management.HotSpotDiagnosticMXBean

--- diagnostic/boot/src/main/java/org/apache/karaf/diagnostic/core/providers/HeapDumpProvider.java
+++ diagnostic/boot/src/main/java/org/apache/karaf/diagnostic/core/providers/HeapDumpProvider.java
@@ -16,7 +16,6 @@
  */
 package org.apache.karaf.diagnostic.core.providers;
 
-import com.sun.management.HotSpotDiagnosticMXBean;
 import org.apache.karaf.diagnostic.core.DumpDestination;
 import org.apache.karaf.diagnostic.core.DumpProvider;
 
@@ -25,6 +24,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.lang.management.ManagementFactory;
+import java.lang.reflect.Method;
 
 /**
  * Create a heap dump.
@@ -37,11 +37,16 @@ public class HeapDumpProvider implements DumpProvider {
         OutputStream out = null;
         try {
             MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-            HotSpotDiagnosticMXBean diagnosticMXBean = ManagementFactory.newPlatformMXBeanProxy(mBeanServer,
-                    "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class);
+            Class<?> diagnosticMXBeanClass = Class.forName("com.sun.management.HotSpotDiagnosticMXBean");
+            Object diagnosticMXBean = ManagementFactory.newPlatformMXBeanProxy(mBeanServer,
+                "com.sun.management:type=HotSpotDiagnostic", diagnosticMXBeanClass);
+
             heapDumpFile = File.createTempFile("heapdump", ".txt");
             heapDumpFile.delete();
-            diagnosticMXBean.dumpHeap(heapDumpFile.getAbsolutePath(), false);
+            
+            Method method = diagnosticMXBeanClass.getMethod("dumpHeap", String.class, boolean.class);
+            method.invoke(diagnosticMXBean, heapDumpFile.getAbsolutePath(), false);
+
             // copy the dump in the destination
             in = new FileInputStream(heapDumpFile);
             out = destination.add("heapdump.txt");
