ebd04896f2b941550a66b0f79c9e0625748f7932
Jay SenSharma <joy.sensharma@gmail.com>
2015-04-12 11:51:57 +0530

    [KARAF-3659] NullPointerException fixed during shutdown due to incorrectly configured JMX SSL

--- management/server/src/main/java/org/apache/karaf/management/ConnectorServerFactory.java
+++ management/server/src/main/java/org/apache/karaf/management/ConnectorServerFactory.java
@@ -273,7 +273,9 @@ public class ConnectorServerFactory {
 
     public void destroy() throws Exception {
         try {
-            this.connectorServer.stop();
+            if (this.connectorServer != null) {
+                this.connectorServer.stop();
+            }
         } finally {
             doUnregister(this.objectName);
         }
