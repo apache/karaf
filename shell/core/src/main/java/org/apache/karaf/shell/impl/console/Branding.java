ba1f4dc115e9229afca60c5c6fdbd0df63c74ef1
Freeman Fang <freeman.fang@gmail.com>
2015-05-13 15:44:07 +0800

    [KARAF-3576]can't load customized branding.properties from KARAF_ETC

--- shell/core/src/main/java/org/apache/karaf/shell/impl/console/Branding.java
+++ shell/core/src/main/java/org/apache/karaf/shell/impl/console/Branding.java
@@ -18,15 +18,23 @@
  */
 package org.apache.karaf.shell.impl.console;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.apache.karaf.shell.api.console.Terminal;
 
 
 public final class Branding {
-    
+   
+    static final Logger LOGGER = LoggerFactory.getLogger(Branding.class);
+     
     private Branding() { }
 
     public static Properties loadBrandingProperties() {
@@ -46,6 +54,18 @@ public final class Branding {
         }
 
         loadProps(props, "org/apache/karaf/branding/branding.properties");
+        // load branding from etc/branding.properties
+        File etcBranding = new File(System.getProperty("karaf.etc"), "branding.properties");
+        if (etcBranding.exists()) {
+            FileInputStream etcBrandingIs = null;
+            try {
+                etcBrandingIs = new FileInputStream(etcBranding);
+            } catch (FileNotFoundException e) {
+                LOGGER.trace("Could not load branding.", e);
+            }
+            loadProps(props, etcBrandingIs);
+        }
+
         return props;
     }
     
@@ -53,6 +73,24 @@ public final class Branding {
         InputStream is = null;
         try {
             is = Branding.class.getClassLoader().getResourceAsStream(resource);
+            if (is != null) {
+                props.load(is);
+            }
+        } catch (IOException e) {
+            // ignore
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    // Ignore
+                }
+            }
+        }
+    }
+
+    protected static void loadProps(Properties props, InputStream is) {
+        try {
             if (is != null) {
                 props.load(is);
             }
