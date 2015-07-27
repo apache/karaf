4a5ce2321e6ab03edbe69bf354f97d0f3a2c95d5
Benson Margulies <benson@basistech.com>
2015-07-26 20:08:19 -0400

    KARAF-3882: more on test.

--- itests/src/test/java/org/apache/karaf/itests/SshKeyFormatTest.java
+++ itests/src/test/java/org/apache/karaf/itests/SshKeyFormatTest.java
@@ -20,8 +20,18 @@
 
 package org.apache.karaf.itests;
 
+import org.apache.sshd.ClientSession;
+import org.apache.sshd.SshClient;
+import org.apache.sshd.client.future.ConnectFuture;
+import org.junit.Test;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.TestProbeBuilder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.OutputStream;
+import java.util.Map;
 
 import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.options;
@@ -35,12 +45,21 @@ public class SshKeyFormatTest extends SshCommandTestBase {
 
     @Configuration
     public Option[] config() {
+        File keyFile = new File("src/test/resources/test.pem");
         return options(composite(super.config()),
-                editConfigurationFilePut("org.apache.karaf.shell.cfg", "hostKey", "/etc/test.pem"),
+                editConfigurationFilePut("org.apache.karaf.shell.cfg", "hostKey", keyFile.getAbsolutePath()),
                 editConfigurationFilePut("org.apache.karaf.shell.cfg", "hostKeyFormat", "PEM")
                 );
     }
 
 
-
+    @Test
+    public void usePemKey() throws Exception {
+        SshClient client = SshClient.setUpDefaultClient();
+        client.start();
+        ConnectFuture future = client.connect("karaf", "localhost", 8101).await();
+        ClientSession session = future.getSession();
+        Map<Object, Object> metadata = session.getMetadataMap();
+        session.close(true);
+    }
 }
