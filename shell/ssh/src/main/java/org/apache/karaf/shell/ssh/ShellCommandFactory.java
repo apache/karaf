7f00332c0e5269f538fc7216f5d0d7e54cbdb3ea
Freeman Fang <freeman.fang@gmail.com>
2015-09-08 15:31:52 +0800

    [KARAF-3978]enable specify different shell.init.script env for remote shell in exec and shell mode

--- shell/ssh/src/main/java/org/apache/karaf/shell/ssh/ShellCommandFactory.java
+++ shell/ssh/src/main/java/org/apache/karaf/shell/ssh/ShellCommandFactory.java
@@ -45,6 +45,7 @@ import org.slf4j.LoggerFactory;
 public class ShellCommandFactory implements CommandFactory {
 
     public static final String SHELL_INIT_SCRIPT = "karaf.shell.init.script";
+    public static final String EXEC_INIT_SCRIPT = "karaf.exec.init.script";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ShellCommandFactory.class);
 
@@ -120,7 +121,10 @@ public class ShellCommandFactory implements CommandFactory {
                             
                             result = JaasHelper.doAs(subject, new PrivilegedExceptionAction<Object>() {
                                 public Object run() throws Exception {
-                                    String scriptFileName = System.getProperty(SHELL_INIT_SCRIPT);
+                                    String scriptFileName = System.getProperty(EXEC_INIT_SCRIPT);
+                                    if (scriptFileName == null) {
+                                        scriptFileName = System.getProperty(SHELL_INIT_SCRIPT);
+                                    }
                                     executeScript(scriptFileName, commandSession);
                                     return commandSession.execute(command);
                                 }
@@ -129,7 +133,10 @@ public class ShellCommandFactory implements CommandFactory {
                             throw e.getException();
                         }
                     } else {
-                        String scriptFileName = System.getProperty(SHELL_INIT_SCRIPT);
+                        String scriptFileName = System.getProperty(EXEC_INIT_SCRIPT);
+                        if (scriptFileName == null) {
+                            scriptFileName = System.getProperty(SHELL_INIT_SCRIPT);
+                        }
                         executeScript(scriptFileName, commandSession);
                         result = commandSession.execute(command);
                     }
