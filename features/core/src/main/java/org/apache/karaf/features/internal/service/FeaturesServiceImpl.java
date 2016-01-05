843909a8915e2c4ba541c8725445b19926ee7ab6
Jean-Baptiste Onofré <jbonofre@apache.org>
2016-01-05 17:59:00 +0100

    KARAF-4031 - Avoid to block boot features when a repository can't be loaded
    KARAF-4083 - Avoid to break features service when a repository can't be loaded

--- features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
@@ -633,14 +633,18 @@ public class FeaturesServiceImpl implements FeaturesService, Deployer.DeployCall
             }
             if (repo == null) {
                 RepositoryImpl rep = new RepositoryImpl(URI.create(uri), blacklisted);
-                rep.load();
-                repo = rep;
-                synchronized (lock) {
-                    repositoryCache.put(uri, repo);
+                try {
+                    rep.load();
+                    repo = rep;
+                    synchronized (lock) {
+                        repositoryCache.put(uri, repo);
+                    }
+                    for (URI u : repo.getRepositories()) {
+                        toLoad.add(u.toString());
+                    }
+                } catch (Exception e) {
+                    LOGGER.warn("Can't load features repository {}", uri, e);
                 }
-            }
-            for (URI u : repo.getRepositories()) {
-                toLoad.add(u.toString());
             }
         }
         List<Repository> repos;
