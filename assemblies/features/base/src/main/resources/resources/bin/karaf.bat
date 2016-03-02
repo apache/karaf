721eafba6f6fb382b50b67f0baf06421e7666b81
Andrea Cosentino <ancosen@gmail.com>
2016-03-02 08:49:31 +0100

    [KARAF-4373] Karaf.bat script produces "The syntax of the command is incorrect" even if it is working properly

--- assemblies/features/base/src/main/resources/resources/bin/karaf.bat
+++ assemblies/features/base/src/main/resources/resources/bin/karaf.bat
@@ -400,11 +400,12 @@ if "%KARAF_PROFILER%" == "" goto :RUN
             if ERRORLEVEL 10 (
                 echo Restarting ...
                 %KARAF_DAEMON% restart!
-        )
-    ) else (
-        if ERRORLEVEL 10 (
-            echo Restarting JVM...
-            goto EXECUTE
+            )
+        ) else (
+            if ERRORLEVEL 10 (
+                echo Restarting JVM...
+                goto EXECUTE
+            )
         )
     )
 
