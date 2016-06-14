826a8b5d6a6e979c8daa18e59271d9ed0f8defa6
Jean-Baptiste Onofré <jbonofre@apache.org>
2016-06-14 11:47:06 +0200

    Fix headers and cleanup imports

--- client/src/main/java/org/apache/sshd/client/kex/FixedDHGEX256.java
+++ client/src/main/java/org/apache/sshd/client/kex/FixedDHGEX256.java
@@ -1,18 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.sshd.client.kex;
-
-import javax.crypto.spec.DHParameterSpec;
-import java.math.BigInteger;
-import java.security.GeneralSecurityException;
-import java.security.KeyPairGenerator;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.sshd.common.KeyExchange;
 import org.apache.sshd.common.NamedFactory;
 import org.apache.sshd.common.digest.SHA256;
 import org.apache.sshd.common.kex.DH;
-import org.apache.sshd.common.util.SecurityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
 
 public class FixedDHGEX256 extends FixedDHGEX {
 
