34d281930b695310ff20a2b77a6a927372dea3a3
Guillaume Nodet <gnodet@apache.org>
2016-07-28 11:53:20 +0200

    [KARAF-4638] Improve shell table output by using unicode box drawing characters

--- shell/table/src/main/java/org/apache/karaf/shell/table/ShellTable.java
+++ shell/table/src/main/java/org/apache/karaf/shell/table/ShellTable.java
@@ -21,6 +21,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @deprecated use {@link org.apache.karaf.shell.support.table.ShellTable} instead
+ */
+@Deprecated
 public class ShellTable {
 
     private List<Col> cols = new ArrayList<Col>();
