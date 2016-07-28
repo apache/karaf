c4cec085cccce60250dff76f0388235c7c786a5c
Guillaume Nodet <gnodet@apache.org>
2016-07-28 11:53:56 +0200

    Revert "[KARAF-4537] Karaf Maven Plugin should respect scope when building a feature"
    This reverts commit 05d50c5737db4393d6d771d5a1b34b14eef9f9cc.

--- tooling/karaf-maven-plugin/src/main/java/org/apache/karaf/tooling/utils/Dependency31Helper.java
+++ tooling/karaf-maven-plugin/src/main/java/org/apache/karaf/tooling/utils/Dependency31Helper.java
@@ -159,7 +159,7 @@ public class Dependency31Helper implements DependencyHelper {
 
         public boolean selectDependency(Dependency dependency) {
             String scope = dependency.getScope();
-            return !"test".equals(scope);
+            return !"test".equals(scope) && !"runtime".equals(scope);
         }
 
         public DependencySelector deriveChildSelector(DependencyCollectionContext context) {
@@ -172,7 +172,7 @@ public class Dependency31Helper implements DependencyHelper {
 
         public boolean selectDependency(Dependency dependency) {
             String scope = dependency.getScope();
-            return !"test".equals(scope);
+            return !"test".equals(scope) && !"provided".equals(scope) && !"runtime".equals(scope);
         }
 
         public DependencySelector deriveChildSelector(DependencyCollectionContext context) {
