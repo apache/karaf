46793684018bf02ab87957546285a6935d800573
Guillaume Nodet <gnodet@apache.org>
2016-11-23 10:23:52 +0100

    [KARAF-4830] Karaf does not start with staged features in etc/org.apache.karaf.features.cfg

--- features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
@@ -1280,7 +1280,9 @@ public class FeaturesServiceImpl implements FeaturesService, Deployer.DeployCall
 
     @Override
     public void startBundle(Bundle bundle) throws BundleException {
-        bundle.start();
+        if (bundle != this.bundle || bundle.getState() != Bundle.STARTING) {
+            bundle.start();
+        }
     }
 
     @Override
