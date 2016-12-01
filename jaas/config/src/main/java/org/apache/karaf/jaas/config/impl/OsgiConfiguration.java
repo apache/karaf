040c805f47abe0f0a3fd18258e14bec5f4bca5a8
Guillaume Nodet <gnodet@apache.org>
2016-12-01 17:14:00 +0100

    [KARAF-4784] OsgiConfiguration for JAAS should fallback to default configuration

--- jaas/config/src/main/java/org/apache/karaf/jaas/config/impl/OsgiConfiguration.java
+++ jaas/config/src/main/java/org/apache/karaf/jaas/config/impl/OsgiConfiguration.java
@@ -23,18 +23,26 @@ import javax.security.auth.login.Configuration;
 
 import org.apache.karaf.jaas.config.JaasRealm;
 import org.apache.karaf.util.collections.CopyOnWriteArrayIdentityList;
+import org.slf4j.LoggerFactory;
 
 public class OsgiConfiguration extends Configuration {
 
     private final List<JaasRealm> realms = new CopyOnWriteArrayIdentityList<JaasRealm>();
+    private Configuration defaultConfiguration;
 
     public void init() {
+        try {
+            defaultConfiguration = Configuration.getConfiguration();
+        } catch (Throwable ex) {
+            // default configuration for fallback could not be retrieved
+            LoggerFactory.getLogger(OsgiConfiguration.class).warn("Unable to retrieve default configuration", ex);
+        }
         Configuration.setConfiguration(this);
     }
 
     public void close() {
         realms.clear();
-        Configuration.setConfiguration(null);
+        Configuration.setConfiguration(defaultConfiguration);
     }
 
     public void register(JaasRealm realm, Map<String,?> properties) {
@@ -60,6 +68,8 @@ public class OsgiConfiguration extends Configuration {
         }
         if (realm != null) {
             return realm.getEntries();
+        } else if (defaultConfiguration != null) {
+            return defaultConfiguration.getAppConfigurationEntry(name);
         }
         return null;
     }
