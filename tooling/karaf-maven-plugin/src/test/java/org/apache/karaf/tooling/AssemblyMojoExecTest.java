b2e4b2a65d2a748267d16d110dff2fd0cdad3871
Paul Campbell <pcampbell@kemitix.net>
2017-04-15 17:29:39 +0100

    tooling.karaf-maven-plugin: AssemblyMojoExecTest: test added dependencies, profiles and features

--- tooling/karaf-maven-plugin/src/test/java/org/apache/karaf/tooling/AssemblyMojoExecTest.java
+++ tooling/karaf-maven-plugin/src/test/java/org/apache/karaf/tooling/AssemblyMojoExecTest.java
@@ -1,5 +1,6 @@
 package org.apache.karaf.tooling;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.karaf.profile.assembly.Builder;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
@@ -16,10 +17,13 @@ import org.apache.maven.project.MavenProject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -27,7 +31,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
 
 /**
  * Tests for {@link AssemblyMojoExec}.
@@ -49,6 +58,9 @@ public class AssemblyMojoExecTest {
 
     private Set<Artifact> dependencyArtifacts;
 
+    @Captor
+    private ArgumentCaptor<String> stringArgumentCaptor;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
@@ -61,34 +73,107 @@ public class AssemblyMojoExecTest {
         assemblyMojo.setConfig(config);
         final Map<String, String> system = new HashMap<>();
         assemblyMojo.setSystem(system);
+        final ImmutableMap.Builder<Builder.Stage, String> builder = new ImmutableMap.Builder<>();
     }
 
     @Test
     public void shouldExecuteMojo() throws Exception {
         //given
-        dependencyArtifacts.add(getDependency("compile", "jar", ""));
-        dependencyArtifacts.add(getDependency("runtime", "jar", ""));
-        dependencyArtifacts.add(getDependency("provided", "jar", ""));
-        dependencyArtifacts.add(getDependency("other", "jar", ""));
-        dependencyArtifacts.add(getDependency("compile", "bundle", ""));
-        dependencyArtifacts.add(getDependency("compile", "other", ""));
+        dependencyArtifacts.addAll(
+                Arrays.asList(getDependency("compile", "jar", ""), getDependency("runtime", "jar", ""),
+                              getDependency("provided", "jar", ""), getDependency("other", "jar", ""),
+                              getDependency("compile", "bundle", ""), getDependency("compile", "other", ""),
+                              getDependency("compile", "kar", ""), getDependency("runtime", "kar", ""),
+                              getDependency("provided", "kar", ""), getDependency("compile", "jar", "features"),
+                              getDependency("compile", "jar", "karaf"), getDependency("runtime", "jar", "karaf"),
+                              getDependency("provided", "jar", "karaf")
+                             ));
 
-        dependencyArtifacts.add(getDependency("compile", "kar", ""));
-        dependencyArtifacts.add(getDependency("runtime", "kar", ""));
-        dependencyArtifacts.add(getDependency("provided", "kar", ""));
-
-        dependencyArtifacts.add(getDependency("compile", "jar", "features"));
-        dependencyArtifacts.add(getDependency("compile", "jar", "karaf"));
-        dependencyArtifacts.add(getDependency("runtime", "jar", "karaf"));
-        dependencyArtifacts.add(getDependency("provided", "jar", "karaf"));
         //when
         assemblyMojoExec.doExecute(assemblyMojo);
         //then
-        // ?
+        assertStageKarsAdded(Builder.Stage.Startup, new String[]{
+                "mvn:org.apache.karaf.features/framework/4.1.2-SNAPSHOT/xml/features",
+                "mvn:org.apache/test-compile-kar-/0.1.0/kar"
+        });
+        assertStageKarsAdded(Builder.Stage.Boot, new String[]{"mvn:org.apache/test-runtime-kar-/0.1.0/kar"});
+        assertStageKarsAdded(Builder.Stage.Installed, new String[]{"mvn:org.apache/test-provided-kar-/0.1.0/kar"});
+
+        assertStageRepoAdded(Builder.Stage.Startup, new String[]{
+                "mvn:org.apache/test-compile-jar-features/0.1.0/jar/features",
+                "mvn:org.apache/test-compile-jar-karaf/0.1.0/jar/karaf"
+        });
+        assertStageRepoAdded(Builder.Stage.Boot, new String[]{"mvn:org.apache/test-runtime-jar-karaf/0.1.0/jar/karaf"});
+        assertStageRepoAdded(
+                Builder.Stage.Installed, new String[]{"mvn:org.apache/test-provided-jar-karaf/0.1.0/jar/karaf"});
+
+        assertFeaturesAdded(new String[]{});
+        assertStageFeaturesAdded(Builder.Stage.Startup, new String[]{"framework"}, 2);
+        assertStageFeaturesAdded(Builder.Stage.Boot, new String[]{}, 1);
+        assertStageFeaturesAdded(Builder.Stage.Installed, new String[]{}, 1);
+        assertBundlesAdded(new String[]{
+                "mvn:org.apache/test-compile-jar-/0.1.0", "mvn:org.apache/test-compile-bundle-/0.1.0/bundle",
+                "mvn:org.apache/test-runtime-jar-/0.1.0", "mvn:org.apache/test-provided-jar-/0.1.0"
+        });
+        assertProfilesAdded(new String[]{});
+    }
+
+    private void assertProfilesAdded(final String[] profiles) {
+        stringArgumentCaptor.getAllValues()
+                            .clear();
+        then(builder).should(times(3))
+                     .profiles(stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getAllValues()).as("Add profiles")
+                                                       .containsExactlyInAnyOrder(profiles);
+    }
+
+    private void assertBundlesAdded(final String[] bundles) {
+        stringArgumentCaptor.getAllValues()
+                            .clear();
+        then(builder).should(times(3))
+                     .bundles(stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getAllValues()).as("Add bundles")
+                                                       .containsExactlyInAnyOrder(bundles);
+    }
+
+    private void assertFeaturesAdded(final String[] features) {
+        stringArgumentCaptor.getAllValues()
+                            .clear();
+        then(builder).should(times(3))
+                     .features(stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getAllValues()).as("Add features")
+                                                       .containsExactlyInAnyOrder(features);
+    }
+
+    private void assertStageFeaturesAdded(final Builder.Stage stage, final String[] features, final int calls) {
+        stringArgumentCaptor.getAllValues()
+                            .clear();
+        then(builder).should(times(calls))
+                     .features(eq(stage), stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getAllValues()).as("Add features to Stage " + stage)
+                                                       .containsExactlyInAnyOrder(features);
+    }
+
+    private void assertStageRepoAdded(final Builder.Stage stage, final String[] repos) {
+        stringArgumentCaptor.getAllValues()
+                            .clear();
+        then(builder).should()
+                     .repositories(eq(stage), anyBoolean(), stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getAllValues()).as("Add repositories to Stage " + stage)
+                                                       .containsExactlyInAnyOrder(repos);
+    }
+
+    private void assertStageKarsAdded(final Builder.Stage stage, final String[] kars) {
+        stringArgumentCaptor.getAllValues()
+                            .clear();
+        then(this.builder).should(times(kars.length))
+                          .kars(eq(stage), anyBoolean(), stringArgumentCaptor.capture());
+        assertThat(stringArgumentCaptor.getAllValues()).as("Add kars to Stage " + stage)
+                                                       .containsExactlyInAnyOrder(kars);
     }
 
     private DefaultArtifact getDependency(final String scope, final String type, final String classifier) {
-        final String groupId = "net.kemitix";
+        final String groupId = "org.apache";
         final String artifactId = String.format("test-%s-%s-%s", scope, type, classifier);
         final String version = "0.1.0";
         final ArtifactHandler artifactHandler = new DefaultArtifactHandlerStub(type, classifier);
@@ -129,7 +214,7 @@ public class AssemblyMojoExecTest {
     }
 
     private DefaultArtifact getProjectArtifact() {
-        final String groupId = "net.kemitix";
+        final String groupId = "org.apache";
         final String artifactId = "assembly-execute-mojo";
         final String version = "0.1.0";
         final String compile = "compile";
