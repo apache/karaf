cbd258c0a2627b5f7ce1b1db9a5aa5bb03ef2dfa
Paul Campbell <pcampbell@kemitix.net>
2017-04-15 15:01:17 +0100

    tooling.karaf-maven-plugin: AssemblyMojoExecTest: add test dependencies

--- tooling/karaf-maven-plugin/src/test/java/org/apache/karaf/tooling/AssemblyMojoExecTest.java
+++ tooling/karaf-maven-plugin/src/test/java/org/apache/karaf/tooling/AssemblyMojoExecTest.java
@@ -1,6 +1,7 @@
 package org.apache.karaf.tooling;
 
 import org.apache.karaf.profile.assembly.Builder;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.handler.ArtifactHandler;
 import org.apache.maven.artifact.repository.ArtifactRepository;
@@ -9,6 +10,7 @@ import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.resources.TestResources;
+import org.apache.maven.plugin.testing.stubs.DefaultArtifactHandlerStub;
 import org.apache.maven.plugin.testing.stubs.StubArtifactRepository;
 import org.apache.maven.project.MavenProject;
 import org.junit.Before;
@@ -19,7 +21,11 @@ import org.mockito.Spy;
 
 import java.io.File;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.mockito.Mockito.doNothing;
 
@@ -41,22 +47,52 @@ public class AssemblyMojoExecTest {
 
     private AssemblyMojoExec assemblyMojoExec;
 
+    private Set<Artifact> dependencyArtifacts;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        assemblyMojo = getAssemblyMojo();
         doNothing().when(builder)
                    .generateAssembly();
+        dependencyArtifacts = new HashSet<>();
+        assemblyMojo = getAssemblyMojo();
         assemblyMojoExec = new AssemblyMojoExec(assemblyMojo.getLog(), () -> builder);
+        final Map<String, String> config = new HashMap<>();
+        assemblyMojo.setConfig(config);
+        final Map<String, String> system = new HashMap<>();
+        assemblyMojo.setSystem(system);
     }
 
     @Test
     public void shouldExecuteMojo() throws Exception {
         //given
+        dependencyArtifacts.add(getDependency("compile", "jar", ""));
+        dependencyArtifacts.add(getDependency("runtime", "jar", ""));
+        dependencyArtifacts.add(getDependency("provided", "jar", ""));
+        dependencyArtifacts.add(getDependency("other", "jar", ""));
+        dependencyArtifacts.add(getDependency("compile", "bundle", ""));
+        dependencyArtifacts.add(getDependency("compile", "other", ""));
+
+        dependencyArtifacts.add(getDependency("compile", "kar", ""));
+        dependencyArtifacts.add(getDependency("runtime", "kar", ""));
+        dependencyArtifacts.add(getDependency("provided", "kar", ""));
+
+        dependencyArtifacts.add(getDependency("compile", "jar", "features"));
+        dependencyArtifacts.add(getDependency("compile", "jar", "karaf"));
+        dependencyArtifacts.add(getDependency("runtime", "jar", "karaf"));
+        dependencyArtifacts.add(getDependency("provided", "jar", "karaf"));
         //when
         assemblyMojoExec.doExecute(assemblyMojo);
         //then
         // ?
+    }
+
+    private DefaultArtifact getDependency(final String scope, final String type, final String classifier) {
+        final String groupId = "net.kemitix";
+        final String artifactId = String.format("test-%s-%s-%s", scope, type, classifier);
+        final String version = "0.1.0";
+        final ArtifactHandler artifactHandler = new DefaultArtifactHandlerStub(type, classifier);
+        return new DefaultArtifact(groupId, artifactId, version, scope, type, classifier, artifactHandler);
     }
 
     private AssemblyMojo getAssemblyMojo() throws Exception {
@@ -87,7 +123,7 @@ public class AssemblyMojoExecTest {
         final MavenProject mavenProject = new MavenProject();
         mavenProject.setFile(pom);
         mavenProject.setArtifact(getProjectArtifact());
-        mavenProject.setDependencyArtifacts(Collections.emptySet());
+        mavenProject.setDependencyArtifacts(dependencyArtifacts);
         mavenProject.setRemoteArtifactRepositories(getArtifactRepositories());
         return mavenProject;
     }
@@ -97,10 +133,10 @@ public class AssemblyMojoExecTest {
         final String artifactId = "assembly-execute-mojo";
         final String version = "0.1.0";
         final String compile = "compile";
-        final String jar = "jar";
+        final String type = "jar";
         final String classifier = "";
-        final ArtifactHandler artifactHandler = null;
-        return new DefaultArtifact(groupId, artifactId, version, compile, jar, classifier, artifactHandler);
+        final ArtifactHandler artifactHandler = new DefaultArtifactHandlerStub(type, classifier);
+        return new DefaultArtifact(groupId, artifactId, version, compile, type, classifier, artifactHandler);
     }
 
     private List<ArtifactRepository> getArtifactRepositories() {
