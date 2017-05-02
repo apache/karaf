a4c6592c5fbf77f605dc371ce73fdfebfcca8ffe
Paul Campbell <pcampbell@kemitix.net>
2017-05-02 09:07:33 +0100

    tooling.karaf-maven-plugin: ExecutableFileTest: expand

--- tooling/karaf-maven-plugin/src/test/java/org/apache/karaf/tooling/assembly/ExecutableFileTest.java
+++ tooling/karaf-maven-plugin/src/test/java/org/apache/karaf/tooling/assembly/ExecutableFileTest.java
@@ -1,7 +1,12 @@
 package org.apache.karaf.tooling.assembly;
 
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,10 +16,34 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class ExecutableFileTest {
 
+    private ExecutableFile executableFile;
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Before
+    public void setUp() throws Exception {
+        executableFile = new ExecutableFile();
+    }
+
+    @Test
+    public void setExecutableOkay() throws Exception {
+        //given
+        //assume Posix filesystem
+        final Path filePath = folder.newFile("executable-file")
+                                    .toPath();
+        Assume.assumeTrue(filePath.getFileSystem()
+                                  .supportedFileAttributeViews()
+                                  .contains("posix"));
+        //when
+        executableFile.make(filePath);
+        //then
+        assertThat(filePath).isExecutable();
+    }
+
     @Test
     public void setExecutableIgnoresIOException() throws Exception {
         //given
-        final ExecutableFile executableFile = new ExecutableFile();
         final String filename = "/dev/null";
         //when
         executableFile.make(Paths.get(filename));
