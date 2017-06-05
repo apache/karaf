3cba8385ffb70081af974b186d655e151defc5cd
clockard <chris.lockard@connexta.com>
2017-06-05 15:15:39 -0700

    [KARAF-5179] Fix usage of karaf.restart.jvm in Main

--- assemblies/features/base/src/main/filtered-resources/resources/bin/karaf.bat
+++ assemblies/features/base/src/main/filtered-resources/resources/bin/karaf.bat
@@ -414,13 +414,14 @@ if "%KARAF_PROFILER%" == "" goto :RUN
                 echo Restarting ...
                 %KARAF_DAEMON% restart!
             )
-        ) else (
-            if ERRORLEVEL 10 (
-                echo Restarting JVM...
-                goto EXECUTE
-            )
+        )
+    ) else (
+        if ERRORLEVEL 10 (
+            echo Restarting JVM...
+            goto EXECUTE
         )
     )
+
 
 rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
