apache/karaf

commit eb95bce7d5a1bcf23a5bda919209394ffcf3c01a

Change Blacklist into an ordinary class

features/core/src/main/java/org/apache/karaf/features/internal/service/Blacklist.java
@@ -16,12 +16,18 @@
  */
 package org.apache.karaf.features.internal.service;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.felix.utils.manifest.Clause;
 import org.apache.felix.utils.manifest.Parser;
@@ -47,25 +53,38 @@ public class Blacklist {
     public static final String TYPE_REPOSITORY = "repository";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Blacklist.class);
-
-    private Blacklist() {
+    private Clause[] clauses;
+    
+    public Blacklist() {
+        this(Collections.emptyList());
     }
 
-    public static void blacklist(Features features, String blacklisted) {
-        Set<String> blacklist = loadBlacklist(blacklisted);
-        blacklist(features, blacklist);
+    public Blacklist(List<String> blacklist) {
+        this.clauses = org.apache.felix.utils.manifest.Parser.parseClauses(blacklist.toArray(new String[blacklist.size()]));
+    }
+    
+    public Blacklist(String blacklistUrl) {
+        Set<String> blacklist = new HashSet<>();
+        if (blacklistUrl != null) {
+            try (InputStream is = new URL(blacklistUrl).openStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                reader.lines() //
+                    .map(line -> line.trim()) //
+                    .filter(line -> line.isEmpty() || line.startsWith("#")).collect(toSet());
+            } catch (FileNotFoundException e) {
+                LOGGER.debug("Unable to load blacklist bundles list", e.toString());
+            } catch (Exception e) {
+                LOGGER.debug("Unable to load blacklist bundles list", e);
+            }
+        }
+        this.clauses = Parser.parseClauses(blacklist.toArray(new String[blacklist.size()]));
     }
 
-    public static void blacklist(Features features, Collection<String> blacklist) {
-        Clause[] clauses = Parser.parseClauses(blacklist.toArray(new String[blacklist.size()]));
-        blacklist(features, clauses);
+    public void blacklist(Features features) {
+        features.getFeature().removeIf(feature -> blacklist(feature));
     }
 
-    public static void blacklist(Features features, Clause[] clauses) {
-        features.getFeature().removeIf(feature -> blacklist(feature, clauses));
-    }
-
-    public static boolean blacklist(Feature feature, Clause[] clauses) {
+    public boolean blacklist(Feature feature) {
         for (Clause clause : clauses) {
             // Check feature name
             if (clause.getName().equals(feature.getName())) {
@@ -83,16 +102,16 @@ public class Blacklist {
                 }
             }
             // Check bundles
-            blacklist(feature.getBundle(), clauses);
+            blacklist(feature.getBundle());
             // Check conditional bundles
             for (Conditional cond : feature.getConditional()) {
-                blacklist(cond.getBundle(), clauses);
+                blacklist(cond.getBundle());
             }
         }
         return false;
     }
 
-    private static void blacklist(List<Bundle> bundles, Clause[] clauses) {
+    private void blacklist(List<Bundle> bundles) {
         for (Iterator<Bundle> iterator = bundles.iterator(); iterator.hasNext();) {
             Bundle info = iterator.next();
             for (Clause clause : clauses) {
@@ -111,33 +130,7 @@ public class Blacklist {
         }
     }
 
-    public static Set<String> loadBlacklist(String blacklistUrl) {
-        Set<String> blacklist = new HashSet<>();
-        try {
-            if (blacklistUrl != null) {
-                try (
-                        InputStream is = new URL(blacklistUrl).openStream()
-                ) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        line = line.trim();
-                        if (!line.isEmpty() && !line.startsWith("#")) {
-                            blacklist.add(line);
-                        }
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            LOGGER.debug("Unable to load blacklist bundles list", e.toString());
-        } catch (Exception e) {
-            LOGGER.debug("Unable to load blacklist bundles list", e);
-        }
-        return blacklist;
-    }
-
-    public static boolean isFeatureBlacklisted(List<String> blacklist, String name, String version) {
-        Clause[] clauses = Parser.parseClauses(blacklist.toArray(new String[blacklist.size()]));
+    public boolean isFeatureBlacklisted(String name, String version) {
         for (Clause clause : clauses) {
             if (clause.getName().equals(name)) {
                 // Check feature version
@@ -157,16 +150,11 @@ public class Blacklist {
         return false;
     }
 
-    public static boolean isBundleBlacklisted(List<String> blacklist, String uri) {
-        return isBlacklisted(blacklist, uri, TYPE_BUNDLE);
+    public boolean isBundleBlacklisted(String uri) {
+        return isBlacklisted(uri, TYPE_BUNDLE);
     }
 
-    public static boolean isBlacklisted(List<String> blacklist, String uri, String btype) {
-        Clause[] clauses = Parser.parseClauses(blacklist.toArray(new String[blacklist.size()]));
-        return isBlacklisted(clauses, uri, btype);
-    }
-
-    public static boolean isBlacklisted(Clause[] clauses, String uri, String btype) {
+    public boolean isBlacklisted(String uri, String btype) {
         for (Clause clause : clauses) {
             String url = clause.getName();
             if (clause.getAttribute(BLACKLIST_URL) != null) {

features/core/src/test/java/org/apache/karaf/features/internal/service/BlacklistTest.java
@@ -16,77 +16,52 @@
  */
 package org.apache.karaf.features.internal.service;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.assertTrue;
 
-import org.apache.karaf.features.internal.model.Bundle;
+import java.net.URL;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+import org.apache.karaf.features.BundleInfo;
 import org.apache.karaf.features.internal.model.Feature;
 import org.apache.karaf.features.internal.model.Features;
 import org.apache.karaf.features.internal.model.JaxbUtil;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-
 public class BlacklistTest {
 
     @Test
     public void testBlacklistFeatureWithRange() {
-        URL url = getClass().getResource("f02.xml");
-        Features features = JaxbUtil.unmarshal(url.toExternalForm(), true);
-
-        List<String> blacklist = new ArrayList<>();
-        blacklist.add("spring;range=\"[2,3)\"");
-
-        Blacklist.blacklist(features, blacklist);
-        for (Feature feature : features.getFeature()) {
-            assertNotEquals("spring/2.5.6.SEC02", feature.getId());
-        }
+        Stream<Feature> features = blacklistWith("spring;range=\"[2,3)\"");
+        assertTrue(features.noneMatch(f -> f.getId().equals("spring/2.5.6.SEC02")));
     }
 
     @Test
     public void testBlacklistFeatureWithVersion() {
-        URL url = getClass().getResource("f02.xml");
-        Features features = JaxbUtil.unmarshal(url.toExternalForm(), true);
-
-        List<String> blacklist = new ArrayList<>();
-        blacklist.add("spring;range=2.5.6.SEC02");
-
-        Blacklist.blacklist(features, blacklist);
-        for (Feature feature : features.getFeature()) {
-            assertNotEquals("spring/2.5.6.SEC02", feature.getId());
-        }
+        Stream<Feature> features = blacklistWith("spring;range=2.5.6.SEC02");
+        assertTrue(features.noneMatch(f -> f.getId().equals("spring/2.5.6.SEC02")));
     }
 
     @Test
     public void testBlacklistFeatureWithoutVersion() {
-        URL url = getClass().getResource("f02.xml");
-        Features features = JaxbUtil.unmarshal(url.toExternalForm(), true);
-
-        List<String> blacklist = new ArrayList<>();
-        blacklist.add("spring");
-
-        Blacklist.blacklist(features, blacklist);
-        for (Feature feature : features.getFeature()) {
-            assertFalse(feature.getId().startsWith("spring/"));
-        }
+        Stream<Feature> features = blacklistWith("spring");
+        assertTrue(features.noneMatch(f -> f.getId().startsWith("spring/")));
     }
 
     @Test
     public void testBlacklistBundle() {
+        String blacklisted = "mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jasypt/1.7_1";
+        Stream<Feature> features = blacklistWith(blacklisted);
+        Stream<BundleInfo> bundles = features.flatMap(f -> f.getBundles().stream());
+        assertTrue(bundles.noneMatch(b -> b.getLocation().equals(blacklisted)));
+    }
+
+    private Stream<Feature> blacklistWith(String blacklistClause) {
         URL url = getClass().getResource("f02.xml");
         Features features = JaxbUtil.unmarshal(url.toExternalForm(), true);
 
-        List<String> blacklist = new ArrayList<>();
-        blacklist.add("mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jasypt/1.7_1");
-
-        Blacklist.blacklist(features, blacklist);
-        for (Feature feature : features.getFeature()) {
-            for (Bundle bundle : feature.getBundle()) {
-                assertNotEquals("mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jasypt/1.7_1",
-                                bundle.getLocation());
-            }
-        }
+        Blacklist blacklist = new Blacklist(Collections.singletonList(blacklistClause));
+        blacklist.blacklist(features);
+        return features.getFeature().stream();
     }
 }

features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
@@ -49,7 +49,6 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.felix.utils.manifest.Clause;
 import org.apache.felix.utils.version.VersionCleaner;
 import org.apache.felix.utils.version.VersionRange;
 import org.apache.felix.utils.version.VersionTable;
@@ -145,7 +144,8 @@ public class FeaturesServiceImpl implements FeaturesService, Deployer.DeployCall
         this.resolver = resolver;
         this.installSupport = installSupport;
         this.globalRepository = globalRepository;
-        this.repositories = new RepositoryCache(cfg.blacklisted);
+        Blacklist blacklist = new Blacklist(cfg.blacklisted);
+        this.repositories = new RepositoryCache(blacklist);
         this.cfg = cfg;
         this.executor = Executors.newSingleThreadExecutor(ThreadUtils.namedThreadFactory("features"));
         loadState();
@@ -260,8 +260,9 @@ public class FeaturesServiceImpl implements FeaturesService, Deployer.DeployCall
                 repositories.addAll(state.repositories);
                 installedFeatures.putAll(copy(state.installedFeatures));
             }
+            Blacklist blacklist = new Blacklist(cfg.blacklisted);
             for (String uri : repositories) {
-                Repository repository = new RepositoryImpl(URI.create(uri), cfg.blacklisted);
+                Repository repository = new RepositoryImpl(URI.create(uri), blacklist);
                 listener.repositoryEvent(new RepositoryEvent(repository, RepositoryEvent.EventType.RepositoryAdded, true));
             }
             for (Map.Entry<String, Set<String>> entry : installedFeatures.entrySet()) {
@@ -979,27 +980,6 @@ public class FeaturesServiceImpl implements FeaturesService, Deployer.DeployCall
     @Override
     public Repository createRepository(URI uri) throws Exception {
         return repositories.create(uri, true, true);
-    }
-
-    private Map<String, Feature> loadAllFeatures(Set<URI> uris) throws Exception {
-        //the outer map's key is feature name, the inner map's key is feature version
-        Map<String, Feature> map = new HashMap<>();
-        // Two phase load:
-        // * first load dependent repositories
-        Set<URI> loaded = new HashSet<>();
-        List<URI> toLoad = new ArrayList<>(uris);
-        Clause[] blacklisted = repositories.getBlacklisted();
-        while (!toLoad.isEmpty()) {
-            URI uri = toLoad.remove(0);
-            if (loaded.add(uri)) {
-                Repository repo = new RepositoryImpl(uri, blacklisted);
-                Collections.addAll(toLoad, repo.getRepositories());
-                for (Feature f : repo.getFeatures()) {
-                    map.put(f.getId(), f);
-                }
-            }
-        }
-        return map;
     }
 
     @Override

features/core/src/main/java/org/apache/karaf/features/internal/service/RepositoryCache.java
@@ -27,30 +27,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.felix.utils.manifest.Clause;
-import org.apache.felix.utils.manifest.Parser;
 import org.apache.karaf.features.Repository;
 
 public class RepositoryCache {
 
     private final Map<String, Repository> repositoryCache = new HashMap<>();
-    private final Clause[] blacklisted;
+    private final Blacklist blacklist;
     
-    public RepositoryCache(String blacklisted) {
-        this.blacklisted = loadBlacklist(blacklisted);
+    public RepositoryCache(Blacklist blacklist) {
+        this.blacklist = blacklist;
     }
 
-    public Clause[] getBlacklisted() {
-        return blacklisted;
-    }
-
-    private static Clause[] loadBlacklist(String blacklisted) {
-        Set<String> blacklistStrings = Blacklist.loadBlacklist(blacklisted);
-        return Parser.parseClauses(blacklistStrings.toArray(new String[blacklistStrings.size()]));
-    }
-    
     public Repository create(URI uri, boolean load, boolean validate) throws Exception {
-        RepositoryImpl repo = new RepositoryImpl(uri, blacklisted);
+        RepositoryImpl repo = new RepositoryImpl(uri, blacklist);
         if (load)
             repo.load(validate);
         return repo;

features/core/src/main/java/org/apache/karaf/features/internal/service/RepositoryImpl.java
@@ -22,10 +22,7 @@ import java.io.InputStream;
 import java.io.InterruptedIOException;
 import java.net.URI;
 import java.util.Objects;
-import java.util.Set;
 
-import org.apache.felix.utils.manifest.Clause;
-import org.apache.felix.utils.manifest.Parser;
 import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.Repository;
 import org.apache.karaf.features.internal.model.Features;
@@ -37,22 +34,16 @@ import org.apache.karaf.features.internal.model.JaxbUtil;
 public class RepositoryImpl implements Repository {
 
     private final URI uri;
-    private final Clause[] blacklisted;
+    private final Blacklist blacklist;
     private Features features;
-
+    
     public RepositoryImpl(URI uri) {
-        this(uri, (Clause[]) null);
+        this(uri, null);
     }
 
-    public RepositoryImpl(URI uri, String blacklisted) {
+    public RepositoryImpl(URI uri, Blacklist blacklist) {
         this.uri = uri;
-        Set<String> blacklistStrings = Blacklist.loadBlacklist(blacklisted);
-        this.blacklisted = Parser.parseClauses(blacklistStrings.toArray(new String[blacklistStrings.size()]));
-    }
-
-    public RepositoryImpl(URI uri, Clause[] blacklisted) {
-        this.uri = uri;
-        this.blacklisted = blacklisted != null ? blacklisted : new Clause[0];
+        this.blacklist = blacklist;
     }
 
     public URI getURI() {
@@ -97,7 +88,9 @@ public class RepositoryImpl implements Repository {
                     InputStream inputStream = new InterruptibleInputStream(uri.toURL().openStream())
             ) {
                 features = JaxbUtil.unmarshal(uri.toASCIIString(), inputStream, validate);
-                Blacklist.blacklist(features, blacklisted);
+                if (blacklist != null) {
+                    blacklist.blacklist(features);
+                }
             } catch (Exception e) {
                 throw new IOException(e.getMessage() + " : " + uri, e);
             }

profile/src/main/java/org/apache/karaf/profile/assembly/ArtifactInstaller.java
@@ -1,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.profile.assembly;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+import org.apache.karaf.features.internal.download.Downloader;
+import org.apache.karaf.features.internal.download.impl.DownloadManagerHelper;
+import org.apache.karaf.features.internal.service.Blacklist;
+import org.apache.karaf.util.maven.Parser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArtifactInstaller {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArtifactInstaller.class);
+
+    private Path systemDirectory;
+    private Downloader downloader;
+    private Blacklist blacklist;
+
+    public ArtifactInstaller(Path systemDirectory, Downloader downloader, List<String> blacklisted) {
+        this.systemDirectory = systemDirectory;
+        this.downloader = downloader;
+        this.blacklist = new Blacklist(blacklisted);
+    }
+    
+    public void installArtifact(String location) throws Exception {
+        LOGGER.info("      adding maven artifact: " + location);
+        location = DownloadManagerHelper.stripUrl(location);
+        if (location.startsWith("mvn:")) {
+            if (location.endsWith("/")) {
+                // for bad formed URL (like in Camel for mustache-compiler), we remove the trailing /
+                location = location.substring(0, location.length() - 1);
+            }
+            downloader.download(location, provider -> {
+                String uri = provider.getUrl();
+                if (blacklist.isBundleBlacklisted(uri)) {
+                    throw new RuntimeException("Bundle " + uri + " is blacklisted");
+                }
+                Path path = pathFromProviderUrl(systemDirectory, uri);
+                synchronized (provider) {
+                    Files.createDirectories(path.getParent());
+                    Files.copy(provider.getFile().toPath(), path, StandardCopyOption.REPLACE_EXISTING);
+                }
+            });
+        } else {
+            LOGGER.warn("Ignoring non maven artifact " + location);
+        }
+    }
+    
+    public static Path pathFromProviderUrl(Path systemDirectory, String url) throws MalformedURLException {
+        String pathString;
+        if (url.startsWith("file:")) {
+            return Paths.get(URI.create(url));
+        }
+        else if (url.startsWith("mvn:")) {
+            pathString = Parser.pathFromMaven(url);
+        }
+        else {
+            pathString = url;
+        }
+        return systemDirectory.resolve(pathString);
+    }
+}

profile/src/main/java/org/apache/karaf/profile/assembly/AssemblyDeployCallback.java
@@ -36,13 +36,13 @@ import org.apache.felix.utils.properties.Properties;
 import org.apache.karaf.features.DeploymentEvent;
 import org.apache.karaf.features.FeatureEvent;
 import org.apache.karaf.features.FeaturesService;
-import org.apache.karaf.features.internal.model.Library;
 import org.apache.karaf.features.internal.download.DownloadManager;
 import org.apache.karaf.features.internal.download.Downloader;
 import org.apache.karaf.features.internal.model.Config;
 import org.apache.karaf.features.internal.model.ConfigFile;
 import org.apache.karaf.features.internal.model.Feature;
 import org.apache.karaf.features.internal.model.Features;
+import org.apache.karaf.features.internal.model.Library;
 import org.apache.karaf.features.internal.service.Blacklist;
 import org.apache.karaf.features.internal.service.Deployer;
 import org.apache.karaf.features.internal.service.State;
@@ -63,6 +63,8 @@ public class AssemblyDeployCallback extends StaticInstallSupport implements Depl
 
     private final DownloadManager manager;
     private final Builder builder;
+    private Blacklist featureBlacklist;
+    private Blacklist bundleBlacklist;
     private final Path homeDirectory;
     private final int defaultStartLevel;
     private final Path etcDirectory;
@@ -72,9 +74,12 @@ public class AssemblyDeployCallback extends StaticInstallSupport implements Depl
 
     private final Map<String, Bundle> bundles = new HashMap<>();
 
+
     public AssemblyDeployCallback(DownloadManager manager, Builder builder, BundleRevision systemBundle, Collection<Features> repositories) throws Exception {
         this.manager = manager;
         this.builder = builder;
+        this.featureBlacklist = new Blacklist(builder.getBlacklistedFeatures());
+        this.bundleBlacklist = new Blacklist(builder.getBlacklistedBundles());
         this.homeDirectory = builder.homeDirectory;
         this.etcDirectory = homeDirectory.resolve("etc");
         this.systemDirectory = homeDirectory.resolve("system");
@@ -190,7 +195,7 @@ public class AssemblyDeployCallback extends StaticInstallSupport implements Depl
     }
     
     private void assertNotBlacklisted(org.apache.karaf.features.Feature feature) {
-        if (Blacklist.isFeatureBlacklisted(builder.getBlacklistedFeatures(), feature.getName(), feature.getVersion())) {
+        if (featureBlacklist.isFeatureBlacklisted(feature.getName(), feature.getVersion())) {
             if (builder.getBlacklistPolicy() == Builder.BlacklistPolicy.Fail) {
                 throw new RuntimeException("Feature " + feature.getId() + " is blacklisted");
             }
@@ -208,7 +213,7 @@ public class AssemblyDeployCallback extends StaticInstallSupport implements Depl
     @Override
     public Bundle installBundle(String region, String uri, InputStream is) throws BundleException {
         // Check blacklist
-        if (Blacklist.isBundleBlacklisted(builder.getBlacklistedBundles(), uri)) {
+        if (bundleBlacklist.isBundleBlacklisted(uri)) {
             if (builder.getBlacklistPolicy() == Builder.BlacklistPolicy.Fail) {
                 throw new RuntimeException("Bundle " + uri + " is blacklisted");
             }

commit 1103f3de2c37539f7e2ee695c5bfe873ef47abc2

Fix blacklist mechanism broken by commit eb95bce7d5a1bcf23a5bda919209394ffcf3c01a

features/core/src/main/java/org/apache/karaf/features/internal/service/Blacklist.java
@@ -69,8 +69,9 @@ public class Blacklist {
             try (InputStream is = new URL(blacklistUrl).openStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
                 reader.lines() //
-                    .map(line -> line.trim()) //
-                    .filter(line -> line.isEmpty() || line.startsWith("#")).collect(toSet());
+                    .map(String::trim) //
+                    .filter(line -> !line.isEmpty() && !line.startsWith("#"))
+                    .forEach(blacklist::add);
             } catch (FileNotFoundException e) {
                 LOGGER.debug("Unable to load blacklist bundles list", e.toString());
             } catch (Exception e) {
@@ -81,7 +82,7 @@ public class Blacklist {
     }
 
     public void blacklist(Features features) {
-        features.getFeature().removeIf(feature -> blacklist(feature));
+        features.getFeature().removeIf(this::blacklist);
     }
 
     public boolean blacklist(Feature feature) {

features/core/src/test/java/org/apache/karaf/features/internal/service/BlacklistTest.java
@@ -56,6 +56,14 @@ public class BlacklistTest {
         assertTrue(bundles.noneMatch(b -> b.getLocation().equals(blacklisted)));
     }
 
+    @Test
+    public void testBlacklistLoad() throws URISyntaxException {
+        Blacklist blacklist = new Blacklist(getClass().getResource("blacklist.txt").toExternalForm());
+        RepositoryImpl repo = new RepositoryImpl(getClass().getResource("f02.xml").toURI(), blacklist, true);
+        Stream<Feature> features = Arrays.asList(repo.getFeatures()).stream();
+        assertTrue(features.noneMatch(f -> f.getId().equals("spring/2.5.6.SEC02")));
+    }
+
     private Stream<org.apache.karaf.features.Feature> blacklistWith(String blacklistClause) {
         URI uri;
         try {