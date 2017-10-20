apache/karaf

commit bf666631fe84f0394a9b948b5088d205bf52db36

[KARAF-5178] Use lambdas where appropriate
Signed-off-by: Stephen Kitt <skitt@redhat.com>

config/src/main/java/org/apache/karaf/config/command/completers/MetaCompleter.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.apache.karaf.config.core.impl.MetaServiceCaller;
-import org.apache.karaf.config.core.impl.MetatypeCallable;
 import org.apache.karaf.shell.api.action.lifecycle.Init;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
@@ -34,7 +33,6 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
 import org.osgi.service.metatype.MetaTypeInformation;
-import org.osgi.service.metatype.MetaTypeService;
 
 @Service
 public class MetaCompleter implements Completer, BundleListener {
@@ -60,27 +58,23 @@ public class MetaCompleter implements Completer, BundleListener {
     }
 
     private synchronized void updateMeta() {
-        List<String> pids = MetaServiceCaller.withMetaTypeService(context, new MetatypeCallable<List<String>>() {
+        List<String> pids = MetaServiceCaller.withMetaTypeService(context, metatypeService -> {
+            List<String> pids1 = new ArrayList<>();
+            Bundle[] bundles = context.getBundles();
+            for (Bundle bundle : bundles) {
 
-            @Override
-            public List<String> callWith(MetaTypeService metatypeService) {
-                List<String> pids = new ArrayList<>();
-                Bundle[] bundles = context.getBundles();
-                for (Bundle bundle : bundles) {
-                    
-                    MetaTypeInformation info = metatypeService.getMetaTypeInformation(bundle);
-                    if (info == null) {
-                        continue;
-                    }
-                    if (info.getFactoryPids() != null) {
-                        pids.addAll(Arrays.asList(info.getFactoryPids()));
-                    }
-                    if (info.getPids() != null) {
-                        pids.addAll(Arrays.asList(info.getPids()));
-                    }
+                MetaTypeInformation info = metatypeService.getMetaTypeInformation(bundle);
+                if (info == null) {
+                    continue;
                 }
-                return pids;
+                if (info.getFactoryPids() != null) {
+                    pids1.addAll(Arrays.asList(info.getFactoryPids()));
+                }
+                if (info.getPids() != null) {
+                    pids1.addAll(Arrays.asList(info.getPids()));
+                }
             }
+            return pids1;
         });
         if (pids != null) {
             delegate.getStrings().clear();

bundle/core/src/main/java/org/apache/karaf/bundle/command/Headers.java
@@ -256,16 +256,8 @@ public class Headers extends BundlesCommand {
         String name = clause.getName();
         Directive[] directives = clause.getDirectives();
         Attribute[] attributes = clause.getAttributes();
-        Arrays.sort(directives, new Comparator<Directive>() {
-            public int compare(Directive o1, Directive o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        });
-        Arrays.sort(attributes, new Comparator<Attribute>() {
-            public int compare(Attribute o1, Attribute o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        });
+        Arrays.sort(directives, Comparator.comparing(Directive::getName));
+        Arrays.sort(attributes, Comparator.comparing(Attribute::getName));
         builder.append(name);
         for (int i = 0; directives != null && i < directives.length; i++) {
             if (noUses && directives[i].getName().equalsIgnoreCase("uses")) {

bundle/core/src/main/java/org/apache/karaf/bundle/command/LoadTest.java
@@ -33,7 +33,6 @@ import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
-import org.osgi.framework.FrameworkEvent;
 import org.osgi.framework.FrameworkListener;
 import org.osgi.framework.wiring.FrameworkWiring;
 
@@ -87,93 +86,85 @@ public class LoadTest implements Action {
             locks[b].set(false);
         }
         for (int i = 0; i < threads; i++) {
-            new Thread() {
-                public void run() {
-                    try {
-                        Random rand = new Random();
-                        for (int j = 0; j < iterations; j++) {
-                            for (;;) {
-                                int b = rand.nextInt(bundles.length);
-                                if (locks[b].compareAndSet(false, true)) {
-                                    try {
-                                        // Only touch active bundles
-                                        if (bundles[b].getState() != Bundle.ACTIVE) {
-                                            continue;
-                                        }
-                                        if (rand.nextInt(100) < refresh) {
-                                            try {
-                                                bundles[b].update();
-                                                final CountDownLatch latch = new CountDownLatch(1);
-                                                wiring.refreshBundles(Collections.singletonList(bundles[b]), new FrameworkListener() {
-                                                    public void frameworkEvent(FrameworkEvent event) {
-                                                        latch.countDown();
-                                                    }
-                                                });
-                                                latch.await();
-                                            } finally {
-                                                while (true) {
-                                                    try {
-                                                        bundles[b].start(Bundle.START_TRANSIENT);
-                                                        break;
-                                                    } catch (Exception e) {
-                                                        Thread.sleep(1);
-                                                    }
-                                                }
-                                            }
-                                        } else {
-                                            try {
-                                                bundles[b].stop(Bundle.STOP_TRANSIENT);
-                                            } finally {
-                                                while (true) {
-                                                    try {
-                                                        bundles[b].start(Bundle.START_TRANSIENT);
-                                                        break;
-                                                    } catch (Exception e) {
-                                                        Thread.sleep(1);
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        Thread.sleep(rand.nextInt(delay));
-                                    } catch (Exception e) {
-                                        boolean ignore = false;
-                                        if (e instanceof BundleException && e.getMessage() != null) {
-                                            String msg = e.getMessage();
-                                            if ("Cannot acquire global lock to update the bundle.".equals(msg) ||
-                                                    "Unable to acquire global lock for resolve.".equals(msg) ||
-                                                    msg.matches("Bundle .* cannot be update, since it is either starting or stopping.")) {
-                                                ignore = true;
-                                            }
-                                        }
-                                        if (!ignore) {
-                                            e.printStackTrace();
-                                        }
-                                    } finally {
-                                        locks[b].set(false);
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                    } catch (Throwable t) {
-                        t.printStackTrace();
-                    } finally {
-                        latch.countDown();
-                    }
-                }
-            }.start();
-        }
-        new Thread() {
-            @Override
-            public void run() {
+            new Thread(() -> {
                 try {
-                    latch.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    Random rand = new Random();
+                    for (int j = 0; j < iterations; j++) {
+                        for (;;) {
+                            int b = rand.nextInt(bundles.length);
+                            if (locks[b].compareAndSet(false, true)) {
+                                try {
+                                    // Only touch active bundles
+                                    if (bundles[b].getState() != Bundle.ACTIVE) {
+                                        continue;
+                                    }
+                                    if (rand.nextInt(100) < refresh) {
+                                        try {
+                                            bundles[b].update();
+                                            final CountDownLatch latch1 = new CountDownLatch(1);
+                                            wiring.refreshBundles(Collections.singletonList(bundles[b]),
+                                                    (FrameworkListener) event -> latch1.countDown());
+                                            latch1.await();
+                                        } finally {
+                                            while (true) {
+                                                try {
+                                                    bundles[b].start(Bundle.START_TRANSIENT);
+                                                    break;
+                                                } catch (Exception e) {
+                                                    Thread.sleep(1);
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        try {
+                                            bundles[b].stop(Bundle.STOP_TRANSIENT);
+                                        } finally {
+                                            while (true) {
+                                                try {
+                                                    bundles[b].start(Bundle.START_TRANSIENT);
+                                                    break;
+                                                } catch (Exception e) {
+                                                    Thread.sleep(1);
+                                                }
+                                            }
+                                        }
+                                    }
+                                    Thread.sleep(rand.nextInt(delay));
+                                } catch (Exception e) {
+                                    boolean ignore = false;
+                                    if (e instanceof BundleException && e.getMessage() != null) {
+                                        String msg = e.getMessage();
+                                        if ("Cannot acquire global lock to update the bundle.".equals(msg) ||
+                                                "Unable to acquire global lock for resolve.".equals(msg) ||
+                                                msg.matches("Bundle .* cannot be update, since it is either starting or stopping.")) {
+                                            ignore = true;
+                                        }
+                                    }
+                                    if (!ignore) {
+                                        e.printStackTrace();
+                                    }
+                                } finally {
+                                    locks[b].set(false);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                } finally {
+                    latch.countDown();
                 }
-                System.err.println("Load test finished");
+            }).start();
+        }
+        new Thread(() -> {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }.start();
+            System.err.println("Load test finished");
+        }).start();
         return null;
     }
 

bundle/core/src/main/java/org/apache/karaf/bundle/command/ShowBundleTree.java
@@ -103,19 +103,16 @@ public class ShowBundleTree extends BundleCommand {
      */
     private void printTree(Tree<Bundle> tree) {
         System.out.printf("%n");
-        tree.write(System.out, new Tree.Converter<Bundle>() {
-
-            public String toString(Node<Bundle> node) {
-                if (versions) {
-                    return String.format("%s / [%s] [%s]",
-                            node.getValue().getSymbolicName(),
-                            node.getValue().getVersion().toString(),
-                            node.getValue().getBundleId());
-                } else {
-                    return String.format("%s [%s]",
-                            node.getValue().getSymbolicName(),
-                            node.getValue().getBundleId());
-                }
+        tree.write(System.out, node -> {
+            if (versions) {
+                return String.format("%s / [%s] [%s]",
+                        node.getValue().getSymbolicName(),
+                        node.getValue().getVersion().toString(),
+                        node.getValue().getBundleId());
+            } else {
+                return String.format("%s [%s]",
+                        node.getValue().getSymbolicName(),
+                        node.getValue().getBundleId());
             }
         });
     }

bundle/core/src/main/java/org/apache/karaf/bundle/command/bundletree/Tree.java
@@ -67,11 +67,7 @@ public class Tree<T> extends Node<T> {
      * @param writer the writer where to write.
      */
     public void write(PrintWriter writer) {
-        write(writer, new Converter<T>() {
-            public String toString(Node<T> node) {
-                return node.getValue().toString();
-            }
-        });
+        write(writer, node -> node.getValue().toString());
     }
 
     /**

bundle/core/src/main/java/org/apache/karaf/bundle/core/internal/BundleWatcherImpl.java
@@ -108,11 +108,7 @@ public class BundleWatcherImpl implements Runnable, BundleListener, BundleWatche
                 if (!updated.isEmpty()) {
                     try {
                         final CountDownLatch latch = new CountDownLatch(1);
-                        wiring.refreshBundles(updated, new FrameworkListener() {
-                            public void frameworkEvent(FrameworkEvent event) {
-                                latch.countDown();
-                            }
-                        });
+                        wiring.refreshBundles(updated, (FrameworkListener) event -> latch.countDown());
                         latch.await();
                     } catch (InterruptedException e) {
                         running.set(false);

bundle/core/src/test/java/org/apache/karaf/bundle/command/bundletree/TreeTest.java
@@ -28,13 +28,11 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Set;
 
-import org.apache.karaf.bundle.command.bundletree.Node;
-import org.apache.karaf.bundle.command.bundletree.Tree;
 import org.junit.Test;
 
 /**
- * Test cases for {@link org.apache.karaf.shell.dev.util.Tree}
- * and {@link org.apache.karaf.shell.dev.util.Node}
+ * Test cases for {@link org.apache.karaf.bundle.command.bundletree.Tree}
+ * and {@link org.apache.karaf.bundle.command.bundletree.Node}
  */
 public class TreeTest {
 
@@ -55,11 +53,7 @@ public class TreeTest {
         tree.addChild("child");
 
         StringWriter writer = new StringWriter();
-        tree.write(new PrintWriter(writer), new Tree.Converter<String>() {
-            public String toString(Node<String> node) {
-                return "my " + node.getValue();
-            }
-        });
+        tree.write(new PrintWriter(writer), node -> "my " + node.getValue());
 
         BufferedReader reader = new BufferedReader(new StringReader(writer.getBuffer().toString()));
 

commit 475be9310ebdc168b73d7b6a49763aab51698084

Fix meta command / completer to not print a warning when MetaType is not available
I think this is a regression caused by https://github.com/apache/karaf/commit/bf666631fe84f0394a9b948b5088d205bf52db36#diff-290cb1e2332aeecaf61aaa6aa3602768
which removes the anonymous inner classes and thus cause a CNFE to be thrown when the MetaCompleter getMethods() is called for introspection.
So this commit slightly change the mechanism and a warning is displayed when the completer is initialized, while a message is printed on the console when calling the command.

config/src/main/java/org/apache/karaf/config/command/MetaCommand.java
@@ -23,15 +23,16 @@ import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.apache.karaf.config.command.completers.MetaCompleter;
-import org.apache.karaf.config.core.impl.MetatypeCallable;
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.Completion;
 import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.support.CommandException;
 import org.apache.karaf.shell.support.table.ShellTable;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
@@ -40,10 +41,15 @@ import org.osgi.service.metatype.AttributeDefinition;
 import org.osgi.service.metatype.MetaTypeInformation;
 import org.osgi.service.metatype.MetaTypeService;
 import org.osgi.service.metatype.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Command(scope = "config", name = "meta", description = "Lists meta type information.")
 @Service
 public class MetaCommand extends ConfigCommandSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MetaCommand.class);
+
     @Argument(name = "pid", description = "The configuration pid", required = true, multiValued = false)
     @Completion(MetaCompleter.class)
     protected String pid;
@@ -72,15 +78,27 @@ public class MetaCommand extends ConfigCommandSupport {
 
     @Override
     public Object doExecute() throws Exception {
-        if (create) {
-            withMetaTypeService(context, new Create());
-        } else {
-            withMetaTypeService(context, new Print());
+        try {
+            if (create) {
+                withMetaTypeService(context, new Create());
+            } else {
+                withMetaTypeService(context, new Print());
+            }
+            return null;
+        } catch (Throwable e) {
+            Throwable ncdfe = e;
+            while (ncdfe != null && !(ncdfe instanceof NoClassDefFoundError)) {
+                ncdfe = ncdfe.getCause();
+            }
+            if (ncdfe != null && ncdfe.getMessage().equals("org/osgi/service/metatype/MetaTypeService")) {
+                throw new CommandException("config:meta disabled because the org.osgi.service.metatype package is not wired", e);
+            } else {
+                throw e;
+            }
         }
-        return null;
     }
         
-    abstract class AbstractMeta implements MetatypeCallable<Void> {
+    abstract class AbstractMeta implements Function<MetaTypeService, Void> {
         protected String getDefaultValueStr(String[] defaultValues) {
             if (defaultValues == null) {
                 return "";
@@ -117,7 +135,7 @@ public class MetaCommand extends ConfigCommandSupport {
     
     class Create extends AbstractMeta {
 
-        public Void callWith(MetaTypeService metaTypeService) {
+        public Void apply(MetaTypeService metaTypeService) {
             ObjectClassDefinition def = getMetatype(metaTypeService, pid);
             if (def == null) {
                 System.out.println("No meta type definition found for pid: " + pid);
@@ -151,7 +169,7 @@ public class MetaCommand extends ConfigCommandSupport {
     }
     
     class Print extends AbstractMeta {
-        public Void callWith(MetaTypeService metaTypeService) {
+        public Void apply(MetaTypeService metaTypeService) {
             ObjectClassDefinition def = getMetatype(metaTypeService, pid);
             if (def == null) {
                 System.out.println("No meta type definition found for pid: " + pid);

config/src/main/java/org/apache/karaf/config/command/completers/MetaCompleter.java
@@ -16,8 +16,6 @@
  */
 package org.apache.karaf.config.command.completers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.karaf.config.core.impl.MetaServiceCaller;
@@ -28,23 +26,42 @@ import org.apache.karaf.shell.api.console.CommandLine;
 import org.apache.karaf.shell.api.console.Completer;
 import org.apache.karaf.shell.api.console.Session;
 import org.apache.karaf.shell.support.completers.StringsCompleter;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
-import org.osgi.service.metatype.MetaTypeInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class MetaCompleter implements Completer, BundleListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MetaCompleter.class);
+
     private final StringsCompleter delegate = new StringsCompleter();
-    
+
     @Reference
     BundleContext context;
     
     @Init
     public void init() {
-        context.registerService(BundleListener.class, this, null);
-        updateMeta();
+        try {
+            updateMeta();
+            context.registerService(BundleListener.class, this, null);
+        } catch (Throwable e) {
+            Throwable ncdfe = e;
+            while (ncdfe != null && !(ncdfe instanceof NoClassDefFoundError)) {
+                ncdfe = ncdfe.getCause();
+            }
+            if (ncdfe != null && ncdfe.getMessage().equals("org/osgi/service/metatype/MetaTypeService")) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.warn("config:meta disabled because the org.osgi.service.metatype package is not wired", e);
+                } else {
+                    LOG.warn("config:meta disabled because the org.osgi.service.metatype package is not wired (enable debug logging for full stack trace).");
+                }
+            } else {
+                throw e;
+            }
+        }
     }
 
     @Override
@@ -58,24 +75,7 @@ public class MetaCompleter implements Completer, BundleListener {
     }
 
     private synchronized void updateMeta() {
-        List<String> pids = MetaServiceCaller.withMetaTypeService(context, metatypeService -> {
-            List<String> pids1 = new ArrayList<>();
-            Bundle[] bundles = context.getBundles();
-            for (Bundle bundle : bundles) {
-
-                MetaTypeInformation info = metatypeService.getMetaTypeInformation(bundle);
-                if (info == null) {
-                    continue;
-                }
-                if (info.getFactoryPids() != null) {
-                    pids1.addAll(Arrays.asList(info.getFactoryPids()));
-                }
-                if (info.getPids() != null) {
-                    pids1.addAll(Arrays.asList(info.getPids()));
-                }
-            }
-            return pids1;
-        });
+        List<String> pids = MetaServiceCaller.getPidsWithMetaInfo(context);
         if (pids != null) {
             delegate.getStrings().clear();
             delegate.getStrings().addAll(pids);

config/src/main/java/org/apache/karaf/config/core/impl/MetaServiceCaller.java
@@ -16,30 +16,52 @@
  */
 package org.apache.karaf.config.core.impl;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.metatype.MetaTypeInformation;
 import org.osgi.service.metatype.MetaTypeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
 /**
  * Allows to use the MetaTypeService as an optional dependency
  */
 public class MetaServiceCaller {
-    private static Logger LOG = LoggerFactory.getLogger(MetaServiceCaller.class);
 
-    public static <T> T withMetaTypeService(BundleContext context, MetatypeCallable<T> callable) {
+    public static <T> T withMetaTypeService(BundleContext context, Function<MetaTypeService, T> callable) {
+        ServiceReference<MetaTypeService> ref = context.getServiceReference(MetaTypeService.class);
         try {
-            ServiceReference<MetaTypeService> ref = context.getServiceReference(MetaTypeService.class);
-            try {
-                MetaTypeService metaService = context.getService(ref);
-                return callable.callWith(metaService);
-            } finally {
-                context.ungetService(ref);
-            }
-        } catch (NoClassDefFoundError e) {
-            LOG.warn("No Metatype Service present");
-            return null;
+            MetaTypeService metaService = context.getService(ref);
+            return callable.apply(metaService);
+        } finally {
+            context.ungetService(ref);
         }
+    }
+
+    public static List<String> getPidsWithMetaInfo(BundleContext context) {
+        return withMetaTypeService(context, metatypeService -> {
+            List<String> pids1 = new ArrayList<>();
+            Bundle[] bundles = context.getBundles();
+            for (Bundle bundle : bundles) {
+
+                MetaTypeInformation info = metatypeService.getMetaTypeInformation(bundle);
+                if (info == null) {
+                    continue;
+                }
+                if (info.getFactoryPids() != null) {
+                    pids1.addAll(Arrays.asList(info.getFactoryPids()));
+                }
+                if (info.getPids() != null) {
+                    pids1.addAll(Arrays.asList(info.getPids()));
+                }
+            }
+            return pids1;
+        });
     }
 }

config/src/main/java/org/apache/karaf/config/core/impl/MetatypeCallable.java
@@ -1,23 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.apache.karaf.config.core.impl;
-
-import org.osgi.service.metatype.MetaTypeService;
-
-public interface MetatypeCallable <T> {
-    T callWith(MetaTypeService metatypeService);
-}