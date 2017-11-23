apache/karaf

commit 9b9dd7947577dec377808344e78736988f7e8428

Primitives should not be boxed just for String conversion
Signed-off-by: Aurélien Pupier <apupier@redhat.com>

webconsole/gogo/src/main/java/org/apache/karaf/webconsole/gogo/Terminal.java
@@ -1025,7 +1025,7 @@ public class Terminal {
         String prefix = "";
         if (p.length() > 0) {
             if (p.charAt(0) >= '<' && p.charAt(0) <= '?') {
-                prefix = "" + p.charAt(0);
+                prefix = Character.toString(p.charAt(0));
                 p = p.substring(1);
             }
         }
@@ -1710,14 +1710,14 @@ public class Terminal {
                 vt100_keyfilter_escape = true;
             } else if (c == 127) {
                 if (vt100_mode_backspace) {
-                    o += (char) 8;
+                    o += Integer.toString(8);
                 } else {
-                    o += (char) 127;
+                    o += Integer.toString(127);
                 }
             } else {
-                o += c;
+                o += Integer.toString(c);
                 if (vt100_mode_lfnewline && c == 13) {
-                    o += (char) 10;
+                    o += Integer.toString(10);
                 }
             }
         }

instance/src/main/java/org/apache/karaf/instance/main/Execute.java
@@ -163,7 +163,7 @@ public class Execute {
         if (exitAllowed) {
             System.exit(rc);
         } else {
-            throw new RuntimeException("" + rc);
+            throw new RuntimeException(Integer.toString(rc));
         }
     }
 

shell/ssh/src/main/java/org/apache/karaf/shell/ssh/SshServerAction.java
@@ -65,7 +65,7 @@ public class SshServerAction implements Action
         server.getProperties().put(SshServer.IDLE_TIMEOUT, Long.toString(idleTimeout));
         
         // nio-workes
-        server.getProperties().put(SshServer.NIO_WORKERS, new Integer(nioWorkers).toString());
+        server.getProperties().put(SshServer.NIO_WORKERS, Integer.toString(nioWorkers));
         
         // welcome banner
         if (welcomeBanner != null) {

commit e57cb70539c12da8273e229b827e532bb47566f4

Fix gogo webconsole plugin
Broken by 9b9dd7947577dec377808344e78736988f7e8428

webconsole/gogo/src/main/java/org/apache/karaf/webconsole/gogo/Terminal.java
@@ -1708,16 +1708,16 @@ public class Terminal {
                 }
             } else if (c == '~') {
                 vt100_keyfilter_escape = true;
-            } else if (c == 127) {
+            } else if (c == '\u007f') {
                 if (vt100_mode_backspace) {
-                    o += Integer.toString(8);
+                    o += '\u0008';
                 } else {
-                    o += Integer.toString(127);
+                    o += '\u007f';
                 }
             } else {
-                o += Integer.toString(c);
-                if (vt100_mode_lfnewline && c == 13) {
-                    o += Integer.toString(10);
+                o += c;
+                if (vt100_mode_lfnewline && c == '\r') {
+                    o += '\n';
                 }
             }
         }