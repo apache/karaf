0f91a095aba0a7abfe8e5f9fe174811a6826c11b
clockard <chris.lockard@connexta.com>
2018-01-16 15:52:39 -0700

    KARAF-5569 Add CommandFactory to ssh server when sftp is disabled

--- shell/ssh/src/main/java/org/apache/karaf/shell/ssh/Activator.java
+++ shell/ssh/src/main/java/org/apache/karaf/shell/ssh/Activator.java
@@ -175,6 +175,8 @@ public class Activator extends BaseActivator implements ManagedService {
             server.setCommandFactory(new ScpCommandFactory.Builder().withDelegate(cmd -> new ShellCommand(sessionFactory, cmd)).build());
             server.setSubsystemFactories(Collections.singletonList(new SftpSubsystemFactory()));
             server.setFileSystemFactory(new VirtualFileSystemFactory(Paths.get(System.getProperty("karaf.base"))));
+        } else {
+            server.setCommandFactory(cmd -> new ShellCommand(sessionFactory, cmd));
         }
         server.setKeyPairProvider(keyPairProvider);
         server.setPasswordAuthenticator(authenticator);
