2fdf6f60638411c7ec6755cf66d4c8a3e79bea5d
Guillaume Nodet <gnodet@gmail.com>
2018-02-14 10:28:55 +0100

    [KARAF-5448] Fix java 9 warning for TCPEndpoint access

--- assemblies/features/base/src/main/filtered-resources/resources/bin/karaf.bat
+++ assemblies/features/base/src/main/filtered-resources/resources/bin/karaf.bat
@@ -394,6 +394,7 @@ if "%KARAF_PROFILER%" == "" goto :RUN
                 --add-opens java.base/java.net=ALL-UNNAMED ^
                 --add-opens java.base/java.lang=ALL-UNNAMED ^
                 --add-opens java.base/java.util=ALL-UNNAMED ^
+                --add-opens java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED ^
                 --add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED ^
                 --add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED ^
                 --add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED ^
