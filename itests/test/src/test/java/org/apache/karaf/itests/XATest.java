049f0db7c5317e8d7a300023f3fdd4c1fa9a10da
Jean-Baptiste Onofré <jbonofre@apache.org>
2018-11-22 17:53:18 +0100

    [KARAF-5901] Add Aries JAXRS Whiteboard example

--- itests/test/src/test/java/org/apache/karaf/itests/XATest.java
+++ itests/test/src/test/java/org/apache/karaf/itests/XATest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.karaf.itests.features;
+package org.apache.karaf.itests;
 
 import org.apache.karaf.features.FeaturesService;
 import org.apache.karaf.itests.KarafTestSupport;
--- itests/test/src/test/java/org/apache/karaf/itests/examples/RestExampleTest.java
+++ itests/test/src/test/java/org/apache/karaf/itests/examples/RestExampleTest.java
@@ -23,13 +23,22 @@ import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
 public class RestExampleTest extends KarafTestSupport {
 
     private void setup() throws Exception {
         addFeaturesRepository("mvn:org.apache.karaf.examples/karaf-rest-example-features/" + System.getProperty("karaf.version") + "/xml");
-        installAndAssertFeature("karaf-rest-example-provider");
     }
 
     private void verify() throws Exception {
@@ -45,6 +54,8 @@ public class RestExampleTest extends KarafTestSupport {
     public void testWithHttpClient() throws Exception {
         setup();
 
+        installAndAssertFeature("karaf-rest-example-provider");
+
         installAndAssertFeature("karaf-rest-example-client-http");
 
         verify();
@@ -54,9 +65,39 @@ public class RestExampleTest extends KarafTestSupport {
     public void testWithCxfClient() throws Exception {
         setup();
 
+        installAndAssertFeature("karaf-rest-example-provider");
+
         installAndAssertFeature("karaf-rest-example-client-cxf");
 
         verify();
+    }
+
+    @Test
+    public void testWhiteboard() throws Exception {
+        setup();
+
+        installAndAssertFeature("karaf-rest-example-whiteboard");
+
+        String output = executeCommand("http:list");
+        while (!output.contains("Deployed")) {
+            Thread.sleep(500);
+            output = executeCommand("http:list");
+        }
+        System.out.println(output);
+
+        URL url = new URL("http://localhost:" + getHttpPort() + "/booking");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        StringBuffer buffer = new StringBuffer();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+        }
+
+        output = buffer.toString();
+        assertEquals("[]", output);
     }
 
 }
