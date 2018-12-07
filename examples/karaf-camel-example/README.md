f33941dbb88393bfc06f2ab72ebe5a5937ecef45
Jean-Baptiste Onofré <jbonofre@apache.org>
2018-12-07 17:30:32 +0100

    [KARAF-5893] Add camel example

---
+++ examples/karaf-camel-example/README.md
@@ -0,0 +1,106 @@
+<!--
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+-->
+# Apache Karaf Camel Example
+
+## Abstract
+
+This example shows how to use Apache Camel in Karaf. Apache Camel is a integration framework, allowing you to integrate several systems and applications all together.
+
+Apache Camel supports several DSL. This example shows how to use the Camel Java DSL and the Camel Blueprint DSL.
+
+It creates several Camel routes, exposing a HTTP endpoint and using a Content Based Router EIP (Enterprise Integration Pattern).
+
+## Artifacts
+
+* **karaf-camel-example-java** is a bundle containing routes described using the Camel Java DSL.
+* **karaf-camel-example-blueprint** is just a wrapper containing routes described using Blueprint. Karaf supports deployment of this DSL directly (in the deploy folder for instance) or packaged as a bundle.
+* **karaf-camel-example-features** provides a Karaf features repository used for the deployment.
+
+## Build
+
+The build uses Apache Maven. Simply use:
+
+```
+mvn clean install
+```
+
+## Feature and Deployment
+
+On a running Karaf instance, register the features repository using:
+
+```
+karaf@root()> feature:repo-add mvn:org.apache.karaf.examples/karaf-camel-example-features/4.2.2-SNAPSHOT/xml
+```
+
+Then, you can install either Camel Java or Blueprint features:
+
+```
+karaf@root()> feature:install karaf-camel-example-java
+```
+
+```
+karaf@root()> feature:install karaf-camel-example-blueprint
+```
+
+## Usage
+
+Once you have install a Camel feature, the main route is started and bind a HTTP endpoint on `http://localhost:9090/example`.
+
+We can test payloads testing different paths of the content based router.
+
+First, let's try an e-mail request:
+
+```
+curl -X POST -H "Content-Type: application/json" http://localhost:9090/example -d '{ "notification": { "type": "email", "to": "foo@bar.com", "message": "This is a test" }}'
+```
+
+You can see in the log that the e-mail route has been called in the log:
+
+```
+karaf@root()> log:display
+10:24:22.453 INFO [qtp1790923892-140] [EXAMPLE INBOUND] Received: { "notification": { "type": "email", "to": "foo@bar.com", "message": "This is a test" }}
+10:24:22.465 INFO [qtp1790923892-140] [EXAMPLE INBOUND] Received email notification
+10:24:22.466 INFO [qtp1790923892-140] [EXAMPLE EMAIL] Sending notification email
+```
+
+And we have the curl response confirming it:
+
+```
+{ "status": "email sent", "to": "foo@bar.com", "subject": "Notification"}
+```
+
+We can also test the `http` path of the content based router:
+
+```
+curl -X POST -H "Content-Type: application/json" http://localhost:9090/example -d '{ "notification": { "type": "http", "service": "http://foo" }}'
+```
+
+We can see the response confirming the call to the http route path:
+
+```
+{ "status": "http requested", "service": "http://foo" }
+```
+
+and the corresponding log messages:
+
+```
+11:17:28.372 INFO [qtp1790923892-138] [EXAMPLE INBOUND] Received: { "notification": { "type": "http", "service": "http://foo" }}
+11:17:28.374 INFO [qtp1790923892-138] [EXAMPLE INBOUND] Received http notification
+11:17:28.374 INFO [qtp1790923892-138] [EXAMPLE HTTP] Sending http notification
+```
---
+++ itests/test/src/test/java/org/apache/karaf/itests/examples/CamelExampleTest.java
@@ -0,0 +1,113 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.karaf.itests.examples;
+
+import org.apache.karaf.itests.KarafTestSupport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class CamelExampleTest extends KarafTestSupport {
+
+    public void setup() throws Exception {
+        addFeaturesRepository("mvn:org.apache.karaf.examples/karaf-camel-example-features/" + System.getProperty("karaf.version") + "/xml");
+    }
+
+    @Test
+    public void testJavaDSL() throws Exception {
+        setup();
+        installAndAssertFeature("karaf-camel-example-java");
+        verify();
+    }
+
+    @Test
+    public void testBlueprintDSL() throws Exception {
+        setup();
+        installAndAssertFeature("karaf-camel-example-blueprint");
+        verify();
+    }
+
+    public void verify() throws Exception {
+        String output = executeCommand("camel:route-list");
+        while (true) {
+            if (!output.contains("Started")) {
+                Thread.sleep(500);
+                output = executeCommand("camel:route-list");
+            } else {
+                break;
+            }
+        }
+        System.out.println(output);
+
+        URL url = new URL("http://localhost:9090/example");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(connection.getOutputStream()));
+        writer.println("{ \"notification\": { \"type\": \"email\", \"to\": \"foo@bar.com\", \"message\": \"this is a test\" }}");
+        writer.flush();
+        writer.close();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        StringBuffer buffer = new StringBuffer();
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+        reader.close();
+
+        output = buffer.toString();
+        System.out.println(output);
+        assertEquals("{ \"status\": \"email sent\", \"to\": \"foo@bar.com\", \"subject\": \"Notification\" }", output);
+
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+
+        writer = new PrintWriter(new OutputStreamWriter(connection.getOutputStream()));
+        writer.println("{ \"notification\": { \"type\": \"http\", \"service\": \"http://foo\" }}");
+        writer.flush();
+        writer.close();
+
+        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        buffer = new StringBuffer();
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+        reader.close();
+
+        output = buffer.toString();
+        System.out.println(output);
+        assertEquals("{ \"status\": \"http requested\", \"service\": \"http://foo\" }", output);
+    }
+
+}
