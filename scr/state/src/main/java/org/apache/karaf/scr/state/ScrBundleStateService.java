e7201d42fc4e9a13b32c9f42dda5ae40a9648be5
Christian Schneider <cschneid@adobe.com>
2019-04-04 17:16:40 +0200

    Use streams in ScrBundleStateService

--- scr/state/src/main/java/org/apache/karaf/scr/state/ScrBundleStateService.java
+++ scr/state/src/main/java/org/apache/karaf/scr/state/ScrBundleStateService.java
@@ -16,21 +16,25 @@
  */
 package org.apache.karaf.scr.state;
 
+import static java.lang.String.join;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+
+import java.util.stream.Stream;
+
 import org.apache.karaf.bundle.core.BundleState;
 import org.apache.karaf.bundle.core.BundleStateService;
 import org.osgi.framework.Bundle;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.runtime.ServiceComponentRuntime;
 import org.osgi.service.component.runtime.dto.ComponentConfigurationDTO;
-import org.osgi.service.component.runtime.dto.ComponentDescriptionDTO;
-import org.osgi.service.component.runtime.dto.UnsatisfiedReferenceDTO;
 
+@Component
 public class ScrBundleStateService implements BundleStateService {
 
-    ServiceComponentRuntime scr;
-
-    public ScrBundleStateService(ServiceComponentRuntime scr) {
-        this.scr = scr;
-    }
+    @Reference
+    private ServiceComponentRuntime scr;
 
     @Override
     public String getName() {
@@ -39,54 +43,39 @@ public class ScrBundleStateService implements BundleStateService {
 
     @Override
     public String getDiag(Bundle bundle) {
-        StringBuilder sb = new StringBuilder();
-        for (ComponentDescriptionDTO desc : scr.getComponentDescriptionDTOs(bundle)) {
-            for (ComponentConfigurationDTO cfg : scr.getComponentConfigurationDTOs(desc)) {
-                if (cfg.state != ComponentConfigurationDTO.ACTIVE
-                        && cfg.state != ComponentConfigurationDTO.SATISFIED) {
-                    sb.append(cfg.description.name).append(" (").append(cfg.id).append(")\n");
-                    if ((cfg.state & ComponentConfigurationDTO.UNSATISFIED_CONFIGURATION) != 0) {
-                        sb.append("  missing configurations: ");
-                        boolean first = true;
-                        for (String s : cfg.description.configurationPid) {
-                            if (!first) {
-                                sb.append(", ");
-                            }
-                            sb.append(s);
-                            first = false;
-                        }
-                        sb.append("\n");
-                    }
-                    if ((cfg.state & ComponentConfigurationDTO.UNSATISFIED_REFERENCE) != 0) {
-                        sb.append("  missing references: ");
-                        boolean first = true;
-                        for (UnsatisfiedReferenceDTO ur : cfg.unsatisfiedReferences) {
-                            if (!first) {
-                                sb.append(", ");
-                            }
-                            sb.append(ur.name);
-                            first = false;
-                        }
-                        sb.append("\n");
-                    }
-                }
-            }
-        }
-        return sb.toString();
+        return allCompConfigs(bundle).map(this::diagComponent).collect(joining());
     }
 
     @Override
     public BundleState getState(Bundle bundle) {
-        if (bundle.getState() == Bundle.ACTIVE) {
-            for (ComponentDescriptionDTO desc : scr.getComponentDescriptionDTOs(bundle)) {
-                for (ComponentConfigurationDTO cfg : scr.getComponentConfigurationDTOs(desc)) {
-                    if (cfg.state != ComponentConfigurationDTO.ACTIVE
-                            && cfg.state != ComponentConfigurationDTO.SATISFIED) {
-                        return BundleState.Waiting;
-                    }
-                }
-            }
+        boolean waiting = bundle.getState() == Bundle.ACTIVE && allCompConfigs(bundle).anyMatch(this::unsatisfied);
+        return waiting ? BundleState.Waiting : BundleState.Unknown;
+    }
+
+    private String diagComponent(ComponentConfigurationDTO cfg) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(cfg.description.name).append(" (").append(cfg.id).append(")\n");
+        if ((cfg.state & ComponentConfigurationDTO.UNSATISFIED_CONFIGURATION) != 0) {
+            sb.append("  missing configurations: ");
+            sb.append(join(", ", cfg.description.configurationPid));
+            sb.append("\n");
         }
-        return BundleState.Unknown;
+        if ((cfg.state & ComponentConfigurationDTO.UNSATISFIED_REFERENCE) != 0) {
+            sb.append("  missing references: ");
+            String references = asList(cfg.unsatisfiedReferences).stream().map(ref -> ref.name).collect(joining(", "));
+            sb.append(references);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private Stream<ComponentConfigurationDTO> allCompConfigs(Bundle bundle) {
+        return scr.getComponentDescriptionDTOs(bundle).stream()
+            .flatMap(desc -> scr.getComponentConfigurationDTOs(desc).stream());
+    }
+
+    private boolean unsatisfied(ComponentConfigurationDTO cfg) {
+        return cfg.state != ComponentConfigurationDTO.ACTIVE
+                && cfg.state != ComponentConfigurationDTO.SATISFIED;
     }
 }
