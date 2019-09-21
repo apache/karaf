0a9dbe37af061a2fe5a0f8ac2e48976ec364785d
Jean-Baptiste Onofré <jbonofre@apache.org>
2019-09-21 13:17:50 +0200

    Revert "KARAF-6405 - Make sure Config files are jailed to the container etc directory"
    This reverts commit 226e9bb43d8f77009508a7f7584a35e9036b2272.

--- features/core/src/test/java/org/apache/karaf/features/internal/service/FeatureConfigInstallerTest.java
+++ features/core/src/test/java/org/apache/karaf/features/internal/service/FeatureConfigInstallerTest.java
@@ -16,25 +16,14 @@
  */
 package org.apache.karaf.features.internal.service;
 
-import org.apache.karaf.features.Feature;
-import org.easymock.EasyMock;
 import org.junit.Test;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FeatureConfigInstallerTest {
-
+    
     private void substEqual(final String src, final String subst) {
         assertEquals(FeatureConfigInstaller.substFinalName(src), subst);
     }
@@ -44,11 +33,11 @@ public class FeatureConfigInstallerTest {
         final String karafBase = "/tmp/karaf.base";
         final String karafEtc = karafBase + "/etc";
         final String foo = "/foo";
-
+        
         System.setProperty("karaf.base", karafBase);
         System.setProperty("karaf.etc", karafEtc);
         System.setProperty("foo", foo);
-
+        
         substEqual("etc/test.cfg", karafBase + File.separator + "etc/test.cfg");
         substEqual("/etc/test.cfg", karafBase + File.separator + "/etc/test.cfg");
         substEqual("${karaf.etc}/test.cfg", karafEtc + "/test.cfg");
@@ -60,108 +49,6 @@ public class FeatureConfigInstallerTest {
         substEqual("${karaf.base}${bar}/etc/test.cfg", karafBase + "/etc/test.cfg");
         substEqual("etc${}/${foo}/test.cfg", karafBase + File.separator + "etc//test.cfg");
         substEqual("${foo}${bar}/${bar}${foo}", foo + "/" + foo);
-    }
-
-    @Test
-    public void testGoodConfigName() throws Exception {
-        String dataDir = "data4";
-
-        Path karafEtc = Files.createTempDirectory("etc");
-
-        System.setProperty("karaf.etc", karafEtc.toString());
-
-        RepositoryImpl repo = new RepositoryImpl(getClass().getResource(dataDir + "/features.xml").toURI());
-        Feature f100 = repo.getFeatures()[0];
-        assertEquals("goodConfigName", f100.getName());
-
-        ConfigurationAdmin configAdmin = EasyMock.createMock(ConfigurationAdmin.class);
-        Configuration config = EasyMock.createMock(Configuration.class);
-
-        EasyMock.expect(configAdmin.listConfigurations(EasyMock.anyString())).andReturn(null);
-        EasyMock.expectLastCall().anyTimes();
-        EasyMock.expect(configAdmin.getConfiguration(EasyMock.anyString(), EasyMock.isNull())).andReturn(config);
-        EasyMock.expectLastCall();
-
-        EasyMock.replay(configAdmin);
-        FeatureConfigInstaller installer = new FeatureConfigInstaller(configAdmin);
-        installer.installFeatureConfigs(f100);
-
-        EasyMock.verify(configAdmin);
-
-        File installedFile = Paths.get(karafEtc.toString(), "config.cfg").toFile();
-        assertTrue(installedFile.exists());
-
-        installedFile.delete();
-        karafEtc.toFile().delete();
-    }
-
-    @Test
-    public void testBadConfigName() throws Exception {
-        String dataDir = "data4";
-
-        Path karafEtc = Files.createTempDirectory("etc");
-
-        System.setProperty("karaf.etc", karafEtc.toString());
-
-        RepositoryImpl repo = new RepositoryImpl(getClass().getResource(dataDir + "/features.xml").toURI());
-        Feature f100 = repo.getFeatures()[1];
-        assertEquals("badConfigName", f100.getName());
-
-        ConfigurationAdmin configAdmin = EasyMock.createMock(ConfigurationAdmin.class);
-        Configuration config = EasyMock.createMock(Configuration.class);
-
-        EasyMock.expect(configAdmin.listConfigurations(EasyMock.anyString())).andReturn(null);
-        EasyMock.expectLastCall().anyTimes();
-        EasyMock.expect(configAdmin.getConfiguration(EasyMock.anyString(), EasyMock.isNull())).andReturn(config);
-        EasyMock.expectLastCall();
-
-        EasyMock.replay(configAdmin);
-        FeatureConfigInstaller installer = new FeatureConfigInstaller(configAdmin);
-        installer.installFeatureConfigs(f100);
-
-        EasyMock.verify(configAdmin);
-
-        // Verify the file was not installed
-        File installedFile = Paths.get(karafEtc.toString(), "../../../../../../../../../../../../tmp/config").toFile();
-        assertFalse(installedFile.exists());
-
-        karafEtc.toFile().delete();
-    }
-
-    @Test
-    public void testBadConfigFileName() throws Exception {
-        String dataDir = "data4";
-
-        Path karafEtc = Files.createTempDirectory("etc");
-        Path karafBase = Files.createTempDirectory("base");
-
-        System.setProperty("karaf.etc", karafEtc.toString());
-        System.setProperty("karaf.base", karafBase.toString());
-
-        RepositoryImpl repo = new RepositoryImpl(getClass().getResource(dataDir + "/features.xml").toURI());
-        Feature f100 = repo.getFeatures()[2];
-        assertEquals("badConfigFileName", f100.getName());
-
-        ConfigurationAdmin configAdmin = EasyMock.createMock(ConfigurationAdmin.class);
-        EasyMock.expect(configAdmin.listConfigurations(EasyMock.anyString())).andReturn(null);
-        EasyMock.expectLastCall().anyTimes();
-
-        EasyMock.replay(configAdmin);
-        FeatureConfigInstaller installer = new FeatureConfigInstaller(configAdmin);
-        try {
-            installer.installFeatureConfigs(f100);
-            fail("Failure expected on a bad config filename");
-        } catch (Throwable t) {
-            // expected
-        }
-        EasyMock.verify(configAdmin);
-
-        // Verify the file was not installed
-        File installedFile = Paths.get(karafBase.toString(), "../../../../../../../../../../../../tmp/config2").toFile();
-        assertFalse(installedFile.exists());
-
-        karafBase.toFile().delete();
-        karafEtc.toFile().delete();
     }
 
 }
