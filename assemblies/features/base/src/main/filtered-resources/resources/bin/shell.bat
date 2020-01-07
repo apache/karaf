dfdcf2eef5ca2d44cc3ac36b312cf486d141f10e
Jean-Baptiste Onofré <jbonofre@apache.org>
2020-01-07 18:33:40 +0100

    [KARAF-6525] Add osgi.core in the shell classpath (required by pax-logging)
    (cherry picked from commit db2cccf3d88e16bebad93604fdd0e94b930a01a9)

--- assemblies/features/base/src/main/filtered-resources/resources/bin/shell.bat
+++ assemblies/features/base/src/main/filtered-resources/resources/bin/shell.bat
@@ -140,6 +140,7 @@ if "%KARAF_DEBUG%" == "" goto :KARAF_DEBUG_END
 set CLASSPATH=%KARAF_HOME%\system\org\apache\karaf\shell\org.apache.karaf.shell.core\@@project.version@@\org.apache.karaf.shell.core-@@project.version@@.jar
 set CLASSPATH=%CLASSPATH%;%KARAF_HOME%\system\org\ops4j\pax\logging\pax-logging-api\@@pax.logging.version@@\pax-logging-api-@@pax.logging.version@@.jar
 set CLASSPATH=%CLASSPATH%;%KARAF_HOME%\system\org\jline\jline\@@jline.version@@\jline-@@jline.version@@.jar
+set CLASSPATH=%CLASSPATH%;%KARAF_HOME%\lib\boot\osgi.core-@@osgi.version@@.jar
 
 :EXECUTE
     if "%SHIFT%" == "true" SET ARGS=%2 %3 %4 %5 %6 %7 %8
