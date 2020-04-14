c1ccb040473d91b4f8be2b31c13a7e9eb7fd11bf
jbonofre <jbonofre@apache.org>
2020-04-14 14:10:41 +0200

    [KARAF-6609] Upgrade to Pax Web 7.2.15 and Jetty 9.4.28.v20200408
    (cherry picked from commit 0b46b1141835db8aa7d160563e6540ac004bac05)

--- itests/test/src/test/java/org/apache/karaf/itests/WebTest.java
+++ itests/test/src/test/java/org/apache/karaf/itests/WebTest.java
@@ -83,7 +83,7 @@ public class WebTest extends BaseTest {
         System.out.println(buffer.toString());
         assertContains("Hello World!", buffer.toString());
 
-        System.out.println(executeCommand("web:uninstall 125"));
+        System.out.println(executeCommand("web:uninstall 126"));
         listOutput = executeCommand("web:list");
         System.out.println(listOutput);
         while (listOutput.contains("/test")) {
