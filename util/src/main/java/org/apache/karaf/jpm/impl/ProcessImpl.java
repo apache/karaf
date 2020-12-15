b5bfe88d7a0137eaad03410491cb786dc463e03a
Miel Donkers <miel.donkers@instana.com>
2020-12-15 22:26:34 +0100

    [KARAF-6963] First send SIGTERM, only SIGKILL if not shutting down nicely

--- util/src/main/java/org/apache/karaf/jpm/impl/ProcessImpl.java
+++ util/src/main/java/org/apache/karaf/jpm/impl/ProcessImpl.java
@@ -81,7 +81,27 @@ public class ProcessImpl implements Process {
             props.put("${pid}", Integer.toString(pid));
             ret = ScriptUtils.execute("destroy", props);
         } else {
-            ret = ScriptUtils.executeProcess(new java.lang.ProcessBuilder("kill", "-9", Integer.toString(pid)));
+            // Try regular SIGTERM command first
+            ret = ScriptUtils.executeProcess(new java.lang.ProcessBuilder("kill", Integer.toString(pid)));
+            if (ret == 0) {
+                boolean isRunning = true;
+                int loopCounter = 0;
+
+                // Verify in 1 second intervals whether the process is still running, so not to block too long
+                do {
+                    loopCounter++;
+                    try {
+                        Thread.sleep(1000);
+                        isRunning = isRunning();
+                    } catch (InterruptedException | IOException ignore) {
+                    }
+                } while (isRunning && loopCounter <= 10);
+
+                // Verify the process got closed nicely, if not send a SIGKILL
+                if (isRunning) {
+                    ret = ScriptUtils.executeProcess(new java.lang.ProcessBuilder("kill", "-9", Integer.toString(pid)));
+                }
+            }
         }
         if (ret != 0) {
             throw new IOException("Unable to destroy process, it may already be terminated");
