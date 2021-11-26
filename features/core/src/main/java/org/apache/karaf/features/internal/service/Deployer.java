6501a29b58b7d52bd29200ed758df2bad070fb91
Stefan Wachter <stefan.wachter@gmx.de>
2021-11-26 11:25:14 +0100

    KARAF-7282: fix autoRefresh setting handling in Deployer
    (cherry picked from commit 275b987cc0c98705f30fd163d157e94776c73682)

--- features/core/src/main/java/org/apache/karaf/features/internal/service/Deployer.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/service/Deployer.java
@@ -357,9 +357,9 @@ public class Deployer {
         boolean noRefreshManaged = request.options.contains(FeaturesService.Option.NoAutoRefreshManagedBundles);
         boolean noRefresh = request.options.contains(FeaturesService.Option.NoAutoRefreshBundles);
         if (!request.autoRefresh) {
-            noRefreshUnmanaged = request.autoRefresh;
-            noRefreshManaged = request.autoRefresh;
-            noRefresh = request.autoRefresh;
+            noRefreshUnmanaged = true;
+            noRefreshManaged = true;
+            noRefresh = true;
         }
         boolean noStart = request.options.contains(FeaturesService.Option.NoAutoStartBundles);
         boolean verbose = request.options.contains(FeaturesService.Option.Verbose);
