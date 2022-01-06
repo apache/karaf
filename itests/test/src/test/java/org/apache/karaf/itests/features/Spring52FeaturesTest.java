c469e79bdfa8f88a5c39504544a39c4a7597b0a6
Jean-Baptiste Onofré <jbonofre@apache.org>
2022-01-06 15:09:47 +0100

    [KARAF-7319] Upgrade to Spring Security 5.6.1
    (cherry picked from commit c0832dc07489627bb70451943293afe8e83f0f51)

--- itests/test/src/test/java/org/apache/karaf/itests/features/Spring52FeaturesTest.java
+++ itests/test/src/test/java/org/apache/karaf/itests/features/Spring52FeaturesTest.java
@@ -111,7 +111,7 @@ public class Spring52FeaturesTest extends BaseTest {
 
     @Test
     public void installSpringSecurityFeature() throws Exception {
-        installAssertAndUninstallFeature("spring-security", System.getProperty("spring.security53.version"));
+        installAssertAndUninstallFeature("spring-security", System.getProperty("spring.security55.version"));
     }
 
 }
