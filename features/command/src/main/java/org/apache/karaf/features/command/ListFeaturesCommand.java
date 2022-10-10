1b18c1b371223616bbe11e4018f5e9e35a19ea2c
Aleksy Wróblewski <aleksy.wroblewski@bbbit.io>
2022-10-10 23:49:02 +0200

    [KARAF-4542] - add '--repository' option to feature:list

--- features/command/src/main/java/org/apache/karaf/features/command/ListFeaturesCommand.java
+++ features/command/src/main/java/org/apache/karaf/features/command/ListFeaturesCommand.java
@@ -17,14 +17,15 @@
 package org.apache.karaf.features.command;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeaturesService;
 import org.apache.karaf.features.Repository;
+import org.apache.karaf.features.command.completers.InstalledRepoNameCompleter;
 import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Completion;
 import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.support.table.Row;
@@ -49,12 +50,16 @@ public class ListFeaturesCommand extends FeaturesCommandSupport {
     @Option(name = "-o", aliases = {"--ordered"}, description = "Display a list using alphabetical order ", required = false, multiValued = false)
     boolean ordered;
 
+    @Option(name = "--repository", description = "Only list features from that repository", required = false, multiValued = false)
+    @Completion(InstalledRepoNameCompleter.class)
+    String repository;
+
     @Option(name = "--no-format", description = "Disable table rendered output", required = false, multiValued = false)
     boolean noFormat;
 
     protected void doExecute(FeaturesService featuresService) throws Exception {
         boolean needsLegend = false;
-        
+
         ShellTable table = new ShellTable();
         table.column("Name");
         table.column("Version");
@@ -67,43 +72,16 @@ public class ListFeaturesCommand extends FeaturesCommandSupport {
         }
         table.emptyTableText(onlyInstalled ? "No features installed" : "No features available");
 
-        List<Repository> repos = Arrays.asList(featuresService.listRepositories());
-        for (Repository r : repos) {
-            List<Feature> features = Arrays.asList(r.getFeatures());
-            if (ordered) {
-                features.sort(new FeatureComparator());
-            }
-            for (Feature f : features) {
-                if (onlyInstalled && !featuresService.isInstalled(f)) {
-                    // Filter out not installed features if we only want to see the installed ones
-                    continue;
-                }
-                if (onlyRequired && !featuresService.isRequired(f)) {
-                    // Filter out not installed features if we only want to see the installed ones
-                    continue;
-                }
-                if (!showBlacklisted && f.isBlacklisted()) {
-                    // Filter out blacklisted
-                    continue;
-                }
-                if (!showHidden && f.isHidden()) {
-                    // Filter out hidden feature if not asked to display those
-                    continue;
-                }
-                Row row = table.addRow();
-                row.addContent(
-                        f.getName(),
-                        f.getVersion(),
-                        featuresService.isRequired(f) ? "x" : "",
-                        featuresService.getState(f.getId()),
-                        r.getName(),
-                        f.getDescription());
-                if (showBlacklisted) {
-                    row.addContent(f.isBlacklisted() ? "yes" : "no");
-                }
-                if (isInstalledViaDeployDir(r.getName())) {
-                    needsLegend = true;
-                }
+        if (repository != null) {
+            List<Feature> features = Arrays.asList(featuresService.getRepository(repository).getFeatures());
+            populateTable(featuresService, repository, features, table);
+            needsLegend = isInstalledViaDeployDir(repository);
+        } else {
+            Repository[] repos = featuresService.listRepositories();
+            for (Repository r : repos) {
+                String repoName = r.getName();
+                populateTable(featuresService, repoName, Arrays.asList(r.getFeatures()), table);
+                needsLegend = isInstalledViaDeployDir(repoName);
             }
         }
 
@@ -119,10 +97,47 @@ public class ListFeaturesCommand extends FeaturesCommandSupport {
         return st != null && st.length() > 1 && st.charAt(st.length() - 1) == '*';
     }
 
-    class FeatureComparator implements Comparator<Feature> {
-        public int compare(Feature o1, Feature o2) {
-            return o1.getName().toLowerCase().compareTo( o2.getName().toLowerCase() );
+    private void populateTable(FeaturesService featuresService,
+                               String repositoryName,
+                               List<Feature> features,
+                               ShellTable table) {
+        if (ordered) {
+            features.sort(new FeatureComparator());
+        }
+        for (Feature f : features) {
+            if (onlyInstalled && !featuresService.isInstalled(f)) {
+                // Filter out not installed features if we only want to see the installed ones
+                continue;
+            }
+            if (onlyRequired && !featuresService.isRequired(f)) {
+                // Filter out not installed features if we only want to see the installed ones
+                continue;
+            }
+            if (!showBlacklisted && f.isBlacklisted()) {
+                // Filter out blacklisted
+                continue;
+            }
+            if (!showHidden && f.isHidden()) {
+                // Filter out hidden feature if not asked to display those
+                continue;
+            }
+            Row row = table.addRow();
+            row.addContent(
+                    f.getName(),
+                    f.getVersion(),
+                    featuresService.isRequired(f) ? "x" : "",
+                    featuresService.getState(f.getId()),
+                    repositoryName,
+                    f.getDescription());
+            if (showBlacklisted) {
+                row.addContent(f.isBlacklisted() ? "yes" : "no");
+            }
         }
     }
 
+    class FeatureComparator implements Comparator<Feature> {
+        public int compare(Feature o1, Feature o2) {
+            return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+        }
+    }
 }
