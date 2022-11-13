d14c6d18c97b2db6b623c4984768acb9b70b7fad
Robert Varga <robert.varga@pantheon.tech>
2022-11-13 22:10:43 +0100

    Revert "[KARAF-7607]: require VINTR, VQUIT and VDSUSP"
    This reverts commit 96128a63c7c71f613b88ca05017c7e7e75b86d85, as it
    prevents the fix from working.

--- shell/ssh/src/main/java/org/apache/karaf/shell/ssh/SshAction.java
+++ shell/ssh/src/main/java/org/apache/karaf/shell/ssh/SshAction.java
@@ -193,9 +193,8 @@ public class SshAction implements Action {
                     try {
                         Map<PtyMode, Integer> modes = new HashMap<>();
                         // Control chars
-                        requireMode(modes, PtyMode.VINTR, attributes, ControlChar.VINTR);
-                        requireMode(modes, PtyMode.VQUIT, attributes, ControlChar.VQUIT);
-                        requireMode(modes, PtyMode.VDSUSP, attributes, ControlChar.VDSUSP);
+                        addMode(modes, PtyMode.VINTR, attributes, ControlChar.VINTR);
+                        addMode(modes, PtyMode.VQUIT, attributes, ControlChar.VQUIT);
                         addMode(modes, PtyMode.VERASE, attributes, ControlChar.VERASE);
                         addMode(modes, PtyMode.VKILL, attributes, ControlChar.VKILL);
                         addMode(modes, PtyMode.VEOF, attributes, ControlChar.VEOF);
@@ -204,6 +203,7 @@ public class SshAction implements Action {
                         addMode(modes, PtyMode.VSTART, attributes, ControlChar.VSTART);
                         addMode(modes, PtyMode.VSTOP, attributes, ControlChar.VSTOP);
                         addMode(modes, PtyMode.VSUSP, attributes, ControlChar.VSUSP);
+                        addMode(modes, PtyMode.VDSUSP, attributes, ControlChar.VDSUSP);
                         addMode(modes, PtyMode.VREPRINT, attributes, ControlChar.VREPRINT);
                         addMode(modes, PtyMode.VWERASE, attributes, ControlChar.VWERASE);
                         addMode(modes, PtyMode.VLNEXT, attributes, ControlChar.VLNEXT);
@@ -313,14 +313,6 @@ public class SshAction implements Action {
         if (value != -1) {
             modes.put(mode, value);
         }
-    }
-
-    private static void requireMode(Map<PtyMode, Integer> modes, PtyMode mode, Attributes attributes, ControlChar ctrl) {
-        final int value = attributes.getControlChar(ctrl);
-        if (value == -1) {
-            throw new IllegalStateException("Required control character " + ctrl + " is not available");
-        }
-        modes.put(mode, value);
     }
 
     private static int getFlag(Attributes attributes, InputFlag flag) {
