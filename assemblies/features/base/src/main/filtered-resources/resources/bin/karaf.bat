ed41a6db175694def1b3f9aba0c415bc477557e5
Robert Varga <robert.varga@pantheon.tech>
2023-09-22 11:09:54 +0200

    [KARAF-7766] Require Java 11 at runtime
    Enforce JVM version 11 on startup. Also simplify ./bin/karaf(.bat).
    
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- assemblies/features/base/src/main/filtered-resources/resources/bin/karaf.bat
+++ assemblies/features/base/src/main/filtered-resources/resources/bin/karaf.bat
@@ -418,56 +418,39 @@ if "%KARAF_PROFILER%" == "" goto :RUN
     if "%IS_RUNNABLE%" == "true" (
         rem If major version is greater than 1 (meaning Java 9 or 10), we don't use endorsed lib but module
         rem If major version is 1 (meaning Java 1.6, 1.7, 1.8), we use endorsed lib
-        if %JAVA_VERSION% GTR 8 (
-            "%JAVA%" %JAVA_OPTS% %OPTS% ^
-                --add-reads=java.xml=java.logging ^
-                --add-exports=java.base/org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED ^
-                --patch-module java.base="%KARAF_HOME%\lib\endorsed\org.apache.karaf.specs.locator-@@project.version@@.jar" ^
-                --patch-module java.xml="%KARAF_HOME%\lib\endorsed\org.apache.karaf.specs.java.xml-@@project.version@@.jar" ^
-                --add-opens java.base/java.security=ALL-UNNAMED ^
-                --add-opens java.base/java.net=ALL-UNNAMED ^
-                --add-opens java.base/java.lang=ALL-UNNAMED ^
-                --add-opens java.base/java.util=ALL-UNNAMED ^
-                --add-opens java.naming/javax.naming.spi=ALL-UNNAMED ^
-                --add-opens java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED ^
-                --add-exports=java.base/sun.net.www.protocol.file=ALL-UNNAMED ^
-                --add-exports=java.base/sun.net.www.protocol.ftp=ALL-UNNAMED ^
-                --add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED ^
-                --add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED ^
-                --add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED ^
-                --add-exports=java.base/sun.net.www.content.text=ALL-UNNAMED ^
-                --add-exports=jdk.xml.dom/org.w3c.dom.html=ALL-UNNAMED ^
-                --add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED ^
-                --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED ^
-                --add-exports=java.security.sasl/com.sun.security.sasl=ALL-UNNAMED ^
-                --add-exports=java.naming/com.sun.jndi.ldap=ALL-UNNAMED ^
-                -classpath "%CLASSPATH%" ^
-                -Dkaraf.instances="%KARAF_HOME%\instances" ^
-                -Dkaraf.home="%KARAF_HOME%" ^
-                -Dkaraf.base="%KARAF_BASE%" ^
-                -Dkaraf.etc="%KARAF_ETC%" ^
-                -Dkaraf.log="%KARAF_LOG%" ^
-                -Dkaraf.restart.jvm.supported=true ^
-                -Djava.io.tmpdir="%KARAF_DATA%\tmp" ^
-                -Dkaraf.data="%KARAF_DATA%" ^
-                -Djava.util.logging.config.file="%KARAF_BASE%\etc\java.util.logging.properties" ^
-                %KARAF_SYSTEM_OPTS% %KARAF_OPTS% %MAIN% %ARGS%
-        ) else (
-            "%JAVA%" %JAVA_OPTS% %OPTS% ^
-                -classpath "%CLASSPATH%" ^
-                -Djava.endorsed.dirs="%JAVA_HOME%\jre\lib\endorsed;%JAVA_HOME%\lib\endorsed;%KARAF_HOME%\lib\endorsed" ^
-                -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%JAVA_HOME%\lib\ext;%KARAF_HOME%\lib\ext" ^
-                -Dkaraf.instances="%KARAF_HOME%\instances" ^
-                -Dkaraf.home="%KARAF_HOME%" ^
-                -Dkaraf.base="%KARAF_BASE%" ^
-                -Dkaraf.etc="%KARAF_ETC%" ^
-                -Dkaraf.log="%KARAF_LOG%" ^
-                -Dkaraf.restart.jvm.supported=true ^
-                -Djava.io.tmpdir="%KARAF_DATA%\tmp" ^
-                -Dkaraf.data="%KARAF_DATA%" ^
-                -Djava.util.logging.config.file="%KARAF_BASE%\etc\java.util.logging.properties" ^
-                %KARAF_SYSTEM_OPTS% %KARAF_OPTS% %MAIN% %ARGS%
-        )
+        "%JAVA%" %JAVA_OPTS% %OPTS% ^
+        --add-reads=java.xml=java.logging ^
+        --add-exports=java.base/org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED ^
+        --patch-module java.base="%KARAF_HOME%\lib\endorsed\org.apache.karaf.specs.locator-@@project.version@@.jar" ^
+        --patch-module java.xml="%KARAF_HOME%\lib\endorsed\org.apache.karaf.specs.java.xml-@@project.version@@.jar" ^
+        --add-opens java.base/java.security=ALL-UNNAMED ^
+        --add-opens java.base/java.net=ALL-UNNAMED ^
+        --add-opens java.base/java.lang=ALL-UNNAMED ^
+        --add-opens java.base/java.util=ALL-UNNAMED ^
+        --add-opens java.naming/javax.naming.spi=ALL-UNNAMED ^
+        --add-opens java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED ^
+        --add-exports=java.base/sun.net.www.protocol.file=ALL-UNNAMED ^
+        --add-exports=java.base/sun.net.www.protocol.ftp=ALL-UNNAMED ^
+        --add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED ^
+        --add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED ^
+        --add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED ^
+        --add-exports=java.base/sun.net.www.content.text=ALL-UNNAMED ^
+        --add-exports=jdk.xml.dom/org.w3c.dom.html=ALL-UNNAMED ^
+        --add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED ^
+        --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED ^
+        --add-exports=java.security.sasl/com.sun.security.sasl=ALL-UNNAMED ^
+        --add-exports=java.naming/com.sun.jndi.ldap=ALL-UNNAMED ^
+        -classpath "%CLASSPATH%" ^
+        -Dkaraf.instances="%KARAF_HOME%\instances" ^
+        -Dkaraf.home="%KARAF_HOME%" ^
+        -Dkaraf.base="%KARAF_BASE%" ^
+        -Dkaraf.etc="%KARAF_ETC%" ^
+        -Dkaraf.log="%KARAF_LOG%" ^
+        -Dkaraf.restart.jvm.supported=true ^
+        -Djava.io.tmpdir="%KARAF_DATA%\tmp" ^
+        -Dkaraf.data="%KARAF_DATA%" ^
+        -Djava.util.logging.config.file="%KARAF_BASE%\etc\java.util.logging.properties" ^
+        %KARAF_SYSTEM_OPTS% %KARAF_OPTS% %MAIN% %ARGS%
     ) else (
         echo There is a Root instance already running with name %ROOT_INSTANCE_NAME% and pid %ROOT_INSTANCE_PID%. If you know what you are doing and want to force the run anyway, SET CHECK_ROOT_INSTANCE_RUNNING=false and re run the command.
         goto :END
