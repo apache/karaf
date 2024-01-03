43fcb6175483864dd7e5e5d4be0e4f9e2963424b
JB Onofré <jb@nanthrax.net>
2024-01-03 17:11:05 +0100

    KARAF-7775: Move config shell commands in a dedicated bundle

---
+++ config/command/src/main/java/org/apache/karaf/config/command/MetaCommand.java
@@ -0,0 +1,191 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.config.command;
+
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.apache.karaf.config.command.completers.MetaCompleter;
+import org.apache.karaf.config.core.MetaServiceCaller;
+import org.apache.karaf.config.core.MetaServiceCaller.MetaInfo;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Completion;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.support.CommandException;
+import org.apache.karaf.shell.support.table.ShellTable;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.ObjectClassDefinition;
+
+@Command(scope = "config", name = "meta", description = "Lists meta type information.")
+@Service
+public class MetaCommand extends ConfigCommandSupport {
+
+    @Argument(name = "pid", description = "The configuration pid", required = true, multiValued = false)
+    @Completion(MetaCompleter.class)
+    protected String pid;
+
+    @Option(name = "-c", description = "Create respective config from metatype defaults", required = false, multiValued = false)
+    protected boolean create;
+
+    @Reference
+    BundleContext context;
+
+    private Map<Integer, String> typeMap;
+
+    public MetaCommand() {
+        typeMap = new HashMap<>();
+        typeMap.put(AttributeDefinition.BOOLEAN, "boolean");
+        typeMap.put(AttributeDefinition.BYTE, "byte");
+        typeMap.put(AttributeDefinition.CHARACTER, "char");
+        typeMap.put(AttributeDefinition.DOUBLE, "double");
+        typeMap.put(AttributeDefinition.FLOAT, "float");
+        typeMap.put(AttributeDefinition.INTEGER, "int");
+        typeMap.put(AttributeDefinition.LONG, "long");
+        typeMap.put(AttributeDefinition.PASSWORD, "password");
+        typeMap.put(AttributeDefinition.SHORT, "short");
+        typeMap.put(AttributeDefinition.STRING, "String");
+    }
+
+    @Override
+    public Object doExecute() throws Exception {
+        try {
+            if (create) {
+                MetaServiceCaller.doWithMetaType(context, pid, new Create());
+            } else {
+                MetaServiceCaller.doWithMetaType(context, pid, new Print());
+            }
+            return null;
+        } catch (Throwable e) {
+            Throwable ncdfe = e;
+            while (ncdfe != null && !(ncdfe instanceof NoClassDefFoundError)) {
+                ncdfe = ncdfe.getCause();
+            }
+            if (ncdfe != null && ncdfe.getMessage().equals("org/osgi/service/metatype/MetaTypeService")) {
+                throw new CommandException("config:meta disabled because the org.osgi.service.metatype package is not wired", e);
+            } else {
+                throw e;
+            }
+        }
+    }
+        
+    abstract class AbstractMeta implements Function<Optional<MetaInfo>, Void> {
+        protected String getDefaultValueStr(String[] defaultValues) {
+            if (defaultValues == null) {
+                return "";
+            }
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+            for (String defaultValue : defaultValues) {
+                if (first) {
+                    first = false;
+                } else {
+                    result.append(",");
+                }
+                result.append(defaultValue);
+            }
+            return result.toString();
+        }
+      
+    }
+    
+    class Create extends AbstractMeta {
+
+        public Void apply(Optional<MetaInfo> info) {
+            if (!info.isPresent()) {                
+                System.out.println("No meta type definition found for pid: " + pid);
+                return null;
+            }
+            
+            try {
+                createDefaultConfig(pid, info.get());
+            } catch (IOException e) {
+                 throw new RuntimeException(e.getMessage(), e);
+            }
+            return null;
+        }
+        
+        private void createDefaultConfig(String pid, MetaInfo info) throws IOException {
+            AttributeDefinition[] attrs = info.getDefinition().getAttributeDefinitions(ObjectClassDefinition.ALL);
+            if (attrs == null) {
+                return;
+            }
+            ConfigurationAdmin configAdmin = configRepository.getConfigAdmin();
+            Configuration config;
+            if(info.isFactory()) {
+                config = configAdmin.createFactoryConfiguration(pid, null);
+            }
+            else {
+                config = configAdmin.getConfiguration(pid, null);
+            }            
+            Dictionary<String, Object> props = new Hashtable<>();
+            for (AttributeDefinition attr : attrs) {
+                String valueStr = getDefaultValueStr(attr.getDefaultValue());
+                if (valueStr != null) {
+                    props.put(attr.getID(), valueStr);
+                }
+            }
+            config.update(props);
+        }
+
+    }
+    
+    class Print extends AbstractMeta {
+        public Void apply(Optional<MetaInfo> info) {           
+            if (!info.isPresent()) {
+                System.out.println("No meta type definition found for pid: " + pid);
+                return null;
+            }
+            if(info.get().isFactory()) {
+                System.out.println("Meta type informations for factory pid: " + pid);
+            }
+            else {
+                System.out.println("Meta type informations for pid: " + pid);
+            }
+            ShellTable table = new ShellTable();
+            table.column("key");
+            table.column("name");
+            table.column("type");
+            table.column("default");
+            table.column("description").wrap();
+            AttributeDefinition[] attrs = info.get().getDefinition().getAttributeDefinitions(ObjectClassDefinition.ALL);
+            if (attrs != null) {
+                for (AttributeDefinition attr : attrs) {
+                    table.addRow().addContent(attr.getID(), attr.getName(), getType(attr.getType()),
+                            getDefaultValueStr(attr.getDefaultValue()), attr.getDescription());
+                }
+            }
+            table.print(System.out);
+            return null;
+        }
+
+        private String getType(int type) {
+            return typeMap.get(type);
+        }
+
+    }        
+}
---
+++ config/command/src/main/java/org/apache/karaf/config/command/completers/ConfigurationPropertyCompleter.java
@@ -0,0 +1,180 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.karaf.config.command.completers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.apache.karaf.config.command.ConfigCommandSupport;
+import org.apache.karaf.config.core.MetaServiceCaller;
+import org.apache.karaf.config.core.MetaServiceCaller.MetaInfo;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.api.console.CommandLine;
+import org.apache.karaf.shell.api.console.Completer;
+import org.apache.karaf.shell.api.console.Session;
+import org.apache.karaf.shell.support.completers.StringsCompleter;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.ObjectClassDefinition;
+
+/**
+ * {@link Completer} for Configuration Admin properties.
+ *
+ * Displays a list of existing properties based on the current configuration being edited.
+ *
+ */
+@Service
+public class ConfigurationPropertyCompleter implements Completer {
+
+    private static final String OPTION = "-p";
+    private static final String ALIAS = "--pid";
+
+    @Reference
+    private ConfigurationAdmin configAdmin;
+    
+    @Reference
+    private BundleContext context;
+
+    public int complete(final Session session, final CommandLine commandLine, final List<String> candidates) {
+        StringsCompleter strings = new StringsCompleter();
+        if (session != null) {
+            String pid = getPid(session, commandLine);
+            Set<String> propertyNames = getPropertyNames(pid);
+            if (propertyNames != null && !propertyNames.isEmpty()) {
+                strings.getStrings().addAll(propertyNames);
+            }
+        }
+        return strings.complete(session, commandLine, candidates);
+    }
+
+    /**
+     * Retrieves the pid stored in the {@link Session} or passed as an argument.
+     * Argument takes precedence from pid stored in the {@link Session}.
+     */
+    private String getPid(Session session, CommandLine commandLine) {
+        String pid = (String) session.get(ConfigCommandSupport.PROPERTY_CONFIG_PID);
+        if (commandLine.getArguments().length > 0) {
+            List<String> arguments = Arrays.asList(commandLine.getArguments());
+            if (arguments.contains(OPTION)) {
+                int index = arguments.indexOf(OPTION);
+                if (arguments.size() > index) {
+                    return arguments.get(index + 1);
+                }
+            }
+
+            if (arguments.contains(ALIAS)) {
+                int index = arguments.indexOf(ALIAS);
+                if (arguments.size() > index) {
+                    return arguments.get(index + 1);
+                }
+            }
+        }
+        return pid;
+    }
+
+    /**
+     * Returns the property names for the given pid.
+     * @param pid
+     * @return
+     */
+    @SuppressWarnings("rawtypes")
+    private Set<String> getPropertyNames(String pid) {
+        Set<String> propertyNames = new HashSet<>();
+        if (pid != null) {     
+            try {
+                String metaPid = pid;
+                Configuration[] configs = configAdmin.listConfigurations("(service.pid="+pid+")");
+                if (configs != null && configs.length > 0) {
+                    Configuration configuration = configs[0];
+                    if (configuration != null) {
+                        Dictionary properties = configuration.getProcessedProperties(null);
+                        if (properties != null) {
+                            Enumeration keys = properties.keys();
+                            while (keys.hasMoreElements()) {
+                                propertyNames.add(String.valueOf(keys.nextElement()));
+                            }
+                        } 
+                        //If we have a factory config that is the PID to use
+                        if(configuration.getFactoryPid()!=null) {
+                            metaPid = configuration.getFactoryPid();
+                        }                                              
+                    }
+                }   
+                //Try to fetch additional properties from the Metatype service
+                List<String> metaTypeProperties = getMetaTypeProperties(metaPid);
+                propertyNames.addAll(metaTypeProperties);
+            } catch (IOException | InvalidSyntaxException e) {
+                //Ignore
+            }            
+        }
+        return propertyNames;
+    }
+    
+    private List<String> getMetaTypeProperties(String pid){
+        try {
+            return MetaServiceCaller.doWithMetaType(context, pid, this::collectMetaConfigProperties);
+        } catch (Throwable e) {
+            Throwable ncdfe = e;
+            while (ncdfe != null && !(ncdfe instanceof NoClassDefFoundError)) {
+                ncdfe = ncdfe.getCause();
+            }
+            if (ncdfe != null && ncdfe.getMessage().equals("org/osgi/service/metatype/MetaTypeService")) {
+                //It seems we don't have MetaTypeService 
+            } else {
+                throw e;
+            }
+        }
+        return Collections.emptyList();
+            
+    }
+    
+    private List<String> collectMetaConfigProperties(Optional<MetaInfo> info){
+        Optional<AttributeDefinition[]> attrs = info.map(e -> e.getDefinition().getAttributeDefinitions(ObjectClassDefinition.ALL));            
+        if(attrs.isPresent()) {
+            List<String> properties = new ArrayList<>(attrs.get().length);            
+            for (AttributeDefinition attr : attrs.get()) {                
+                properties.add(attr.getID());
+            }           
+            return properties;    
+        }
+        return Collections.emptyList();
+    }
+
+    public ConfigurationAdmin getConfigAdmin() {
+        return configAdmin;
+    }
+
+    public void setConfigAdmin(ConfigurationAdmin configAdmin) {
+        this.configAdmin = configAdmin;
+    }
+       
+}
