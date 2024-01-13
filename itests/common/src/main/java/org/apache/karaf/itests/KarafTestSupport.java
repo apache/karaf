9469db7e7c47b30214ae0c93fb79e1da97e51a04
Robert Varga <robert.varga@pantheon.tech>
2024-01-13 22:13:48 +0100

    [KARAF-7768] Remove org/apache/karaf/specs

--- itests/common/src/main/java/org/apache/karaf/itests/KarafTestSupport.java
+++ itests/common/src/main/java/org/apache/karaf/itests/KarafTestSupport.java
@@ -261,12 +261,6 @@ public class KarafTestSupport {
                 KarafDistributionOption.editConfigurationFilePut("etc/branding.properties", "welcome", ""), // No welcome banner
                 KarafDistributionOption.editConfigurationFilePut("etc/branding-ssh.properties", "welcome", ""),
                 new VMOption("--add-reads=java.xml=java.logging"),
-                new VMOption("--add-exports=java.base/org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED"),
-                new VMOption("--patch-module"),
-                new VMOption("java.base=lib/endorsed/org.apache.karaf.specs.locator-"
-                    + System.getProperty("karaf.version") + ".jar"),
-                new VMOption("--patch-module"), new VMOption("java.xml=lib/endorsed/org.apache.karaf.specs.java.xml-"
-                    + System.getProperty("karaf.version") + ".jar"),
                 new VMOption("--add-opens"),
                 new VMOption("java.base/java.security=ALL-UNNAMED"),
                 new VMOption("--add-opens"),
