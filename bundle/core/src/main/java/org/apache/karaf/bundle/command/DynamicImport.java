2e5bafa93ed6bc9d66817a64fb31fc7c297bdcb9
CptBartender <fryderyk.wysocki@gmail.com>
2026-06-11 10:03:11 +0200

    Added option to configure dynamic imports (#2682)

--- bundle/core/src/main/java/org/apache/karaf/bundle/command/DynamicImport.java
+++ bundle/core/src/main/java/org/apache/karaf/bundle/command/DynamicImport.java
@@ -16,26 +16,54 @@
  */
 package org.apache.karaf.bundle.command;
 
+import java.util.List;
+import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.osgi.framework.Bundle;
 
 /**
- * Command for enabling/disabling debug logging on a bundle and calculating the difference in
+ * Command for enabling/disabling dynamic imports on a bundle and calculating the difference in
  * wired imports.
  */
 @Command(scope = "bundle", name = "dynamic-import", description = "Enables/disables dynamic-import for a given bundle.")
 @Service
 public class DynamicImport extends BundleCommand {
 
+    @Argument(index = 1, name = "packages", description = "Bundle URLs separated by whitespaces", required = false, multiValued = true)
+    List<String> packages;
+
+    @Option(name = "--enable", aliases = {"-e"}, description = "Forces the command to execute", required = false, multiValued = false)
+    boolean enable;
+
+    @Option(name = "--disable", aliases = {"-d"}, description = "Forces the command to execute", required = false, multiValued = false)
+    boolean disable;
+
     @Override
     protected Object doExecute(Bundle bundle) throws Exception {
-        if (bundleService.isDynamicImport(bundle)) {
+        if (enable && disable) {
+            throw new IllegalArgumentException("Cannot 'enable' and 'disable' at the same time");
+        }
+        if ((enable || disable) && (packages != null && !packages.isEmpty())) {
+            throw new IllegalArgumentException("Options are incompatible with providing package list");
+        }
+        if (enable) {
+            System.out.printf("Enabling dynamic imports on bundle %s%n", bundle);
+            bundleService.setDynamicImports(bundle, List.of("*"));
+        } else if (disable) {
             System.out.printf("Disabling dynamic imports on bundle %s%n", bundle);
-            bundleService.disableDynamicImports(bundle);
+            bundleService.setDynamicImports(bundle, List.of());
+        } else if (packages != null && !packages.isEmpty()) {
+            System.out.printf("Enabling dynamic imports for [%s] on bundle %s%n",
+                String.join(", ",packages), bundle);
+            bundleService.setDynamicImports(bundle, packages);
+        } else if (bundleService.isDynamicImport(bundle)) {
+            System.out.printf("Disabling dynamic imports on bundle %s%n", bundle);
+            bundleService.setDynamicImports(bundle, List.of());
         } else {
             System.out.printf("Enabling dynamic imports on bundle %s%n", bundle);
-            bundleService.enableDynamicImports(bundle);
+            bundleService.setDynamicImports(bundle, List.of("*"));
         }
         return null;
     }
